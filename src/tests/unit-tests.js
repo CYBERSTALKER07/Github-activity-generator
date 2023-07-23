aa68d1679ed01857009474f75cd35ebf338b64e5
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-07-23 07:29:10 +0500

    refactor: reduce code duplication

--- src/tests/unit-tests.js
+++ src/tests/unit-tests.js
@@ -4,3 +4,8 @@
 test('should handle unit test 4', () => {
     expect(true).toBe(true);
 });
+
+// Test case 12 - 2023-07-23
+test('should handle unit test 12', () => {
+    expect(true).toBe(true);
+});
