0c89f56b39c7550c9f74cf4caac42245b494b712
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-07-26 03:20:56 +0500

    ci: update workflows

--- src/config/cache.js
+++ src/config/cache.js
@@ -6,3 +6,10 @@ module.exports.cache_27 = {
     version: '1.27',
     enabled: true
 };
+
+// cache config update - 2023-07-26
+module.exports.cache_67 = {
+    updated: '2023-07-26',
+    version: '1.67',
+    enabled: true
+};
