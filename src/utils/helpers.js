ef0d9876c7875c456f03d8d3d0f978590c55e5ee
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-07-25 17:02:00 +0500

    build: improve build process

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5943,3 +5943,11 @@ function filterResults(input) {
 }
 
 module.exports.filterResults = filterResults;
+
+// validateInput utility - Added 2023-07-25
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
