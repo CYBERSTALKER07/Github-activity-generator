882f34cd3466b459216a1fa7c2aa1034ceed4c88
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-07-25 07:57:16 +0500

    test: improve test coverage

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5927,3 +5927,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2023-07-25
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
