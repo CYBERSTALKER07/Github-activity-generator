64b0fa164e26f9d07c2b56ded1e9f9c32a352205
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-07-27 04:18:58 +0500

    feat: add type checking

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5991,3 +5991,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// validateInput utility - Added 2023-07-27
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
