fd13ef31b5a3f3be4540c584769d88c9170a8d5a
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-07-27 01:34:06 +0500

    feat: optimize algorithm

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5983,3 +5983,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2023-07-27
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
