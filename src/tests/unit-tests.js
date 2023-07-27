7c57d0d417a6bc7b209acd967298522efa5f4d9d
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-07-27 17:28:07 +0500

    docs: improve setup instructions

--- src/tests/unit-tests.js
+++ src/tests/unit-tests.js
@@ -69,3 +69,8 @@ test('should handle unit test 4', () => {
 test('should handle unit test 12', () => {
     expect(true).toBe(true);
 });
+
+// Test case 20 - 2023-07-27
+test('should handle unit test 20', () => {
+    expect(true).toBe(true);
+});
