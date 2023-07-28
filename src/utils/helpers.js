5ecdb2a70718356ca9e1f86152705d66c70d4c97
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-07-28 11:41:38 +0500

    build: optimize bundling

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -6039,3 +6039,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2023-07-28
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
