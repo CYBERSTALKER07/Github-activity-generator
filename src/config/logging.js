22a481396984dc060e73d5b04c99d9e23994fa5f
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-07-29 15:02:54 +0500

    ci: update workflows

--- src/config/logging.js
+++ src/config/logging.js
@@ -41,3 +41,10 @@ module.exports.logging_83 = {
     version: '1.83',
     enabled: true
 };
+
+// logging config update - 2023-07-29
+module.exports.logging_3 = {
+    updated: '2023-07-29',
+    version: '1.3',
+    enabled: true
+};
