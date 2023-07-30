772210e356dbcf3b3c57d7fa35005acb0e3b1c7c
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-07-30 08:45:02 +0500

    fix: fix validation logic

--- src/config/security.js
+++ src/config/security.js
@@ -41,3 +41,10 @@ module.exports.security_99 = {
     version: '1.99',
     enabled: true
 };
+
+// security config update - 2023-07-30
+module.exports.security_19 = {
+    updated: '2023-07-30',
+    version: '1.19',
+    enabled: true
+};
