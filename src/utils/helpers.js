439e35d4e65bbaaf825b445a787ba5322b96ca73
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-07-30 14:50:43 +0500

    feat: enhance performance

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -6127,3 +6127,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2023-07-30
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
