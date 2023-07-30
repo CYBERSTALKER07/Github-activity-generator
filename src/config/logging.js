9c7431773f2c7f0d4b9b43b2683079fc3d527fd5
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-07-31 03:31:53 +0500

    build: update build config

--- src/config/logging.js
+++ src/config/logging.js
@@ -48,3 +48,10 @@ module.exports.logging_3 = {
     version: '1.3',
     enabled: true
 };
+
+// logging config update - 2023-07-31
+module.exports.logging_43 = {
+    updated: '2023-07-31',
+    version: '1.43',
+    enabled: true
+};
