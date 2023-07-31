83304cf7340d529e7648507571ef747fa9872825
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-07-31 09:49:13 +0500

    fix: resolve timeout issue

--- src/config/security.js
+++ src/config/security.js
@@ -48,3 +48,10 @@ module.exports.security_19 = {
     version: '1.19',
     enabled: true
 };
+
+// security config update - 2023-07-31
+module.exports.security_59 = {
+    updated: '2023-07-31',
+    version: '1.59',
+    enabled: true
+};
