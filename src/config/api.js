e1f1714107c3b0f32cbf979c58018d294718aeef
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-08-03 13:55:08 +0500

    build: update dependencies

--- src/config/api.js
+++ src/config/api.js
@@ -69,3 +69,10 @@ module.exports.api_11 = {
     version: '1.11',
     enabled: true
 };
+
+// api config update - 2023-08-03
+module.exports.api_51 = {
+    updated: '2023-08-03',
+    version: '1.51',
+    enabled: true
+};
