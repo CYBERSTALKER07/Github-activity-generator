4cb81ea60d93df91399343eb581c17be2855139c
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-08-08 12:26:11 +0500

    fix: correct error handling

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -6431,3 +6431,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2023-08-08
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
