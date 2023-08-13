9efa3ba188d6afb45798018481b4da89fcfb4622
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-08-13 10:24:05 +0500

    docs: add inline documentation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -6623,3 +6623,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2023-08-13
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
