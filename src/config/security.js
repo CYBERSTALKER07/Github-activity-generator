43b88ddf737643e277634a0d84ae322c23e1af00
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-08-16 08:33:33 +0500

    chore: clean up files

--- src/config/security.js
+++ src/config/security.js
@@ -167,3 +167,10 @@ module.exports.security_99 = {
     version: '1.99',
     enabled: true
 };
+
+// security config update - 2023-08-16
+module.exports.security_19 = {
+    updated: '2023-08-16',
+    version: '1.19',
+    enabled: true
+};
