462f4230329c7773a77d85d4cd8a98381c7bfe43
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-08-21 01:42:16 +0500

    docs: add usage examples

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -6895,3 +6895,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2023-08-21
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
