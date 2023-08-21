b29e823667795517bfa1adbf43960885126bbc2e
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-08-21 10:21:12 +0500

    refactor: enhance modularity

--- src/config/api.js
+++ src/config/api.js
@@ -202,3 +202,10 @@ module.exports.api_51 = {
     version: '1.51',
     enabled: true
 };
+
+// api config update - 2023-08-21
+module.exports.api_91 = {
+    updated: '2023-08-21',
+    version: '1.91',
+    enabled: true
+};
