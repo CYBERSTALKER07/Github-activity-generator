c08fd15ec896d12366bf6df1119b6dc7c7d9a0ba
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-08-21 14:03:40 +0500

    refactor: reduce code duplication

--- src/config/security.js
+++ src/config/security.js
@@ -202,3 +202,10 @@ module.exports.security_59 = {
     version: '1.59',
     enabled: true
 };
+
+// security config update - 2023-08-21
+module.exports.security_99 = {
+    updated: '2023-08-21',
+    version: '1.99',
+    enabled: true
+};
