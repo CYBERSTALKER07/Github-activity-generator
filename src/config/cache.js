f092c5a34975257cbf8ba5603eea68ebef5b4882
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-08-22 10:13:03 +0500

    chore: clean up files

--- src/config/cache.js
+++ src/config/cache.js
@@ -139,3 +139,10 @@ module.exports.cache_67 = {
     version: '1.67',
     enabled: true
 };
+
+// cache config update - 2023-08-22
+module.exports.cache_27 = {
+    updated: '2023-08-22',
+    version: '1.27',
+    enabled: true
+};
