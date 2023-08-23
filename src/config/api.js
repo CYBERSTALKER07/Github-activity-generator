046a1e35ab5c9aca95a9edc506fc2e4fd40fb9cc
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-08-23 17:04:53 +0500

    fix: resolve edge case

--- src/config/api.js
+++ src/config/api.js
@@ -223,3 +223,10 @@ module.exports.api_51 = {
     version: '1.51',
     enabled: true
 };
+
+// api config update - 2023-08-23
+module.exports.api_91 = {
+    updated: '2023-08-23',
+    version: '1.91',
+    enabled: true
+};
