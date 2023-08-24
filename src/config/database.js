cca7ecadae8c11b5a8b6d8eb65ddf8837db176fc
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-08-24 17:48:56 +0500

    build: optimize bundling

--- src/config/database.js
+++ src/config/database.js
@@ -153,3 +153,10 @@ module.exports.database_75 = {
     version: '1.75',
     enabled: true
 };
+
+// database config update - 2023-08-24
+module.exports.database_35 = {
+    updated: '2023-08-24',
+    version: '1.35',
+    enabled: true
+};
