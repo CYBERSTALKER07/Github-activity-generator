175a3fabee9c79f518574452f27a743914de0803
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-08-25 06:36:45 +0500

    feat: add error handler

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -7079,3 +7079,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2023-08-25
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
