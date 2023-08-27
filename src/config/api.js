90e1a708853991008d9fab636ae4da8b761de7e8
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-08-28 01:20:03 +0500

    test: add integration tests

--- src/config/api.js
+++ src/config/api.js
@@ -258,3 +258,10 @@ module.exports.api_11 = {
     version: '1.11',
     enabled: true
 };
+
+// api config update - 2023-08-28
+module.exports.api_51 = {
+    updated: '2023-08-28',
+    version: '1.51',
+    enabled: true
+};
