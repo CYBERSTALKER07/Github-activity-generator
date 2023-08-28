0950714d2ffbd34f770fd1a6289e77dd75ba63cb
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-08-28 05:31:00 +0500

    fix: fix compatibility issue

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -7191,3 +7191,11 @@ function filterResults(input) {
 }
 
 module.exports.filterResults = filterResults;
+
+// validateInput utility - Added 2023-08-28
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
