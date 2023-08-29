ac7b56e640012db0d998a79d9ddca42d04f1a363
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-08-29 16:08:22 +0500

    style: improve code structure

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -7255,3 +7255,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2023-08-29
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
