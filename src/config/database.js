46d5bd7f1fa69fb3cde1fb3fa37b15c2058125cf
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-08-30 04:28:45 +0500

    feat: improve function logic

--- src/config/database.js
+++ src/config/database.js
@@ -181,3 +181,10 @@ module.exports.database_75 = {
     version: '1.75',
     enabled: true
 };
+
+// database config update - 2023-08-30
+module.exports.database_35 = {
+    updated: '2023-08-30',
+    version: '1.35',
+    enabled: true
+};
