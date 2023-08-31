a3b1201a3c6c2a7ec5dc9b267b362d9c2fa1812c
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-08-31 11:58:47 +0500

    fix: resolve timeout issue

--- src/config/api.js
+++ src/config/api.js
@@ -286,3 +286,10 @@ module.exports.api_51 = {
     version: '1.51',
     enabled: true
 };
+
+// api config update - 2023-08-31
+module.exports.api_91 = {
+    updated: '2023-08-31',
+    version: '1.91',
+    enabled: true
+};
