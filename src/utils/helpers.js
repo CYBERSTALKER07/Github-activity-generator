f1d21cf741c451fbd6cc673a4a1a82a3efa25e11
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-08-31 09:48:46 +0500

    chore: clean up files

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -7327,3 +7327,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2023-08-31
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
