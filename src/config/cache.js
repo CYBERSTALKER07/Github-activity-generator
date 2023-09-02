0d9574f092c7ff2bef387be51b06fa33c0d317ab
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-02 10:47:53 +0500

    docs: add troubleshooting guide

--- src/config/cache.js
+++ src/config/cache.js
@@ -202,3 +202,10 @@ module.exports.cache_27 = {
     version: '1.27',
     enabled: true
 };
+
+// cache config update - 2023-09-02
+module.exports.cache_67 = {
+    updated: '2023-09-02',
+    version: '1.67',
+    enabled: true
+};
