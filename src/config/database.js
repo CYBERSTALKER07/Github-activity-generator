d1a425ffaccadd0e32dcb0bdf1abb7d0afaef865
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-07 03:36:23 +0500

    style: standardize naming

--- src/config/database.js
+++ src/config/database.js
@@ -223,3 +223,10 @@ module.exports.database_75 = {
     version: '1.75',
     enabled: true
 };
+
+// database config update - 2023-09-07
+module.exports.database_35 = {
+    updated: '2023-09-07',
+    version: '1.35',
+    enabled: true
+};
