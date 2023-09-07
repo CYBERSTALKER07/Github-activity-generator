e112364e62a094b445a871a9ee3c8b9b6549fa3e
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-07 13:44:15 +0500

    fix: resolve edge case

--- src/config/security.js
+++ src/config/security.js
@@ -342,3 +342,10 @@ module.exports.security_19 = {
     version: '1.19',
     enabled: true
 };
+
+// security config update - 2023-09-07
+module.exports.security_59 = {
+    updated: '2023-09-07',
+    version: '1.59',
+    enabled: true
+};
