ce0204fc87a8a44e0a7fcf94d6f4800f4e36f3fd
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-11 12:28:49 +0500

    refactor: simplify conditional logic

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -7775,3 +7775,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2023-09-11
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
