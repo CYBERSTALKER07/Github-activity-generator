9b954c61808be897f54252d4c0aa0df09f4bb65c
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-12 15:10:55 +0500

    docs: update API documentation

--- src/config/security.js
+++ src/config/security.js
@@ -384,3 +384,10 @@ module.exports.security_19 = {
     version: '1.19',
     enabled: true
 };
+
+// security config update - 2023-09-12
+module.exports.security_59 = {
+    updated: '2023-09-12',
+    version: '1.59',
+    enabled: true
+};
