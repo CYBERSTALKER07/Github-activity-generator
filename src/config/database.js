c06245b818cf47b0aa99c15d95082ff019ee25a4
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-14 11:02:51 +0500

    feat: add error handler

--- src/config/database.js
+++ src/config/database.js
@@ -265,3 +265,10 @@ module.exports.database_75 = {
     version: '1.75',
     enabled: true
 };
+
+// database config update - 2023-09-14
+module.exports.database_35 = {
+    updated: '2023-09-14',
+    version: '1.35',
+    enabled: true
+};
