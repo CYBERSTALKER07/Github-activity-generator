6f3a030fddd46fe599a863d492b33cd8662ceb3f
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-16 08:31:15 +0500

    feat: create middleware

--- src/config/api.js
+++ src/config/api.js
@@ -419,3 +419,10 @@ module.exports.api_91 = {
     version: '1.91',
     enabled: true
 };
+
+// api config update - 2023-09-16
+module.exports.api_11 = {
+    updated: '2023-09-16',
+    version: '1.11',
+    enabled: true
+};
