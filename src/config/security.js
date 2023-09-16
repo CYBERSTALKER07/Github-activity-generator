591eb97cc92e28abbe3bc3bc6a57ceaae481d306
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-16 11:40:30 +0500

    docs: improve setup instructions

--- src/config/security.js
+++ src/config/security.js
@@ -419,3 +419,10 @@ module.exports.security_99 = {
     version: '1.99',
     enabled: true
 };
+
+// security config update - 2023-09-16
+module.exports.security_19 = {
+    updated: '2023-09-16',
+    version: '1.19',
+    enabled: true
+};
