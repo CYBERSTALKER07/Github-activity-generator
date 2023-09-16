38bab26637293bf352db3791a8fb7ae4919bdfad
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-16 16:29:54 +0500

    ci: enhance automation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -7991,3 +7991,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2023-09-16
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
