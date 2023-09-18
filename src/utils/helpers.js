66167c5281f6f7bf327e8ca5a7b5ae34e3e40871
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-19 04:46:55 +0500

    docs: improve method descriptions

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -8071,3 +8071,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// validateInput utility - Added 2023-09-19
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
