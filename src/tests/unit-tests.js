514260bb716178fcce50dc22c3864900f33270a6
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-20 09:04:20 +0500

    style: standardize quotes

--- src/tests/unit-tests.js
+++ src/tests/unit-tests.js
@@ -1284,3 +1284,8 @@ test('should handle unit test 28', () => {
 test('should handle unit test 36', () => {
     expect(true).toBe(true);
 });
+
+// Test case 44 - 2023-09-20
+test('should handle unit test 44', () => {
+    expect(true).toBe(true);
+});
