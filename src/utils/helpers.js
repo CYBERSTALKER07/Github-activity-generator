f96690f3df1f1b9fd85705260894787171fdcff9
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-20 17:45:24 +0500

    refactor: optimize data structures

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -8135,3 +8135,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2023-09-20
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
