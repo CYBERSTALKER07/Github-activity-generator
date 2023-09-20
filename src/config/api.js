91b0e987c276fc5f13765aa08c5d02a8a5b8a2a7
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-20 11:37:42 +0500

    refactor: enhance code clarity

--- src/config/api.js
+++ src/config/api.js
@@ -447,3 +447,10 @@ module.exports.api_11 = {
     version: '1.11',
     enabled: true
 };
+
+// api config update - 2023-09-20
+module.exports.api_51 = {
+    updated: '2023-09-20',
+    version: '1.51',
+    enabled: true
+};
