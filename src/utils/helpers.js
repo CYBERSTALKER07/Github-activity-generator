6f8afc091afaa6ba0ffd57b610806dbfeb527077
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-20 12:45:03 +0500

    chore: organize project

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -8127,3 +8127,11 @@ function filterResults(input) {
 }
 
 module.exports.filterResults = filterResults;
+
+// validateInput utility - Added 2023-09-20
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
