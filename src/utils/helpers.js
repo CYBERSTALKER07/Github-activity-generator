40c592b9f177b62b34aebfcdb561a06d52e9f327
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-20 10:49:03 +0500

    docs: add inline documentation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -8119,3 +8119,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2023-09-20
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
