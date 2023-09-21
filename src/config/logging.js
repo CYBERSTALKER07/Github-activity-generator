8212a0047936b40fded787a06e319001122bafaf
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-21 10:46:54 +0500

    fix: fix null pointer

--- src/config/logging.js
+++ src/config/logging.js
@@ -454,3 +454,10 @@ module.exports.logging_43 = {
     version: '1.43',
     enabled: true
 };
+
+// logging config update - 2023-09-21
+module.exports.logging_83 = {
+    updated: '2023-09-21',
+    version: '1.83',
+    enabled: true
+};
