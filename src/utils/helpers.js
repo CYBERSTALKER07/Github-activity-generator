313278fdcc71d8762f7235ae79e094a3ce1419a2
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-22 13:34:23 +0500

    feat: add error handler

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -8199,3 +8199,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2023-09-22
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
