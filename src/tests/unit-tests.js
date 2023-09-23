10cf3236ea913afde987040f7b8cd726d25905df
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-23 06:28:33 +0500

    feat: add type checking

--- src/tests/unit-tests.js
+++ src/tests/unit-tests.js
@@ -1349,3 +1349,8 @@ test('should handle unit test 36', () => {
 test('should handle unit test 44', () => {
     expect(true).toBe(true);
 });
+
+// Test case 52 - 2023-09-23
+test('should handle unit test 52', () => {
+    expect(true).toBe(true);
+});
