3308aa9b93eb32f381d8a5bbd280053d1d7daae7
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-24 14:41:41 +0500

    style: update indentation

--- src/tests/unit-tests.js
+++ src/tests/unit-tests.js
@@ -1389,3 +1389,8 @@ test('should handle unit test 4', () => {
 test('should handle unit test 12', () => {
     expect(true).toBe(true);
 });
+
+// Test case 20 - 2023-09-24
+test('should handle unit test 20', () => {
+    expect(true).toBe(true);
+});
