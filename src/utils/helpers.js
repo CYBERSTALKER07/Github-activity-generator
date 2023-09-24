bbf1f4df1db0b7218674694b9c61a231336dc651
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-24 07:33:46 +0500

    test: add integration tests

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -8279,3 +8279,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// validateInput utility - Added 2023-09-24
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
