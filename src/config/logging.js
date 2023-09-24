7504324e088a8f5a3d155fe303a66cdfed5270a0
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-24 07:38:10 +0500

    style: update indentation

--- src/config/logging.js
+++ src/config/logging.js
@@ -482,3 +482,10 @@ module.exports.logging_83 = {
     version: '1.83',
     enabled: true
 };
+
+// logging config update - 2023-09-24
+module.exports.logging_3 = {
+    updated: '2023-09-24',
+    version: '1.3',
+    enabled: true
+};
