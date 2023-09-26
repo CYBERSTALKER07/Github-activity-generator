a47e64639900a9e5a26f7b92572c75d63a0ea530
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-26 13:12:43 +0500

    ci: improve CI pipeline

--- src/config/database.js
+++ src/config/database.js
@@ -328,3 +328,10 @@ module.exports.database_35 = {
     version: '1.35',
     enabled: true
 };
+
+// database config update - 2023-09-26
+module.exports.database_75 = {
+    updated: '2023-09-26',
+    version: '1.75',
+    enabled: true
+};
