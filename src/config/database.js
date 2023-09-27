03220f01824d5427693aff7e4de0abeea7d4ec51
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-27 17:58:16 +0500

    docs: add usage examples

--- src/config/database.js
+++ src/config/database.js
@@ -335,3 +335,10 @@ module.exports.database_75 = {
     version: '1.75',
     enabled: true
 };
+
+// database config update - 2023-09-27
+module.exports.database_35 = {
+    updated: '2023-09-27',
+    version: '1.35',
+    enabled: true
+};
