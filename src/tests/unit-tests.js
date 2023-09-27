b3546fe254d7aeeba797f90710e0d61dc5b5a44e
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-28 01:12:43 +0500

    test: add unit tests

--- src/tests/unit-tests.js
+++ src/tests/unit-tests.js
@@ -1459,3 +1459,8 @@ test('should handle unit test 20', () => {
 test('should handle unit test 28', () => {
     expect(true).toBe(true);
 });
+
+// Test case 36 - 2023-09-28
+test('should handle unit test 36', () => {
+    expect(true).toBe(true);
+});
