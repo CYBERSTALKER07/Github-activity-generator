41442ebcad2c625b3681f9017df3e8a40f116dc0
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-27 06:55:00 +0500

    refactor: simplify conditional logic

--- src/tests/unit-tests.js
+++ src/tests/unit-tests.js
@@ -1439,3 +1439,8 @@ test('should handle unit test 84', () => {
 test('should handle unit test 92', () => {
     expect(true).toBe(true);
 });
+
+// Test case 4 - 2023-09-27
+test('should handle unit test 4', () => {
+    expect(true).toBe(true);
+});
