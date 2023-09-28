b4e0a1c2ba6635759e1e78e99ebaa8d48bfaf534
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-29 01:24:03 +0500

    docs: add inline documentation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -8455,3 +8455,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2023-09-29
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
