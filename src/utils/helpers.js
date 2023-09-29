24ecd247203cc682d5360e3bfe29f602aeeb1d5e
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-29 09:40:25 +0500

    feat: add type checking

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -8471,3 +8471,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2023-09-29
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
