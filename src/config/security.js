55464e2631e21ae247a6f3fd3290c6fd66156ccd
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-09-29 14:55:23 +0500

    test: improve test coverage

--- src/config/security.js
+++ src/config/security.js
@@ -517,3 +517,10 @@ module.exports.security_59 = {
     version: '1.59',
     enabled: true
 };
+
+// security config update - 2023-09-29
+module.exports.security_99 = {
+    updated: '2023-09-29',
+    version: '1.99',
+    enabled: true
+};
