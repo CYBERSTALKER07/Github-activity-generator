8660414742d39a3ccda8a9d34f308699e22b236d
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-01 10:35:25 +0500

    docs: add inline documentation

--- src/config/database.js
+++ src/config/database.js
@@ -356,3 +356,10 @@ module.exports.database_35 = {
     version: '1.35',
     enabled: true
 };
+
+// database config update - 2023-10-01
+module.exports.database_75 = {
+    updated: '2023-10-01',
+    version: '1.75',
+    enabled: true
+};
