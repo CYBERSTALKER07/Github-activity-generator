3f2c48d7b044edf4da0d401bcefc474116740fb5
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-01 12:22:19 +0500

    style: organize imports

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -8567,3 +8567,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2023-10-01
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
