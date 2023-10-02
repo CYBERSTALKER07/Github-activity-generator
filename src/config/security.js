12eb46d5e578e7d46b324e448263e012721891d5
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-02 11:34:06 +0500

    docs: add troubleshooting guide

--- src/config/security.js
+++ src/config/security.js
@@ -545,3 +545,10 @@ module.exports.security_99 = {
     version: '1.99',
     enabled: true
 };
+
+// security config update - 2023-10-02
+module.exports.security_19 = {
+    updated: '2023-10-02',
+    version: '1.19',
+    enabled: true
+};
