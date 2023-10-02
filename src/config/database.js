05d3bd484d056bfed50ec9fbf111b6cc316a0694
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-03 02:20:02 +0500

    ci: update workflows

--- src/config/database.js
+++ src/config/database.js
@@ -363,3 +363,10 @@ module.exports.database_75 = {
     version: '1.75',
     enabled: true
 };
+
+// database config update - 2023-10-03
+module.exports.database_35 = {
+    updated: '2023-10-03',
+    version: '1.35',
+    enabled: true
+};
