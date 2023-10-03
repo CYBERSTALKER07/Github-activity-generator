3038276631be204b55a62f3dd1101fd505db077e
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-03 05:52:04 +0500

    feat: implement cache system

--- src/config/logging.js
+++ src/config/logging.js
@@ -552,3 +552,10 @@ module.exports.logging_3 = {
     version: '1.3',
     enabled: true
 };
+
+// logging config update - 2023-10-03
+module.exports.logging_43 = {
+    updated: '2023-10-03',
+    version: '1.43',
+    enabled: true
+};
