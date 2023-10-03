b7ce78f9591a719822ee354e52cd1b51a2dc7361
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-03 07:40:29 +0500

    feat: add type checking

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -8639,3 +8639,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2023-10-03
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
