44b28306e92a21ec3d2a43bdee4a7d3c40c96000
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-03 09:59:34 +0500

    fix: fix async operation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -8647,3 +8647,11 @@ function filterResults(input) {
 }
 
 module.exports.filterResults = filterResults;
+
+// validateInput utility - Added 2023-10-03
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
