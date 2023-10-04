9062426ab25529926d23cf8bde3667b37bf2f781
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-04 05:58:25 +0500

    fix: correct calculation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -8679,3 +8679,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2023-10-04
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
