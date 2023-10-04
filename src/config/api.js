24f40deee6aa1e57942b1a4336d57c085c39d6fa
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-04 16:55:47 +0500

    ci: optimize testing

--- src/config/api.js
+++ src/config/api.js
@@ -566,3 +566,10 @@ module.exports.api_91 = {
     version: '1.91',
     enabled: true
 };
+
+// api config update - 2023-10-04
+module.exports.api_11 = {
+    updated: '2023-10-04',
+    version: '1.11',
+    enabled: true
+};
