0934df0683742fed3467f5881264177608d78c54
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-06 04:44:01 +0500

    ci: improve CI pipeline

--- src/config/api.js
+++ src/config/api.js
@@ -573,3 +573,10 @@ module.exports.api_11 = {
     version: '1.11',
     enabled: true
 };
+
+// api config update - 2023-10-06
+module.exports.api_51 = {
+    updated: '2023-10-06',
+    version: '1.51',
+    enabled: true
+};
