7be40b8bc30b4b6c59004ce2478877b9414cd8dd
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-06 12:54:21 +0500

    feat: implement helper method

--- src/tests/unit-tests.js
+++ src/tests/unit-tests.js
@@ -1664,3 +1664,8 @@ test('should handle unit test 60', () => {
 test('should handle unit test 68', () => {
     expect(true).toBe(true);
 });
+
+// Test case 76 - 2023-10-06
+test('should handle unit test 76', () => {
+    expect(true).toBe(true);
+});
