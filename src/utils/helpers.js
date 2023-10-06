6bc595ed6ea9b5167849d4325f5b404d05f32a89
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-06 15:42:11 +0500

    refactor: streamline workflow

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -8775,3 +8775,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2023-10-06
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
