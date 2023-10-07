48484369ddbd0c318c1cc92f85d7f3e84ae737e5
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-07 09:46:23 +0500

    docs: add troubleshooting guide

--- src/config/logging.js
+++ src/config/logging.js
@@ -587,3 +587,10 @@ module.exports.logging_83 = {
     version: '1.83',
     enabled: true
 };
+
+// logging config update - 2023-10-07
+module.exports.logging_3 = {
+    updated: '2023-10-07',
+    version: '1.3',
+    enabled: true
+};
