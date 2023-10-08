33ce72cef4578d601e08797548f2c75359cec230
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-09 03:35:30 +0500

    fix: resolve edge case

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -8823,3 +8823,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2023-10-09
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
