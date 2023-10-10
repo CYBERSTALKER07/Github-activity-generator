2246a5db548d39ad370a73f44588eba3ff79535a
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-10 13:26:06 +0500

    docs: update README

--- src/config/database.js
+++ src/config/database.js
@@ -398,3 +398,10 @@ module.exports.database_35 = {
     version: '1.35',
     enabled: true
 };
+
+// database config update - 2023-10-10
+module.exports.database_75 = {
+    updated: '2023-10-10',
+    version: '1.75',
+    enabled: true
+};
