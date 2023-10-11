4c273bed51b2f0d94e8fb4c5ab00006cef650971
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-11 14:05:34 +0500

    chore: clean up files

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -8911,3 +8911,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2023-10-11
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
