dccb3717acc2832b54282bd6f021b90bcdaa59ce
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-13 01:23:35 +0500

    perf: enhance efficiency

--- src/config/api.js
+++ src/config/api.js
@@ -615,3 +615,10 @@ module.exports.api_11 = {
     version: '1.11',
     enabled: true
 };
+
+// api config update - 2023-10-13
+module.exports.api_51 = {
+    updated: '2023-10-13',
+    version: '1.51',
+    enabled: true
+};
