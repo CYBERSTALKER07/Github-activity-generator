0c7f4b15e5001a2fa9c087dd09712a2f81283064
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-14 01:12:45 +0500

    perf: optimize performance

--- src/config/api.js
+++ src/config/api.js
@@ -622,3 +622,10 @@ module.exports.api_51 = {
     version: '1.51',
     enabled: true
 };
+
+// api config update - 2023-10-14
+module.exports.api_91 = {
+    updated: '2023-10-14',
+    version: '1.91',
+    enabled: true
+};
