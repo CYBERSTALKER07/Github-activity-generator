0035ccbfc57580bbb36ad2fa6a0aefbf29017e27
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-14 06:41:42 +0500

    docs: add configuration docs

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -9007,3 +9007,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// validateInput utility - Added 2023-10-14
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
