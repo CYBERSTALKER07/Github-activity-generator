ef4fdfca05ab03f1fb4b02158c3c82f995eb18c2
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-14 14:43:41 +0500

    build: improve build process

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -9023,3 +9023,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2023-10-14
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
