0b742d691f27ec8388d091aa7c02ddc94e316b4e
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-14 13:45:23 +0500

    fix: fix async operation

--- src/config/api.js
+++ src/config/api.js
@@ -629,3 +629,10 @@ module.exports.api_91 = {
     version: '1.91',
     enabled: true
 };
+
+// api config update - 2023-10-14
+module.exports.api_11 = {
+    updated: '2023-10-14',
+    version: '1.11',
+    enabled: true
+};
