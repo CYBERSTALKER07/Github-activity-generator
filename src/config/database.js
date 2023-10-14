d934db5c7363befe41ae33305f4e8054ffd35537
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-15 03:59:04 +0500

    refactor: simplify conditional logic

--- src/config/database.js
+++ src/config/database.js
@@ -419,3 +419,10 @@ module.exports.database_75 = {
     version: '1.75',
     enabled: true
 };
+
+// database config update - 2023-10-15
+module.exports.database_35 = {
+    updated: '2023-10-15',
+    version: '1.35',
+    enabled: true
+};
