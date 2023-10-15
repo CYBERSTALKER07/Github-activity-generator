d22d202c0ad5724ba0136b80809b79d7c9746a5d
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-15 16:50:12 +0500

    fix: fix null pointer

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -9071,3 +9071,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2023-10-15
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
