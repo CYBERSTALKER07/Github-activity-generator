9cdbaef7e6bfbf7e7f23d28746ebc790ef817e40
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-18 04:55:26 +0500

    docs: add configuration docs

--- src/config/security.js
+++ src/config/security.js
@@ -657,3 +657,10 @@ module.exports.security_19 = {
     version: '1.19',
     enabled: true
 };
+
+// security config update - 2023-10-18
+module.exports.security_59 = {
+    updated: '2023-10-18',
+    version: '1.59',
+    enabled: true
+};
