034cc0937183bc3851062e1e0c9ea0e444b9a8c1
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-18 07:54:44 +0500

    docs: improve setup instructions

--- src/config/cache.js
+++ src/config/cache.js
@@ -440,3 +440,10 @@ module.exports.cache_27 = {
     version: '1.27',
     enabled: true
 };
+
+// cache config update - 2023-10-18
+module.exports.cache_67 = {
+    updated: '2023-10-18',
+    version: '1.67',
+    enabled: true
+};
