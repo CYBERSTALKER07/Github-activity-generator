31edac1aa52d2ef28770ae99d6bc6e90da678014
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-24 17:28:23 +0500

    build: optimize bundling

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -9463,3 +9463,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2023-10-24
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
