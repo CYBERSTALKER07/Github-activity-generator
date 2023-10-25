a525b79e50d226d5c83fcf24c827614506464cee
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-25 06:00:08 +0500

    ci: enhance automation

--- src/config/api.js
+++ src/config/api.js
@@ -720,3 +720,10 @@ module.exports.api_11 = {
     version: '1.11',
     enabled: true
 };
+
+// api config update - 2023-10-25
+module.exports.api_51 = {
+    updated: '2023-10-25',
+    version: '1.51',
+    enabled: true
+};
