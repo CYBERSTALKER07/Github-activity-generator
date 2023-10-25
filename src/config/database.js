a5fb0d138f1b6340dfd3caf01e00fc328e5ca98f
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-26 03:08:12 +0500

    fix: fix compatibility issue

--- src/config/database.js
+++ src/config/database.js
@@ -482,3 +482,10 @@ module.exports.database_35 = {
     version: '1.35',
     enabled: true
 };
+
+// database config update - 2023-10-26
+module.exports.database_75 = {
+    updated: '2023-10-26',
+    version: '1.75',
+    enabled: true
+};
