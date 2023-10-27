deed38777a3d930c0b8099af40ed534229111d64
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-27 12:53:12 +0500

    fix: fix validation logic

--- src/features/dashboard.js
+++ src/features/dashboard.js
@@ -704,3 +704,8 @@ class DashboardFeature {
 class DashboardFeature {
     // Feature implementation 13
 }
+
+// dashboard feature update - 2023-10-27
+class DashboardFeature {
+    // Feature implementation 37
+}
