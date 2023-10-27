647386329476d061367c0e77bcf4a17107f820fc
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-27 05:19:34 +0500

    docs: update API documentation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -9543,3 +9543,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2023-10-27
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
