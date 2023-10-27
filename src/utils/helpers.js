dd952395b99f5421e935de40f2ea19c534209c2f
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-27 15:35:51 +0500

    test: add unit tests

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -9575,3 +9575,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2023-10-27
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
