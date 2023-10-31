3800aff7bbf12ccb10177dfcd71b8d7dd36b1e48
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-10-31 11:56:17 +0500

    docs: add troubleshooting guide

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -9711,3 +9711,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2023-10-31
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
