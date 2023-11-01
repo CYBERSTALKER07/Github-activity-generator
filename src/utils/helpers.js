2ef14a9025b6d4857b4aad257923e2e562705fac
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-01 09:46:33 +0500

    refactor: enhance code clarity

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -9751,3 +9751,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2023-11-01
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
