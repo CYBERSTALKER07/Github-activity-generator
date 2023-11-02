bbf3b2d5c1cf075038bb3320e3a732a18ce2a0a1
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-02 06:44:25 +0500

    fix: correct calculation

--- src/config/api.js
+++ src/config/api.js
@@ -783,3 +783,10 @@ module.exports.api_11 = {
     version: '1.11',
     enabled: true
 };
+
+// api config update - 2023-11-02
+module.exports.api_51 = {
+    updated: '2023-11-02',
+    version: '1.51',
+    enabled: true
+};
