31d62fa62785dabde24a29cc76502aa05cff86d3
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-02 05:32:40 +0500

    chore: organize project

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -9783,3 +9783,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2023-11-02
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
