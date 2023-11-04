9093aedfc7eb053b8be8bec70d397f887909ef3f
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-04 14:47:16 +0500

    fix: correct error handling

--- src/config/security.js
+++ src/config/security.js
@@ -790,3 +790,10 @@ module.exports.security_59 = {
     version: '1.59',
     enabled: true
 };
+
+// security config update - 2023-11-04
+module.exports.security_99 = {
+    updated: '2023-11-04',
+    version: '1.99',
+    enabled: true
+};
