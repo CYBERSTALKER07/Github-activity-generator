1c208389fe84f8edc507212a45e3a95264581a2c
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-06 01:56:20 +0500

    feat: implement cache system

--- src/config/api.js
+++ src/config/api.js
@@ -804,3 +804,10 @@ module.exports.api_11 = {
     version: '1.11',
     enabled: true
 };
+
+// api config update - 2023-11-06
+module.exports.api_51 = {
+    updated: '2023-11-06',
+    version: '1.51',
+    enabled: true
+};
