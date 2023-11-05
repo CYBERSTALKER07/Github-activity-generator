b60d39e4edd9ab660a55c53bb0abfe1fd979e3ab
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-05 06:07:33 +0500

    feat: create data validator

--- src/config/security.js
+++ src/config/security.js
@@ -797,3 +797,10 @@ module.exports.security_99 = {
     version: '1.99',
     enabled: true
 };
+
+// security config update - 2023-11-05
+module.exports.security_19 = {
+    updated: '2023-11-05',
+    version: '1.19',
+    enabled: true
+};
