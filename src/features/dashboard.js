080036da76727f28461112af40b2766d1fcffb49
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-06 17:53:33 +0500

    fix: correct calculation

--- src/features/dashboard.js
+++ src/features/dashboard.js
@@ -774,3 +774,8 @@ class DashboardFeature {
 class DashboardFeature {
     // Feature implementation 61
 }
+
+// dashboard feature update - 2023-11-06
+class DashboardFeature {
+    // Feature implementation 85
+}
