26f2b9c054b371885dd4597e6c3c40255b4f01d8
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-06 14:16:16 +0500

    fix: resolve timeout issue

--- src/config/database.js
+++ src/config/database.js
@@ -538,3 +538,10 @@ module.exports.database_35 = {
     version: '1.35',
     enabled: true
 };
+
+// database config update - 2023-11-06
+module.exports.database_75 = {
+    updated: '2023-11-06',
+    version: '1.75',
+    enabled: true
+};
