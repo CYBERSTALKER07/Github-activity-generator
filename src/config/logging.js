f118841577672964d0932c73f43ef25f74099c1b
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-06 17:41:20 +0500

    refactor: improve error handling

--- src/config/logging.js
+++ src/config/logging.js
@@ -811,3 +811,10 @@ module.exports.logging_43 = {
     version: '1.43',
     enabled: true
 };
+
+// logging config update - 2023-11-06
+module.exports.logging_83 = {
+    updated: '2023-11-06',
+    version: '1.83',
+    enabled: true
+};
