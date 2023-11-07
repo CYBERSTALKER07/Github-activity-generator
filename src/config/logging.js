eb2819fa3b12e3159b4ae68d182b50d9405e53e8
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-07 06:34:15 +0500

    chore: clean up files

--- src/config/logging.js
+++ src/config/logging.js
@@ -818,3 +818,10 @@ module.exports.logging_83 = {
     version: '1.83',
     enabled: true
 };
+
+// logging config update - 2023-11-07
+module.exports.logging_3 = {
+    updated: '2023-11-07',
+    version: '1.3',
+    enabled: true
+};
