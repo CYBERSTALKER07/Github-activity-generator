4f65ad1ef5a0102f775febe16bc67f7be0aa57ad
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-10 08:56:39 +0500

    test: add unit tests

--- src/config/security.js
+++ src/config/security.js
@@ -832,3 +832,10 @@ module.exports.security_59 = {
     version: '1.59',
     enabled: true
 };
+
+// security config update - 2023-11-10
+module.exports.security_99 = {
+    updated: '2023-11-10',
+    version: '1.99',
+    enabled: true
+};
