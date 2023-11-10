f9c9b32d56da7a1e60d6d7ae0b949d7d10934c8a
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-10 10:41:21 +0500

    test: add unit tests

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -10047,3 +10047,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// validateInput utility - Added 2023-11-10
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
