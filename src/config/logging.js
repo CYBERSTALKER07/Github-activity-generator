f536856cfd3e819b25eba640a405a8e3a20b446a
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-11 11:04:00 +0500

    test: add unit tests

--- src/config/logging.js
+++ src/config/logging.js
@@ -846,3 +846,10 @@ module.exports.logging_3 = {
     version: '1.3',
     enabled: true
 };
+
+// logging config update - 2023-11-11
+module.exports.logging_43 = {
+    updated: '2023-11-11',
+    version: '1.43',
+    enabled: true
+};
