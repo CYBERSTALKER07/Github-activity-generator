3b34c7874ac3f8c702ebc09f9122b4d962462165
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-12 14:36:26 +0500

    ci: optimize testing

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -10135,3 +10135,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2023-11-12
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
