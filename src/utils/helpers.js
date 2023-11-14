092ef134c58a9b49d6d70151dc628dd598684fd9
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-14 16:41:07 +0500

    build: update dependencies

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -10199,3 +10199,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2023-11-14
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
