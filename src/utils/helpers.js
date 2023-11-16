814c1cac7973737eef59f7bed9d6f87f15480d6f
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-16 07:26:58 +0500

    test: improve test coverage

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -10223,3 +10223,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2023-11-16
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
