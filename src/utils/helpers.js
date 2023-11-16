241f9c53e03363ec90d9b46fac6f873fde1da8c1
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-17 01:47:00 +0500

    ci: enhance automation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -10255,3 +10255,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// validateInput utility - Added 2023-11-17
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
