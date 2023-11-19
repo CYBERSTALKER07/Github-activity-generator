31c89b93f811c7aa185a139fe6f639ffe5e0a878
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-19 06:23:48 +0500

    fix: resolve timeout issue

--- src/config/logging.js
+++ src/config/logging.js
@@ -895,3 +895,10 @@ module.exports.logging_43 = {
     version: '1.43',
     enabled: true
 };
+
+// logging config update - 2023-11-19
+module.exports.logging_83 = {
+    updated: '2023-11-19',
+    version: '1.83',
+    enabled: true
+};
