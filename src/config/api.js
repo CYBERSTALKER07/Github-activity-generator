2281a283ce2094a74f95fcc612585687a84f47cc
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-19 16:57:30 +0500

    fix: fix compatibility issue

--- src/config/api.js
+++ src/config/api.js
@@ -902,3 +902,10 @@ module.exports.api_91 = {
     version: '1.91',
     enabled: true
 };
+
+// api config update - 2023-11-19
+module.exports.api_11 = {
+    updated: '2023-11-19',
+    version: '1.11',
+    enabled: true
+};
