7ca238b476a69a117c0522e405b69f42bf92769c
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-22 15:53:29 +0500

    perf: reduce memory usage

--- src/config/security.js
+++ src/config/security.js
@@ -923,3 +923,10 @@ module.exports.security_99 = {
     version: '1.99',
     enabled: true
 };
+
+// security config update - 2023-11-22
+module.exports.security_19 = {
+    updated: '2023-11-22',
+    version: '1.19',
+    enabled: true
+};
