d8cb364a0ea3b3d459da8e628dd854f4e5cc622c
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-23 13:52:49 +0500

    fix: fix null pointer

--- src/config/security.js
+++ src/config/security.js
@@ -930,3 +930,10 @@ module.exports.security_19 = {
     version: '1.19',
     enabled: true
 };
+
+// security config update - 2023-11-23
+module.exports.security_59 = {
+    updated: '2023-11-23',
+    version: '1.59',
+    enabled: true
+};
