dd0dc67e97682bd374c71debcae2c438dc7ae516
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-23 10:17:21 +0500

    refactor: simplify conditional logic

--- src/config/api.js
+++ src/config/api.js
@@ -930,3 +930,10 @@ module.exports.api_11 = {
     version: '1.11',
     enabled: true
 };
+
+// api config update - 2023-11-23
+module.exports.api_51 = {
+    updated: '2023-11-23',
+    version: '1.51',
+    enabled: true
+};
