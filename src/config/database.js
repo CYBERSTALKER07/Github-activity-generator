04917041dc28449abbbb8164a0cfd6b5821f0a08
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-24 09:46:34 +0500

    feat: implement helper method

--- src/config/database.js
+++ src/config/database.js
@@ -622,3 +622,10 @@ module.exports.database_35 = {
     version: '1.35',
     enabled: true
 };
+
+// database config update - 2023-11-24
+module.exports.database_75 = {
+    updated: '2023-11-24',
+    version: '1.75',
+    enabled: true
+};
