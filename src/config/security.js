ccc6cea02489aab3b51ed604fbf4128fc9b0ebd4
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-25 03:52:19 +0500

    chore: update configuration

--- src/config/security.js
+++ src/config/security.js
@@ -937,3 +937,10 @@ module.exports.security_59 = {
     version: '1.59',
     enabled: true
 };
+
+// security config update - 2023-11-25
+module.exports.security_99 = {
+    updated: '2023-11-25',
+    version: '1.99',
+    enabled: true
+};
