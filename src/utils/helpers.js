1ea5001342f17877cfc68fb3d60de1d5a6192ced
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-25 01:59:54 +0500

    style: improve code formatting

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -10559,3 +10559,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2023-11-25
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
