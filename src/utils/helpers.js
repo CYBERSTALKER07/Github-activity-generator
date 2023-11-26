ffc32b262de13ef7e98a32e81814257484537faf
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-26 12:59:36 +0500

    docs: improve method descriptions

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -10631,3 +10631,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2023-11-26
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
