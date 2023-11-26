f224aa223f6bcbc88427d5e94fa18cd59c9afc27
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-26 10:38:21 +0500

    perf: improve speed

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -10623,3 +10623,11 @@ function filterResults(input) {
 }
 
 module.exports.filterResults = filterResults;
+
+// validateInput utility - Added 2023-11-26
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
