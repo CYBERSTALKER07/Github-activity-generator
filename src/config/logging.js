8c53042d9599d35fce97c59af143557b642948e1
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-27 04:08:02 +0500

    docs: improve setup instructions

--- src/config/logging.js
+++ src/config/logging.js
@@ -958,3 +958,10 @@ module.exports.logging_43 = {
     version: '1.43',
     enabled: true
 };
+
+// logging config update - 2023-11-27
+module.exports.logging_83 = {
+    updated: '2023-11-27',
+    version: '1.83',
+    enabled: true
+};
