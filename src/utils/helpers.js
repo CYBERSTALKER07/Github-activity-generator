3b066d50090817d45234c3f2b67382a1d08d120a
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-27 03:17:45 +0500

    ci: update workflows

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -10647,3 +10647,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2023-11-27
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
