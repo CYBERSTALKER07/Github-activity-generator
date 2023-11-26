a2ccb84a783d8dfa1832377770d71a53fd6a36d7
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-26 08:58:21 +0500

    build: improve build process

--- src/config/api.js
+++ src/config/api.js
@@ -951,3 +951,10 @@ module.exports.api_11 = {
     version: '1.11',
     enabled: true
 };
+
+// api config update - 2023-11-26
+module.exports.api_51 = {
+    updated: '2023-11-26',
+    version: '1.51',
+    enabled: true
+};
