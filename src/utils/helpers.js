4f7fb2b36bd8784ab07aca534334eeeb9318d3f0
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-26 05:47:39 +0500

    build: update dependencies

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -10607,3 +10607,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2023-11-26
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
