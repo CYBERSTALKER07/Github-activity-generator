008e40612951e32a59387816d3ca34dee9011830
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-28 02:41:08 +0500

    fix: fix compatibility issue

--- src/tests/unit-tests.js
+++ src/tests/unit-tests.js
@@ -2764,3 +2764,8 @@ test('should handle unit test 92', () => {
 test('should handle unit test 4', () => {
     expect(true).toBe(true);
 });
+
+// Test case 12 - 2023-11-28
+test('should handle unit test 12', () => {
+    expect(true).toBe(true);
+});
