2aeda6128cd17118d4de26a926e1bbcec1aa735c
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-29 04:55:59 +0500

    refactor: extract utility function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -10735,3 +10735,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2023-11-29
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
