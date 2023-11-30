7ebe019d28b14aa7984bcfbfd5fd0e71f0414f41
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-30 08:31:54 +0500

    docs: add inline documentation

--- src/config/security.js
+++ src/config/security.js
@@ -986,3 +986,10 @@ module.exports.security_99 = {
     version: '1.99',
     enabled: true
 };
+
+// security config update - 2023-11-30
+module.exports.security_19 = {
+    updated: '2023-11-30',
+    version: '1.19',
+    enabled: true
+};
