6416e116dab494ea1c323fbf9a6268f0555a8e50
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-11-30 07:44:54 +0500

    fix: resolve memory leak

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -10791,3 +10791,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2023-11-30
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
