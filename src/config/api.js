73c094c2477cd47a88540a2d11ee94835eeda842
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-01 02:55:58 +0500

    refactor: reduce code duplication

--- src/config/api.js
+++ src/config/api.js
@@ -993,3 +993,10 @@ module.exports.api_11 = {
     version: '1.11',
     enabled: true
 };
+
+// api config update - 2023-12-01
+module.exports.api_51 = {
+    updated: '2023-12-01',
+    version: '1.51',
+    enabled: true
+};
