5131014d2cb6df5bcc8bd80c33376d7ac3b91615
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-01 17:41:34 +0500

    fix: fix compatibility issue

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -10855,3 +10855,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2023-12-01
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
