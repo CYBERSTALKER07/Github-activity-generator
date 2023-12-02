82daf16f21c1a7ba8e86decd73cd083ba3310b02
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-02 10:05:31 +0500

    feat: implement helper method

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -10871,3 +10871,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2023-12-02
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
