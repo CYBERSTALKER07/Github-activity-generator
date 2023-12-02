ab7269873c0bc7bc64e25fdf108251dadecfd529
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-02 06:57:21 +0500

    style: clean up comments

--- src/config/api.js
+++ src/config/api.js
@@ -1000,3 +1000,10 @@ module.exports.api_51 = {
     version: '1.51',
     enabled: true
 };
+
+// api config update - 2023-12-02
+module.exports.api_91 = {
+    updated: '2023-12-02',
+    version: '1.91',
+    enabled: true
+};
