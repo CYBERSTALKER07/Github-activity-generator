e63b807ab5b0921aa99694b1527471c37e51b35d
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-03 12:34:11 +0500

    style: improve code structure

--- src/tests/unit-tests.js
+++ src/tests/unit-tests.js
@@ -2899,3 +2899,8 @@ test('should handle unit test 20', () => {
 test('should handle unit test 28', () => {
     expect(true).toBe(true);
 });
+
+// Test case 36 - 2023-12-03
+test('should handle unit test 36', () => {
+    expect(true).toBe(true);
+});
