3f4ec5de326c0a50f1704c896acd8ba147830d12
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-03 15:38:36 +0500

    style: organize imports

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -10919,3 +10919,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2023-12-03
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
