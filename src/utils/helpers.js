094b638f6f7aa0d0de1c715dbe8c2707614d7b85
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-04 02:25:56 +0500

    test: enhance test suite

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -10927,3 +10927,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2023-12-04
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
