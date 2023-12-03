c4ce1950792765e2f2e27e97f3db26e72919d7f4
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-03 17:04:50 +0500

    fix: fix null pointer

--- src/config/logging.js
+++ src/config/logging.js
@@ -1014,3 +1014,10 @@ module.exports.logging_3 = {
     version: '1.3',
     enabled: true
 };
+
+// logging config update - 2023-12-03
+module.exports.logging_43 = {
+    updated: '2023-12-03',
+    version: '1.43',
+    enabled: true
+};
