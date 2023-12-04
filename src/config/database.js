cb6211940d0b5cbfc3226de85a29c663bc468ac6
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-04 12:43:36 +0500

    refactor: improve error handling

--- src/config/database.js
+++ src/config/database.js
@@ -678,3 +678,10 @@ module.exports.database_35 = {
     version: '1.35',
     enabled: true
 };
+
+// database config update - 2023-12-04
+module.exports.database_75 = {
+    updated: '2023-12-04',
+    version: '1.75',
+    enabled: true
+};
