d448573e4183dafaa69ee20d65be0cfe9fedfca5
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-05 08:08:57 +0500

    fix: correct error handling

--- src/config/api.js
+++ src/config/api.js
@@ -1028,3 +1028,10 @@ module.exports.api_91 = {
     version: '1.91',
     enabled: true
 };
+
+// api config update - 2023-12-05
+module.exports.api_11 = {
+    updated: '2023-12-05',
+    version: '1.11',
+    enabled: true
+};
