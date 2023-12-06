441d6322ef69a788baaf2b6c40f8591f49ec8a63
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-06 06:13:23 +0500

    refactor: enhance code clarity

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -11023,3 +11023,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2023-12-06
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
