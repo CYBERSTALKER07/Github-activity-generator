8640c3a9b4f0a236459adb830f4707a1efd84727
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-07 09:34:05 +0500

    feat: optimize algorithm

--- src/config/api.js
+++ src/config/api.js
@@ -1042,3 +1042,10 @@ module.exports.api_51 = {
     version: '1.51',
     enabled: true
 };
+
+// api config update - 2023-12-07
+module.exports.api_91 = {
+    updated: '2023-12-07',
+    version: '1.91',
+    enabled: true
+};
