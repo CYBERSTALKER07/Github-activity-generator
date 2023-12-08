b84d5d12773da5157c61be55d792be8d94a92bfd
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-08 07:02:33 +0500

    style: clean up whitespace

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -11103,3 +11103,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2023-12-08
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
