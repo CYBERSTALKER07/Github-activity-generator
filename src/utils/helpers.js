eaf45115ca14cd7f567d3325c87a91c1123f6460
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-11 16:14:22 +0500

    style: update indentation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -11255,3 +11255,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2023-12-11
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
