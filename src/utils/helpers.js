c55bcbb56446f73bd53d9f55e49c6a0e171b332e
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-12 08:32:54 +0500

    feat: create data validator

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -11279,3 +11279,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2023-12-12
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
