79dda2a80636175d0d67c0af06bf04ada3e03728
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-12 14:07:37 +0500

    perf: reduce memory usage

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -11295,3 +11295,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// validateInput utility - Added 2023-12-12
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
