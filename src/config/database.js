3abf1f8d8610e1d22b50a212b32794fe1b5c6e90
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-14 17:44:31 +0500

    test: enhance test suite

--- src/config/database.js
+++ src/config/database.js
@@ -734,3 +734,10 @@ module.exports.database_35 = {
     version: '1.35',
     enabled: true
 };
+
+// database config update - 2023-12-14
+module.exports.database_75 = {
+    updated: '2023-12-14',
+    version: '1.75',
+    enabled: true
+};
