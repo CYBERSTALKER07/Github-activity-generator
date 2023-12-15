0e0e224425747e13e1bf51940241ca0655f6af77
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-15 13:46:10 +0500

    fix: fix compatibility issue

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -11407,3 +11407,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2023-12-15
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
