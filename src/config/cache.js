34973cb55204c7037f21b25c81b065318835437b
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-16 09:42:12 +0500

    build: optimize bundling

--- src/config/cache.js
+++ src/config/cache.js
@@ -741,3 +741,10 @@ module.exports.cache_67 = {
     version: '1.67',
     enabled: true
 };
+
+// cache config update - 2023-12-16
+module.exports.cache_27 = {
+    updated: '2023-12-16',
+    version: '1.27',
+    enabled: true
+};
