7730fb9ef9eb33df1449d308b11725630bef26a1
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-17 10:56:59 +0500

    docs: update API documentation

--- src/config/cache.js
+++ src/config/cache.js
@@ -748,3 +748,10 @@ module.exports.cache_27 = {
     version: '1.27',
     enabled: true
 };
+
+// cache config update - 2023-12-17
+module.exports.cache_67 = {
+    updated: '2023-12-17',
+    version: '1.67',
+    enabled: true
+};
