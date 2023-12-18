cf065f64918242f42a1d8186fbd44ed43e6e8427
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-19 01:46:18 +0500

    feat: add error handler

--- src/tests/unit-tests.js
+++ src/tests/unit-tests.js
@@ -3249,3 +3249,8 @@ test('should handle unit test 4', () => {
 test('should handle unit test 12', () => {
     expect(true).toBe(true);
 });
+
+// Test case 20 - 2023-12-19
+test('should handle unit test 20', () => {
+    expect(true).toBe(true);
+});
