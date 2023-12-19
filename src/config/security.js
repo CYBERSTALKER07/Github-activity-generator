39685641592c4436f83cbeeda84f3a6c7b212a57
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-19 16:19:27 +0500

    refactor: improve error handling

--- src/config/security.js
+++ src/config/security.js
@@ -1140,3 +1140,10 @@ module.exports.security_19 = {
     version: '1.19',
     enabled: true
 };
+
+// security config update - 2023-12-19
+module.exports.security_59 = {
+    updated: '2023-12-19',
+    version: '1.59',
+    enabled: true
+};
