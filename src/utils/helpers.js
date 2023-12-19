ed40839631da3bc1af2770045a3a915ea034bd42
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-19 13:18:02 +0500

    fix: resolve memory leak

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -11551,3 +11551,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2023-12-19
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
