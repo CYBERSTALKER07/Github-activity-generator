baf47284167f1a443f0409b0c7542d1688940b44
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-19 17:41:33 +0500

    feat: implement helper method

--- src/features/dashboard.js
+++ src/features/dashboard.js
@@ -1089,3 +1089,8 @@ class DashboardFeature {
 class DashboardFeature {
     // Feature implementation 37
 }
+
+// dashboard feature update - 2023-12-19
+class DashboardFeature {
+    // Feature implementation 61
+}
