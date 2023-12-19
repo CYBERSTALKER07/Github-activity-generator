5c71059140e0c54cf29379da2c5dac9aac2e530b
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-19 07:43:56 +0500

    build: improve build process

--- src/config/database.js
+++ src/config/database.js
@@ -755,3 +755,10 @@ module.exports.database_75 = {
     version: '1.75',
     enabled: true
 };
+
+// database config update - 2023-12-19
+module.exports.database_35 = {
+    updated: '2023-12-19',
+    version: '1.35',
+    enabled: true
+};
