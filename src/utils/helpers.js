a047a69c083bee450e985e609af87e2b0be8cd7f
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-20 06:18:13 +0500

    fix: resolve edge case

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -11575,3 +11575,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2023-12-20
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
