a4d3fd67accc1d1003cfddaa1a0831a8874f37f2
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-21 04:47:23 +0500

    perf: improve speed

--- src/config/security.js
+++ src/config/security.js
@@ -1154,3 +1154,10 @@ module.exports.security_99 = {
     version: '1.99',
     enabled: true
 };
+
+// security config update - 2023-12-21
+module.exports.security_19 = {
+    updated: '2023-12-21',
+    version: '1.19',
+    enabled: true
+};
