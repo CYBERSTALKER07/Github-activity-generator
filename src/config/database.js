90c0010d00805f0f26dec87928f4c5cd3098921d
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-21 11:13:24 +0500

    style: clean up comments

--- src/config/database.js
+++ src/config/database.js
@@ -769,3 +769,10 @@ module.exports.database_75 = {
     version: '1.75',
     enabled: true
 };
+
+// database config update - 2023-12-21
+module.exports.database_35 = {
+    updated: '2023-12-21',
+    version: '1.35',
+    enabled: true
+};
