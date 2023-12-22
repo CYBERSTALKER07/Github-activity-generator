48a5e96dd028beb2d8d3da88c7b8a1726fc9ebab
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-23 01:54:19 +0500

    docs: add usage examples

--- src/config/api.js
+++ src/config/api.js
@@ -1168,3 +1168,10 @@ module.exports.api_51 = {
     version: '1.51',
     enabled: true
 };
+
+// api config update - 2023-12-23
+module.exports.api_91 = {
+    updated: '2023-12-23',
+    version: '1.91',
+    enabled: true
+};
