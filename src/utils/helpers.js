87c9f56a9831ae759555b0db2f8c785026b61f1b
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-23 17:23:02 +0500

    fix: fix async operation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -11743,3 +11743,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2023-12-23
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
