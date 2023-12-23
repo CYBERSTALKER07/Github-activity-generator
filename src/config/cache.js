3e4f9d137dcdfc7bb541e4b3cf5b8a4fe442093c
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-23 16:10:18 +0500

    fix: correct error handling

--- src/config/cache.js
+++ src/config/cache.js
@@ -783,3 +783,10 @@ module.exports.cache_67 = {
     version: '1.67',
     enabled: true
 };
+
+// cache config update - 2023-12-23
+module.exports.cache_27 = {
+    updated: '2023-12-23',
+    version: '1.27',
+    enabled: true
+};
