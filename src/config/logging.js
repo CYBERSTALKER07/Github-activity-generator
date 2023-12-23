1f717ae2a52943daf0465952d49786d092dcdc39
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-24 02:36:20 +0500

    style: standardize quotes

--- src/config/logging.js
+++ src/config/logging.js
@@ -1182,3 +1182,10 @@ module.exports.logging_3 = {
     version: '1.3',
     enabled: true
 };
+
+// logging config update - 2023-12-24
+module.exports.logging_43 = {
+    updated: '2023-12-24',
+    version: '1.43',
+    enabled: true
+};
