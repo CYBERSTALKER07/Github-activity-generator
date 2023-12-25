8e76523ab84065c7a0db7332666b65fdac7a0b4f
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-25 16:00:39 +0500

    fix: fix validation logic

--- src/config/logging.js
+++ src/config/logging.js
@@ -1196,3 +1196,10 @@ module.exports.logging_83 = {
     version: '1.83',
     enabled: true
 };
+
+// logging config update - 2023-12-25
+module.exports.logging_3 = {
+    updated: '2023-12-25',
+    version: '1.3',
+    enabled: true
+};
