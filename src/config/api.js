28d0c85d102cb46fe6a9960a0859afb579ab7002
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-25 08:08:48 +0500

    style: improve code formatting

--- src/config/api.js
+++ src/config/api.js
@@ -1189,3 +1189,10 @@ module.exports.api_51 = {
     version: '1.51',
     enabled: true
 };
+
+// api config update - 2023-12-25
+module.exports.api_91 = {
+    updated: '2023-12-25',
+    version: '1.91',
+    enabled: true
+};
