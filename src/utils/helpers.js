93da51e1ca2deccf86243bfaf71153c4f469db73
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-25 07:45:54 +0500

    feat: add error handler

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -11799,3 +11799,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2023-12-25
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
