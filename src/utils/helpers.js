f65555253f54d4c2e71eda296163bee88dfd0692
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-27 03:42:46 +0500

    build: improve build process

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -11855,3 +11855,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2023-12-27
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
