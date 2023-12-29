eecd6115e3b112993b4147bf3cab15e105089033
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-29 12:28:18 +0500

    refactor: optimize data structures

--- src/config/api.js
+++ src/config/api.js
@@ -1217,3 +1217,10 @@ module.exports.api_91 = {
     version: '1.91',
     enabled: true
 };
+
+// api config update - 2023-12-29
+module.exports.api_11 = {
+    updated: '2023-12-29',
+    version: '1.11',
+    enabled: true
+};
