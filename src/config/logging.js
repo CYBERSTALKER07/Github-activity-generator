4504a8a19409c0a9669748f0f8d6a1349949339b
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-29 09:46:11 +0500

    refactor: optimize data structures

--- src/config/logging.js
+++ src/config/logging.js
@@ -1217,3 +1217,10 @@ module.exports.logging_83 = {
     version: '1.83',
     enabled: true
 };
+
+// logging config update - 2023-12-29
+module.exports.logging_3 = {
+    updated: '2023-12-29',
+    version: '1.3',
+    enabled: true
+};
