d55d08b32756bfd21ac2159a152eeba3580d3b72
GitHub Contributor <shsoliyev@aut-edu.uz>
2023-12-30 13:17:45 +0500

    fix: resolve edge case

--- src/config/logging.js
+++ src/config/logging.js
@@ -1224,3 +1224,10 @@ module.exports.logging_3 = {
     version: '1.3',
     enabled: true
 };
+
+// logging config update - 2023-12-30
+module.exports.logging_43 = {
+    updated: '2023-12-30',
+    version: '1.43',
+    enabled: true
+};
