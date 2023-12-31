c1d43eac42f62e8f60fe35dd80e9261ad883d856
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-01-01 03:24:48 +0500

    refactor: extract utility function

--- src/config/logging.js
+++ src/config/logging.js
@@ -1238,3 +1238,10 @@ module.exports.logging_83 = {
     version: '1.83',
     enabled: true
 };
+
+// logging config update - 2024-01-01
+module.exports.logging_3 = {
+    updated: '2024-01-01',
+    version: '1.3',
+    enabled: true
+};
