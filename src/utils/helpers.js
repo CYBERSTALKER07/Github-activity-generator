f9c74dec60e673315cbef4b8150bba91571e24b7
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-01-01 12:30:56 +0500

    refactor: improve method signatures

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -12047,3 +12047,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-01-01
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
