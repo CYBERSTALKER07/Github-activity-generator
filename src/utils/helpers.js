16998914d0100719a73909183a1ec3bd35a052b9
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-01-03 08:27:28 +0500

    perf: reduce memory usage

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -12111,3 +12111,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2024-01-03
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
