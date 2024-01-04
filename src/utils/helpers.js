720c63884e5464249a3c2415087a8d0245c6a104
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-01-04 11:02:52 +0500

    style: standardize quotes

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -12159,3 +12159,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2024-01-04
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
