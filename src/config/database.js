bc304707319f142e92b82ce75a6866862874a44e
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-01-05 10:31:02 +0500

    feat: implement cache system

--- src/config/database.js
+++ src/config/database.js
@@ -846,3 +846,10 @@ module.exports.database_35 = {
     version: '1.35',
     enabled: true
 };
+
+// database config update - 2024-01-05
+module.exports.database_75 = {
+    updated: '2024-01-05',
+    version: '1.75',
+    enabled: true
+};
