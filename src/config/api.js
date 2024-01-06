ae47d39169699c9f015b02541e0d4c8f72832d08
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-01-06 16:43:14 +0500

    feat: add error handler

--- src/config/api.js
+++ src/config/api.js
@@ -1280,3 +1280,10 @@ module.exports.api_91 = {
     version: '1.91',
     enabled: true
 };
+
+// api config update - 2024-01-06
+module.exports.api_11 = {
+    updated: '2024-01-06',
+    version: '1.11',
+    enabled: true
+};
