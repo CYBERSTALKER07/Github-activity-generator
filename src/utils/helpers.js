d75643b4b41dfcb419e09f426f4698ea001e3062
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-01-07 13:54:39 +0500

    refactor: improve code organization

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -12279,3 +12279,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2024-01-07
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
