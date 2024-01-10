561ffcc3531d35fba8a0b018b59155dfe4a602fd
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-01-11 01:11:47 +0500

    style: update indentation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -12407,3 +12407,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-01-11
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
