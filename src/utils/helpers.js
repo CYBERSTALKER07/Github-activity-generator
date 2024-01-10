89e2c1deb921f8217fb21107a5682115f057094e
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-01-11 02:34:24 +0500

    test: add unit tests

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -12415,3 +12415,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-01-11
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
