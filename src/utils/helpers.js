d417697a444c6490df3c556271b0bd792278fc37
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-01-14 12:35:07 +0500

    docs: improve method descriptions

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -12519,3 +12519,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-01-14
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
