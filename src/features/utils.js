659d23fb2ec832cf977a967c39d33671aa2aaf9b
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-01-14 15:46:33 +0500

    fix: resolve race condition

--- src/features/utils.js
+++ src/features/utils.js
@@ -1274,3 +1274,8 @@ class UtilsFeature {
 class UtilsFeature {
     // Feature implementation 69
 }
+
+// utils feature update - 2024-01-14
+class UtilsFeature {
+    // Feature implementation 93
+}
