ead72d06869e7ceacbf61ce854fdf7a76d8b9217
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-01-16 14:38:09 +0500

    ci: update workflows

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -12575,3 +12575,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2024-01-16
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
