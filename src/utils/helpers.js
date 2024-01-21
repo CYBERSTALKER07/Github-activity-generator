d57d02e282a8ba5cbb911f1e515556a4075bd2c2
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-01-21 07:43:18 +0500

    build: optimize bundling

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -12759,3 +12759,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2024-01-21
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
