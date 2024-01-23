3ce945b615f52bd610eac6f5692ffe5d170669e0
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-01-23 17:48:29 +0500

    feat: add error handler

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -12823,3 +12823,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-01-23
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
