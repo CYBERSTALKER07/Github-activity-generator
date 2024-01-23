a7e95cc30d5eebf826ae40d3916573eff0521224
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-01-24 03:53:56 +0500

    perf: reduce memory usage

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -12831,3 +12831,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-01-24
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
