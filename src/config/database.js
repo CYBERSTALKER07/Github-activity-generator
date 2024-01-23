50df41d3366f209046999eabf08024a9bf7deeb2
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-01-24 01:33:37 +0500

    docs: improve setup instructions

--- src/config/database.js
+++ src/config/database.js
@@ -930,3 +930,10 @@ module.exports.database_35 = {
     version: '1.35',
     enabled: true
 };
+
+// database config update - 2024-01-24
+module.exports.database_75 = {
+    updated: '2024-01-24',
+    version: '1.75',
+    enabled: true
+};
