9fc19682bd1a14f0b85f45c90f641ccb21d7fdab
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-01-24 11:17:22 +0500

    docs: add inline documentation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -12847,3 +12847,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2024-01-24
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
