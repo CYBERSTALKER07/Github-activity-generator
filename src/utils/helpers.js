1f156a697c1e38162f4c27f8706daf14c93bee92
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-01-27 09:37:47 +0500

    chore: maintenance tasks

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -12903,3 +12903,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2024-01-27
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
