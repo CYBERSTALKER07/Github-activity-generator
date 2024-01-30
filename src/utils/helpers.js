572b6b84416c71c3e7161f9227f39a4c534d3a31
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-01-30 13:06:55 +0500

    ci: improve CI pipeline

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -13007,3 +13007,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2024-01-30
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
