060c9dbef68bdd93eafd9645c959a83be4d302ce
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-01-30 08:51:26 +0500

    build: update build config

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -12991,3 +12991,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2024-01-30
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
