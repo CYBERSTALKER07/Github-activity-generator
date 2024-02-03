f4cd2bf4fd9689af47b305760db39f609816d59c
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-02-03 14:51:38 +0500

    chore: organize project

--- src/config/logging.js
+++ src/config/logging.js
@@ -1455,3 +1455,10 @@ module.exports.logging_3 = {
     version: '1.3',
     enabled: true
 };
+
+// logging config update - 2024-02-03
+module.exports.logging_43 = {
+    updated: '2024-02-03',
+    version: '1.43',
+    enabled: true
+};
