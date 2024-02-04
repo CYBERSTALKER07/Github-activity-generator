5474846b95a30e2ed8a86d13599d2a2c63b39fd1
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-02-04 13:00:26 +0500

    docs: improve method descriptions

--- src/config/database.js
+++ src/config/database.js
@@ -972,3 +972,10 @@ module.exports.database_35 = {
     version: '1.35',
     enabled: true
 };
+
+// database config update - 2024-02-04
+module.exports.database_75 = {
+    updated: '2024-02-04',
+    version: '1.75',
+    enabled: true
+};
