3dcee555e5264b742a0ebcffa800e028d833cb1a
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-02-05 08:44:43 +0500

    fix: resolve memory leak

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -13159,3 +13159,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2024-02-05
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
