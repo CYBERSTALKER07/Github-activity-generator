9d0f0c5ece2eb1ec06d759de11a026a0e2726295
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-02-05 16:18:01 +0500

    chore: clean up files

--- src/config/api.js
+++ src/config/api.js
@@ -1469,3 +1469,10 @@ module.exports.api_91 = {
     version: '1.91',
     enabled: true
 };
+
+// api config update - 2024-02-05
+module.exports.api_11 = {
+    updated: '2024-02-05',
+    version: '1.11',
+    enabled: true
+};
