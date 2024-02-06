a0dcae17b9eadffd65c9465ac744ca25ca4ebbd4
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-02-06 06:56:11 +0500

    test: add integration tests

--- src/config/cache.js
+++ src/config/cache.js
@@ -979,3 +979,10 @@ module.exports.cache_67 = {
     version: '1.67',
     enabled: true
 };
+
+// cache config update - 2024-02-06
+module.exports.cache_27 = {
+    updated: '2024-02-06',
+    version: '1.27',
+    enabled: true
+};
