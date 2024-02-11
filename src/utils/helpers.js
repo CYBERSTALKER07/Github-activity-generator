307753447ec828eac64f73dc99183f54bf85b997
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-02-12 03:07:18 +0500

    fix: fix compatibility issue

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -13375,3 +13375,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// validateInput utility - Added 2024-02-12
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
