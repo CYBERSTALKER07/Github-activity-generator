cfbec2138ec94a82149a8b81fb87ff102b615966
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-02-16 07:33:25 +0500

    build: optimize bundling

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -13495,3 +13495,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-02-16
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
