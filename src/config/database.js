6087a9374f1e73f2ed508d8334fe383a47e9a982
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-02-19 11:56:54 +0500

    refactor: optimize data structures

--- src/config/database.js
+++ src/config/database.js
@@ -1035,3 +1035,10 @@ module.exports.database_75 = {
     version: '1.75',
     enabled: true
 };
+
+// database config update - 2024-02-19
+module.exports.database_35 = {
+    updated: '2024-02-19',
+    version: '1.35',
+    enabled: true
+};
