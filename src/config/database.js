7f4e04e89a6eac62e6e3db27d84675168105ea2d
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-02-21 16:46:13 +0500

    perf: optimize performance

--- src/config/database.js
+++ src/config/database.js
@@ -1049,3 +1049,10 @@ module.exports.database_75 = {
     version: '1.75',
     enabled: true
 };
+
+// database config update - 2024-02-21
+module.exports.database_35 = {
+    updated: '2024-02-21',
+    version: '1.35',
+    enabled: true
+};
