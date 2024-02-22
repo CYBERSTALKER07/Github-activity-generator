4aee903c778d284d19b1a2d1106847fe5d9354aa
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-02-22 06:17:05 +0500

    docs: add usage examples

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -13735,3 +13735,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2024-02-22
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
