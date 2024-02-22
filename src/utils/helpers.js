cd3b2ae8b0f19f8ce10afca8e42d34c50c50e831
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-02-23 04:18:56 +0500

    ci: optimize testing

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -13767,3 +13767,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-02-23
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
