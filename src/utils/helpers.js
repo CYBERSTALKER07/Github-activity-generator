8f49c56a8cadebe13a859ad82452695b30d17454
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-02-24 04:53:58 +0500

    feat: implement cache system

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -13815,3 +13815,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-02-24
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
