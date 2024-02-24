525bf936da9bcfc91d3381621bb4e3f6e30fc72b
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-02-24 08:57:00 +0500

    ci: optimize testing

--- src/config/database.js
+++ src/config/database.js
@@ -1063,3 +1063,10 @@ module.exports.database_75 = {
     version: '1.75',
     enabled: true
 };
+
+// database config update - 2024-02-24
+module.exports.database_35 = {
+    updated: '2024-02-24',
+    version: '1.35',
+    enabled: true
+};
