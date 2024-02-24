3d4aca4fe90ac0d21771a5f674ae9dba99578aa1
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-02-24 15:36:02 +0500

    docs: add inline documentation

--- src/config/logging.js
+++ src/config/logging.js
@@ -1602,3 +1602,10 @@ module.exports.logging_3 = {
     version: '1.3',
     enabled: true
 };
+
+// logging config update - 2024-02-24
+module.exports.logging_43 = {
+    updated: '2024-02-24',
+    version: '1.43',
+    enabled: true
+};
