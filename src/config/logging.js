ca1014d3242fbf5295627f7e23ab7083420e2831
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-02-25 15:02:32 +0500

    refactor: streamline workflow

--- src/config/logging.js
+++ src/config/logging.js
@@ -1609,3 +1609,10 @@ module.exports.logging_43 = {
     version: '1.43',
     enabled: true
 };
+
+// logging config update - 2024-02-25
+module.exports.logging_83 = {
+    updated: '2024-02-25',
+    version: '1.83',
+    enabled: true
+};
