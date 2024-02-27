a6fc0ebe774ebc9aeed8a79dd5ea11f5be09bb17
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-02-28 01:39:15 +0500

    fix: resolve edge case

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -13975,3 +13975,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-02-28
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
