26619e587b52efeca6eb774fa419f6c4e95c7188
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-02-28 08:45:23 +0500

    docs: update README

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -13999,3 +13999,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// validateInput utility - Added 2024-02-28
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
