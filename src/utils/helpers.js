a8e4b2230c270cc1a273214bf16d19d712473b56
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-02-28 13:55:21 +0500

    feat: create middleware

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -14007,3 +14007,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2024-02-28
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
