7313401ab4d06879df02ca71b78d6dce0a531cf6
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-02-29 05:52:50 +0500

    ci: improve CI pipeline

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -14023,3 +14023,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-02-29
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
