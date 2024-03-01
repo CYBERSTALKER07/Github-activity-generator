b7e03b56b4273daf131183a3a69cc267b65380cd
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-03-01 08:26:30 +0500

    fix: correct error handling

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -14063,3 +14063,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2024-03-01
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
