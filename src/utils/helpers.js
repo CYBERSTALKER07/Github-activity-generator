108632d01a6d4fff175e6a8ecd60cb60647f16cf
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-03-07 03:41:11 +0500

    refactor: enhance code clarity

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -14255,3 +14255,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2024-03-07
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
