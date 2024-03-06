cc52213d01c2a92fe72e5df5cf5148f39c39eb86
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-03-06 14:53:55 +0500

    test: add integration tests

--- src/config/database.js
+++ src/config/database.js
@@ -1119,3 +1119,10 @@ module.exports.database_75 = {
     version: '1.75',
     enabled: true
 };
+
+// database config update - 2024-03-06
+module.exports.database_35 = {
+    updated: '2024-03-06',
+    version: '1.35',
+    enabled: true
+};
