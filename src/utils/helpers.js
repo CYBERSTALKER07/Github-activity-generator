1d1e248e1660712a67eba0897215f2f91ddecfc8
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-03-08 01:45:34 +0500

    feat: add new utility function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -14279,3 +14279,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-03-08
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
