fe559d7568ec224ad6a2742da38ef3d136feac1f
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-03-09 03:39:04 +0500

    fix: resolve race condition

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -14327,3 +14327,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-03-09
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
