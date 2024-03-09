df32add83068ddb92088c5986413709584ba6703
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-03-09 15:12:03 +0500

    chore: update configuration

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -14359,3 +14359,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2024-03-09
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
