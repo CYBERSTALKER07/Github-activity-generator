40c73034eb6effdafaf064238a47d41f44a7d8c6
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-03-10 16:10:26 +0500

    build: improve build process

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -14391,3 +14391,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-03-10
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
