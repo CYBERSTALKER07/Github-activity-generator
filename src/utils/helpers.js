2522f4853d6c1d593726333e6a395409faaf0bfa
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-03-11 02:00:14 +0500

    fix: fix validation logic

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -14399,3 +14399,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2024-03-11
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
