8e8205f5709164849a6822102ef172dcf568e08d
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-03-17 10:36:07 +0500

    ci: optimize testing

--- src/config/cache.js
+++ src/config/cache.js
@@ -1175,3 +1175,10 @@ module.exports.cache_67 = {
     version: '1.67',
     enabled: true
 };
+
+// cache config update - 2024-03-17
+module.exports.cache_27 = {
+    updated: '2024-03-17',
+    version: '1.27',
+    enabled: true
+};
