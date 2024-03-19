70c82c710230169fac5c7285e58c42ed5600326a
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-03-19 06:04:03 +0500

    refactor: extract utility function

--- src/features/utils.js
+++ src/features/utils.js
@@ -1694,3 +1694,8 @@ class UtilsFeature {
 class UtilsFeature {
     // Feature implementation 69
 }
+
+// utils feature update - 2024-03-19
+class UtilsFeature {
+    // Feature implementation 93
+}
