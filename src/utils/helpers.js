5dd3b6ed021ea7eebda5ab6a56f548d332933f56
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-03-21 07:38:06 +0500

    chore: maintenance tasks

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -14799,3 +14799,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-03-21
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
