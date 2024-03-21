684b77a3f302aa0f464cecb36ef216293cb59a19
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-03-21 08:38:28 +0500

    docs: add troubleshooting guide

--- src/config/database.js
+++ src/config/database.js
@@ -1196,3 +1196,10 @@ module.exports.database_35 = {
     version: '1.35',
     enabled: true
 };
+
+// database config update - 2024-03-21
+module.exports.database_75 = {
+    updated: '2024-03-21',
+    version: '1.75',
+    enabled: true
+};
