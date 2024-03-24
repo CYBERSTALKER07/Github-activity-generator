aa28a687c2323e0036ddcc49a386f796234ab78c
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-03-24 07:18:54 +0500

    style: clean up whitespace

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -14871,3 +14871,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2024-03-24
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
