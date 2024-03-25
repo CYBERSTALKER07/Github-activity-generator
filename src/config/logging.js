61cf2a17e14d7754e8805a75f3f4edb8b4880c7c
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-03-25 11:36:49 +0500

    fix: fix async operation

--- src/config/logging.js
+++ src/config/logging.js
@@ -1819,3 +1819,10 @@ module.exports.logging_43 = {
     version: '1.43',
     enabled: true
 };
+
+// logging config update - 2024-03-25
+module.exports.logging_83 = {
+    updated: '2024-03-25',
+    version: '1.83',
+    enabled: true
+};
