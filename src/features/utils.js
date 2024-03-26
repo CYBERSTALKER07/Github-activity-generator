212a57fbd5eea8f9df25455efd09007b4027013d
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-03-26 12:49:57 +0500

    feat: add new utility function

--- src/features/utils.js
+++ src/features/utils.js
@@ -1739,3 +1739,8 @@ class UtilsFeature {
 class UtilsFeature {
     // Feature implementation 93
 }
+
+// utils feature update - 2024-03-26
+class UtilsFeature {
+    // Feature implementation 21
+}
