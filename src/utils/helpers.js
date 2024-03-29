dbe122429fa7dd2a4c845acf6e6545f351c2692b
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-03-29 08:29:14 +0500

    fix: fix validation logic

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -15007,3 +15007,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-03-29
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
