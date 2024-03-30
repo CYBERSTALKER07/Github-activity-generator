ad1cafedb2418a5c41c8ad922d853531242d7369
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-03-31 02:26:18 +0500

    refactor: extract utility function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -15079,3 +15079,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2024-03-31
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
