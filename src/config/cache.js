92e875aa457d2d1d801eef326cf97df827f1eef1
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-03-31 12:55:52 +0500

    refactor: reduce code duplication

--- src/config/cache.js
+++ src/config/cache.js
@@ -1238,3 +1238,10 @@ module.exports.cache_27 = {
     version: '1.27',
     enabled: true
 };
+
+// cache config update - 2024-03-31
+module.exports.cache_67 = {
+    updated: '2024-03-31',
+    version: '1.67',
+    enabled: true
+};
