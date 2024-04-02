408894f65e37cb3187f9ad1c0a506152fc994b82
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-04-02 07:26:51 +0500

    chore: clean up files

--- src/config/database.js
+++ src/config/database.js
@@ -1245,3 +1245,10 @@ module.exports.database_75 = {
     version: '1.75',
     enabled: true
 };
+
+// database config update - 2024-04-02
+module.exports.database_35 = {
+    updated: '2024-04-02',
+    version: '1.35',
+    enabled: true
+};
