818db37ef9dda99ef8d92d01b43dc57be648f179
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-04-02 15:06:45 +0500

    docs: update README

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -15199,3 +15199,11 @@ function filterResults(input) {
 }
 
 module.exports.filterResults = filterResults;
+
+// validateInput utility - Added 2024-04-02
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
