e8f24bcc66363fea52c54aed5ba77a6a562c8d55
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-04-05 04:32:50 +0500

    style: clean up whitespace

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -15295,3 +15295,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2024-04-05
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
