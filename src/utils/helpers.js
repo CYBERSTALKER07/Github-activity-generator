38a82f739cae87ea603a74435b429bf9acb4374b
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-04-11 02:18:41 +0500

    feat: create data validator

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -15495,3 +15495,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2024-04-11
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
