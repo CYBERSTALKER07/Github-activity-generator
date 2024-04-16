683f08f43be3c911534bc250fdbf715b6ee6e57f
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-04-17 02:46:04 +0500

    fix: fix null pointer

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -15663,3 +15663,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// validateInput utility - Added 2024-04-17
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
