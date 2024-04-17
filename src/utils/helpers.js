2370f09ea1104b6456b6d421ca3d72a611d5a5af
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-04-17 15:43:10 +0500

    fix: fix null pointer

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -15687,3 +15687,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-04-17
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
