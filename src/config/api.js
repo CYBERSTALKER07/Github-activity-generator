fadb4dd97d57c8bd4db430f71ac81f8a680ed2ce
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-04-18 12:52:36 +0500

    style: clean up whitespace

--- src/config/api.js
+++ src/config/api.js
@@ -1980,3 +1980,10 @@ module.exports.api_11 = {
     version: '1.11',
     enabled: true
 };
+
+// api config update - 2024-04-18
+module.exports.api_51 = {
+    updated: '2024-04-18',
+    version: '1.51',
+    enabled: true
+};
