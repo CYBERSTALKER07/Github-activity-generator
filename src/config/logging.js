782475294ed0fcc7d0ac50a8b4c302dbcd95ed82
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-04-18 09:35:11 +0500

    refactor: simplify conditional logic

--- src/config/logging.js
+++ src/config/logging.js
@@ -1980,3 +1980,10 @@ module.exports.logging_3 = {
     version: '1.3',
     enabled: true
 };
+
+// logging config update - 2024-04-18
+module.exports.logging_43 = {
+    updated: '2024-04-18',
+    version: '1.43',
+    enabled: true
+};
