91bef3e17e502a396e045d98384a3d507f428aaf
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-04-21 06:51:19 +0500

    ci: update workflows

--- src/config/api.js
+++ src/config/api.js
@@ -2001,3 +2001,10 @@ module.exports.api_11 = {
     version: '1.11',
     enabled: true
 };
+
+// api config update - 2024-04-21
+module.exports.api_51 = {
+    updated: '2024-04-21',
+    version: '1.51',
+    enabled: true
+};
