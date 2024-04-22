2f53d36f5aa3ac4efbafebfe5de45dd5ae938be0
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-04-23 02:13:17 +0500

    refactor: reduce code duplication

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -15887,3 +15887,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-04-23
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
