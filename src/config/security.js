e37c5e84c3d31aa93ba4b8d2dc1afcf6b7db9b07
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-04-22 09:42:36 +0500

    build: optimize bundling

--- src/config/security.js
+++ src/config/security.js
@@ -2008,3 +2008,10 @@ module.exports.security_59 = {
     version: '1.59',
     enabled: true
 };
+
+// security config update - 2024-04-22
+module.exports.security_99 = {
+    updated: '2024-04-22',
+    version: '1.99',
+    enabled: true
+};
