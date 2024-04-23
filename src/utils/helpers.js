5fc1ef6ad1e62c2e675b4d6688deafc62c8469f2
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-04-24 01:13:11 +0500

    ci: enhance automation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -15919,3 +15919,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2024-04-24
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
