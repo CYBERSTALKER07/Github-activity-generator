9312593bddd038c368eed28d763ced0aa1347e6a
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-04-25 11:23:51 +0500

    style: format JSON files

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -15959,3 +15959,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2024-04-25
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
