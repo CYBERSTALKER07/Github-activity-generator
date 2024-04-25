c325202199fe36d3c0f575b731eac37a33814835
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-04-26 03:39:55 +0500

    style: improve code formatting

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -15983,3 +15983,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2024-04-26
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
