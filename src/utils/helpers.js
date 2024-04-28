25544c9feb7cc4fcbfa3b04188ab9fa94da14475
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-04-28 15:07:27 +0500

    feat: improve function logic

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -16087,3 +16087,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2024-04-28
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
