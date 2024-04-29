16fb204bfd373ce7b5d98df67b9e2fa74191fd0f
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-04-30 02:49:27 +0500

    ci: update workflows

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -16095,3 +16095,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-04-30
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
