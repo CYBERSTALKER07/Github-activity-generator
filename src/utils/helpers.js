043242dc4907e5fe187c8b3076de0634fdc01aff
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-04-30 11:23:42 +0500

    build: improve build process

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -16127,3 +16127,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2024-04-30
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
