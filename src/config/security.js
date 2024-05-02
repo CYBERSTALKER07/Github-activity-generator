34c6fcd237210bd6ccc1544bdd082b5effb0d469
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-05-02 08:51:38 +0500

    feat: add type checking

--- src/config/security.js
+++ src/config/security.js
@@ -2078,3 +2078,10 @@ module.exports.security_99 = {
     version: '1.99',
     enabled: true
 };
+
+// security config update - 2024-05-02
+module.exports.security_19 = {
+    updated: '2024-05-02',
+    version: '1.19',
+    enabled: true
+};
