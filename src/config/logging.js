10093ec0db77813a5bea50257f585c000d452c1c
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-05-03 02:00:16 +0500

    refactor: reduce code duplication

--- src/config/logging.js
+++ src/config/logging.js
@@ -2085,3 +2085,10 @@ module.exports.logging_3 = {
     version: '1.3',
     enabled: true
 };
+
+// logging config update - 2024-05-03
+module.exports.logging_43 = {
+    updated: '2024-05-03',
+    version: '1.43',
+    enabled: true
+};
