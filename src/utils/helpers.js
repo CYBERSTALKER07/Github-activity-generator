911e1daa800c2e09ef5b847082bb050a23f9284d
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-05-03 12:03:54 +0500

    chore: organize project

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -16247,3 +16247,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2024-05-03
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
