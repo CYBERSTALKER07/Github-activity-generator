52b08ed4cacaac4218f5f72da45ab55978ae93a0
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-05-03 06:33:25 +0500

    style: update indentation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -16231,3 +16231,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2024-05-03
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
