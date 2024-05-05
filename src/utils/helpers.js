17122985548e792e7307a1e5d764684fed7bfe02
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-05-06 04:51:53 +0500

    refactor: improve method signatures

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -16343,3 +16343,11 @@ function filterResults(input) {
 }
 
 module.exports.filterResults = filterResults;
+
+// validateInput utility - Added 2024-05-06
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
