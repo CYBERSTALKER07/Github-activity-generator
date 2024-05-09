a1866c85eff53a9d5704aa97d0cbbd1f16def97b
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-05-09 13:00:01 +0500

    refactor: optimize data structures

--- src/config/security.js
+++ src/config/security.js
@@ -2113,3 +2113,10 @@ module.exports.security_59 = {
     version: '1.59',
     enabled: true
 };
+
+// security config update - 2024-05-09
+module.exports.security_99 = {
+    updated: '2024-05-09',
+    version: '1.99',
+    enabled: true
+};
