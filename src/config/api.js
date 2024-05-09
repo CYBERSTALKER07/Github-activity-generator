4f7acb410d6955d2f682a4ea8d1544b3bbded99b
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-05-09 06:09:48 +0500

    style: standardize naming

--- src/config/api.js
+++ src/config/api.js
@@ -2113,3 +2113,10 @@ module.exports.api_51 = {
     version: '1.51',
     enabled: true
 };
+
+// api config update - 2024-05-09
+module.exports.api_91 = {
+    updated: '2024-05-09',
+    version: '1.91',
+    enabled: true
+};
