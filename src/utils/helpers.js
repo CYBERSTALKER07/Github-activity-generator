3213551cb20db7507d2191769519a948b437058e
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-05-10 06:38:46 +0500

    fix: resolve timeout issue

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -16415,3 +16415,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-05-10
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
