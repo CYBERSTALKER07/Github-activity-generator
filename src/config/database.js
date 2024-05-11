c8badea2a10a6a00248cd4d991c9863a5ec85f60
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-05-11 07:18:10 +0500

    feat: optimize algorithm

--- src/config/database.js
+++ src/config/database.js
@@ -1420,3 +1420,10 @@ module.exports.database_35 = {
     version: '1.35',
     enabled: true
 };
+
+// database config update - 2024-05-11
+module.exports.database_75 = {
+    updated: '2024-05-11',
+    version: '1.75',
+    enabled: true
+};
