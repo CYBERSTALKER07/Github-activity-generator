bf07983946ef275848aa94df3965cd7c311110b8
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-05-13 17:19:59 +0500

    feat: add type checking

--- src/config/api.js
+++ src/config/api.js
@@ -2148,3 +2148,10 @@ module.exports.api_11 = {
     version: '1.11',
     enabled: true
 };
+
+// api config update - 2024-05-13
+module.exports.api_51 = {
+    updated: '2024-05-13',
+    version: '1.51',
+    enabled: true
+};
