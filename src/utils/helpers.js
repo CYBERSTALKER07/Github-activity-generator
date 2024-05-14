60344be63fc1252f44e0b78e5f1dfa698b5b48b2
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-05-15 03:29:41 +0500

    chore: organize project

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -16583,3 +16583,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2024-05-15
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
