8e527c2158f0d6715b2b08f297edf4768d020ff6
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-05-21 01:49:46 +0500

    refactor: simplify conditional logic

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -16767,3 +16767,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2024-05-21
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
