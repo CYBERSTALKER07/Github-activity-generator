93dcced21cd014ec3cacc586234a5b9100a0ddf8
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-05-21 15:12:04 +0500

    refactor: improve method signatures

--- src/config/api.js
+++ src/config/api.js
@@ -2197,3 +2197,10 @@ module.exports.api_51 = {
     version: '1.51',
     enabled: true
 };
+
+// api config update - 2024-05-21
+module.exports.api_91 = {
+    updated: '2024-05-21',
+    version: '1.91',
+    enabled: true
+};
