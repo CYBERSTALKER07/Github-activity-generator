6b0692f623b015a2640dbe3c124563f14c9b3b6e
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-05-22 04:38:27 +0500

    fix: resolve edge case

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -16815,3 +16815,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2024-05-22
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
