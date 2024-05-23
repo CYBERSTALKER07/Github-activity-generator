af38190022dc30bcad341a96c18960b43f4d77c2
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-05-24 04:57:26 +0500

    ci: improve CI pipeline

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -16903,3 +16903,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2024-05-24
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
