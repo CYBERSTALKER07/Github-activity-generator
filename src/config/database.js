ffc5b41a21813e19fc041ac78272babbfcba6d70
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-05-25 02:51:27 +0500

    chore: update configuration

--- src/config/database.js
+++ src/config/database.js
@@ -1483,3 +1483,10 @@ module.exports.database_75 = {
     version: '1.75',
     enabled: true
 };
+
+// database config update - 2024-05-25
+module.exports.database_35 = {
+    updated: '2024-05-25',
+    version: '1.35',
+    enabled: true
+};
