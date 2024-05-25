e53a570b0b59bdfb37c268a8acb9943d30335622
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-05-25 17:58:44 +0500

    style: format JSON files

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -16983,3 +16983,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-05-25
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
