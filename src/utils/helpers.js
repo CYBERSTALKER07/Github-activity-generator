5265aa2b3cc6fef09fc5687f679eaa73c536b444
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-05-29 04:33:17 +0500

    build: optimize bundling

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -17119,3 +17119,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// validateInput utility - Added 2024-05-29
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
