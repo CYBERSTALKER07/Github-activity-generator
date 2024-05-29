dcd25ad47fd847812722e867e4b98b35cf430a9f
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-05-29 08:54:53 +0500

    test: add integration tests

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -17135,3 +17135,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-05-29
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
