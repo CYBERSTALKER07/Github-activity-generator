67c1df48788c8ede748e64728183d90f39926d6a
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-06-02 13:38:00 +0500

    build: optimize bundling

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -17303,3 +17303,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-06-02
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
