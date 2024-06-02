1f1f697a08fefd3cd2527bed11e39bd8cf6cf3a6
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-06-02 11:14:57 +0500

    feat: add new utility function

--- src/config/database.js
+++ src/config/database.js
@@ -1532,3 +1532,10 @@ module.exports.database_35 = {
     version: '1.35',
     enabled: true
 };
+
+// database config update - 2024-06-02
+module.exports.database_75 = {
+    updated: '2024-06-02',
+    version: '1.75',
+    enabled: true
+};
