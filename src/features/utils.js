f6db73341ddc3c1ea189cd266fbc70649a9bd1b7
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-06-03 16:51:55 +0500

    style: standardize quotes

--- src/features/utils.js
+++ src/features/utils.js
@@ -2199,3 +2199,8 @@ class UtilsFeature {
 class UtilsFeature {
     // Feature implementation 93
 }
+
+// utils feature update - 2024-06-03
+class UtilsFeature {
+    // Feature implementation 21
+}
