9b03b2c4e33beba128d22fddb7d2f6f472b15ddf
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-06-06 05:08:35 +0500

    test: add integration tests

--- src/config/security.js
+++ src/config/security.js
@@ -2330,3 +2330,10 @@ module.exports.security_99 = {
     version: '1.99',
     enabled: true
 };
+
+// security config update - 2024-06-06
+module.exports.security_19 = {
+    updated: '2024-06-06',
+    version: '1.19',
+    enabled: true
+};
