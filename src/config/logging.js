d4d440fbb4ce5853089a13aeedc9d7763c8f7de8
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-06-07 01:51:37 +0500

    feat: create data validator

--- src/config/logging.js
+++ src/config/logging.js
@@ -2337,3 +2337,10 @@ module.exports.logging_3 = {
     version: '1.3',
     enabled: true
 };
+
+// logging config update - 2024-06-07
+module.exports.logging_43 = {
+    updated: '2024-06-07',
+    version: '1.43',
+    enabled: true
+};
