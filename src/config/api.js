6d270edd129267dea1adf2ab5b054c9de107b019
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-06-07 06:49:38 +0500

    feat: implement helper method

--- src/config/api.js
+++ src/config/api.js
@@ -2337,3 +2337,10 @@ module.exports.api_11 = {
     version: '1.11',
     enabled: true
 };
+
+// api config update - 2024-06-07
+module.exports.api_51 = {
+    updated: '2024-06-07',
+    version: '1.51',
+    enabled: true
+};
