afc34ae887e267ae6a5ebd4a6218c76a778385dd
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-06-08 14:27:56 +0500

    test: add integration tests

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -17527,3 +17527,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2024-06-08
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
