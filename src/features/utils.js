b491d304ca51582374a192aad978d6b6e15a1fff
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-06-08 12:44:24 +0500

    fix: fix null pointer

--- src/features/utils.js
+++ src/features/utils.js
@@ -2234,3 +2234,8 @@ class UtilsFeature {
 class UtilsFeature {
     // Feature implementation 69
 }
+
+// utils feature update - 2024-06-08
+class UtilsFeature {
+    // Feature implementation 93
+}
