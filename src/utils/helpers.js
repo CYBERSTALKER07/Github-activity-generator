5e835f04cf581973614b46dcdd068590bb292898
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-06-11 12:12:48 +0500

    chore: maintenance tasks

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -17599,3 +17599,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2024-06-11
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
