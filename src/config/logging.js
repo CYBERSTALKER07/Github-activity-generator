5a40095e22f56b3e23b00b23aa09b730a696882f
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-06-11 05:49:24 +0500

    perf: improve speed

--- src/config/logging.js
+++ src/config/logging.js
@@ -2358,3 +2358,10 @@ module.exports.logging_3 = {
     version: '1.3',
     enabled: true
 };
+
+// logging config update - 2024-06-11
+module.exports.logging_43 = {
+    updated: '2024-06-11',
+    version: '1.43',
+    enabled: true
+};
