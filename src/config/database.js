22ffc717a707520f2cea212c085d05f9d1c86942
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-06-13 12:22:16 +0500

    fix: fix async operation

--- src/config/database.js
+++ src/config/database.js
@@ -1581,3 +1581,10 @@ module.exports.database_75 = {
     version: '1.75',
     enabled: true
 };
+
+// database config update - 2024-06-13
+module.exports.database_35 = {
+    updated: '2024-06-13',
+    version: '1.35',
+    enabled: true
+};
