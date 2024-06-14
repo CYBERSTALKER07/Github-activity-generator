d88a80d525327201d983c8bf82442610f7510901
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-06-14 10:41:51 +0500

    refactor: extract utility function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -17711,3 +17711,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-06-14
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
