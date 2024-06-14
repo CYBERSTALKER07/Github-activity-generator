511c79289102fd08bd10b16ed85847e61df6a6ba
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-06-14 05:28:27 +0500

    feat: enhance performance

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -17695,3 +17695,11 @@ function filterResults(input) {
 }
 
 module.exports.filterResults = filterResults;
+
+// validateInput utility - Added 2024-06-14
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
