b0f93087f97957ebb1b68c139719c45190b57cbd
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-06-15 16:21:51 +0500

    feat: improve function logic

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -17767,3 +17767,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-06-15
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
