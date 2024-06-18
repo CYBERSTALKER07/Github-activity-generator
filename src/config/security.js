45565ce98c4ad7a3feb29c2a323523eacdd813ea
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-06-18 17:48:33 +0500

    feat: add error handler

--- src/config/security.js
+++ src/config/security.js
@@ -2414,3 +2414,10 @@ module.exports.security_99 = {
     version: '1.99',
     enabled: true
 };
+
+// security config update - 2024-06-18
+module.exports.security_19 = {
+    updated: '2024-06-18',
+    version: '1.19',
+    enabled: true
+};
