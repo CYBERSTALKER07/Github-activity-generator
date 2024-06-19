3fb08c800aadea1a30954bb125711e11a8598420
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-06-19 08:34:46 +0500

    fix: fix null pointer

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -17879,3 +17879,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2024-06-19
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
