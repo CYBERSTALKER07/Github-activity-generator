947c7a6d4cbb3430c39cfb46aaa39da3411df0f5
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-06-21 08:34:04 +0500

    perf: reduce memory usage

--- src/config/api.js
+++ src/config/api.js
@@ -2428,3 +2428,10 @@ module.exports.api_51 = {
     version: '1.51',
     enabled: true
 };
+
+// api config update - 2024-06-21
+module.exports.api_91 = {
+    updated: '2024-06-21',
+    version: '1.91',
+    enabled: true
+};
