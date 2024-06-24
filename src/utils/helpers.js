042156200f73cc7325b6ef8b3026e5695b9995da
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-06-24 11:22:14 +0500

    style: format JSON files

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -18079,3 +18079,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-06-24
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
