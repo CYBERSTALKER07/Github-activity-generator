067dda9c43b395e8f4df3242be45bf28322f8d89
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-06-24 14:56:55 +0500

    ci: improve CI pipeline

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -18087,3 +18087,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2024-06-24
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
