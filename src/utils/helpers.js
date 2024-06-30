e98b0c03d1031791e515b75e2bd0d31426692bde
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-06-30 17:22:15 +0500

    test: add unit tests

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -18295,3 +18295,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2024-06-30
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
