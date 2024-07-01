2ba70fb48f4f93140016b9c3cbac75177767317f
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-07-01 13:03:17 +0500

    feat: implement helper method

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -18327,3 +18327,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2024-07-01
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
