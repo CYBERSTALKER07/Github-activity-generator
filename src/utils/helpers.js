528608e7885c7a92c0cc4b78b05fcd7745144b38
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-07-03 04:18:12 +0500

    feat: enhance performance

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -18383,3 +18383,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-07-03
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
