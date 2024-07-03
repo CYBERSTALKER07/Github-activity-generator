907018e4e89bd606df5cf5859a558fce2875d5ef
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-07-03 12:14:56 +0500

    refactor: reduce code duplication

--- src/config/database.js
+++ src/config/database.js
@@ -1679,3 +1679,10 @@ module.exports.database_75 = {
     version: '1.75',
     enabled: true
 };
+
+// database config update - 2024-07-03
+module.exports.database_35 = {
+    updated: '2024-07-03',
+    version: '1.35',
+    enabled: true
+};
