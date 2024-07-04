f84258d3008a99a4fedf0290b4968fcae31434a0
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-07-04 16:51:02 +0500

    build: optimize bundling

--- src/config/api.js
+++ src/config/api.js
@@ -2533,3 +2533,10 @@ module.exports.api_51 = {
     version: '1.51',
     enabled: true
 };
+
+// api config update - 2024-07-04
+module.exports.api_91 = {
+    updated: '2024-07-04',
+    version: '1.91',
+    enabled: true
+};
