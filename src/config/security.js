2943c3f58456a837159259812dc016f494d10bbe
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-07-05 03:25:39 +0500

    ci: improve CI pipeline

--- src/config/security.js
+++ src/config/security.js
@@ -2533,3 +2533,10 @@ module.exports.security_59 = {
     version: '1.59',
     enabled: true
 };
+
+// security config update - 2024-07-05
+module.exports.security_99 = {
+    updated: '2024-07-05',
+    version: '1.99',
+    enabled: true
+};
