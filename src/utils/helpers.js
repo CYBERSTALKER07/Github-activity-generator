f8804df648471a6467ea67cf374637cfdfe21c3f
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-07-05 01:43:36 +0500

    feat: add new utility function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -18463,3 +18463,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2024-07-05
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
