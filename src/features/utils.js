ffb93b48b86e850b0bbf3d26f97fa9916b352ea8
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-07-05 09:45:28 +0500

    feat: add error handler

--- src/features/utils.js
+++ src/features/utils.js
@@ -2419,3 +2419,8 @@ class UtilsFeature {
 class UtilsFeature {
     // Feature implementation 93
 }
+
+// utils feature update - 2024-07-05
+class UtilsFeature {
+    // Feature implementation 21
+}
