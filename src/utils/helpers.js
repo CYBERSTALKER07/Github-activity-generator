a23ff5ea51503ce6244ee06dd583542ac7b86fc8
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-07-08 03:13:41 +0500

    style: standardize naming

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -18543,3 +18543,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-07-08
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
