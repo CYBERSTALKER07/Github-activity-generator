3035ce86eb0ef482967fe544ac59d7b903675786
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-07-07 16:02:45 +0500

    refactor: enhance modularity

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -18535,3 +18535,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2024-07-07
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
