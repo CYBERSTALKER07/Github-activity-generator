c7a740562bebb435e24dd0e40b3967b2ddf8dfa1
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-07-08 14:43:36 +0500

    ci: improve CI pipeline

--- src/config/api.js
+++ src/config/api.js
@@ -2554,3 +2554,10 @@ module.exports.api_51 = {
     version: '1.51',
     enabled: true
 };
+
+// api config update - 2024-07-08
+module.exports.api_91 = {
+    updated: '2024-07-08',
+    version: '1.91',
+    enabled: true
+};
