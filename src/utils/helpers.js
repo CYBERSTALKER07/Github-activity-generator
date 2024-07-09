7f2c376d7f4eafc79e3f837b1e7bdc6b76886f12
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-07-09 11:22:45 +0500

    feat: implement cache system

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -18591,3 +18591,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-07-09
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
