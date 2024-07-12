37e3180d71b3de586ce2540456a6a6cef3ac9d22
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-07-12 12:13:50 +0500

    docs: improve method descriptions

--- src/features/utils.js
+++ src/features/utils.js
@@ -2454,3 +2454,8 @@ class UtilsFeature {
 class UtilsFeature {
     // Feature implementation 69
 }
+
+// utils feature update - 2024-07-12
+class UtilsFeature {
+    // Feature implementation 93
+}
