765782e61684622fa53aa86f43e1e58473d2af08
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-07-14 01:03:01 +0500

    docs: update changelog

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -18719,3 +18719,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2024-07-14
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
