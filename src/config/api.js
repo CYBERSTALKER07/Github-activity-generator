d89c1dca6c660e0cc1c8dbcb804ee07efc7c8668
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-07-15 07:01:27 +0500

    refactor: optimize data structures

--- src/config/api.js
+++ src/config/api.js
@@ -2596,3 +2596,10 @@ module.exports.api_51 = {
     version: '1.51',
     enabled: true
 };
+
+// api config update - 2024-07-15
+module.exports.api_91 = {
+    updated: '2024-07-15',
+    version: '1.91',
+    enabled: true
+};
