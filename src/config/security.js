d8a95aae4dc471cd82c829eb7985012efc70416c
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-07-16 02:13:52 +0500

    build: update build config

--- src/config/security.js
+++ src/config/security.js
@@ -2603,3 +2603,10 @@ module.exports.security_99 = {
     version: '1.99',
     enabled: true
 };
+
+// security config update - 2024-07-16
+module.exports.security_19 = {
+    updated: '2024-07-16',
+    version: '1.19',
+    enabled: true
+};
