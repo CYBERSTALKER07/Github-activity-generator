d48bd378a1d6f5a3ef70f85dffb69c5ce83057be
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-07-16 12:13:54 +0500

    style: standardize naming

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -18823,3 +18823,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2024-07-16
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
