8f9df47be6bc2da4cd2179968029adc159c0e28a
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-07-17 14:52:15 +0500

    style: improve code formatting

--- src/config/api.js
+++ src/config/api.js
@@ -2617,3 +2617,10 @@ module.exports.api_51 = {
     version: '1.51',
     enabled: true
 };
+
+// api config update - 2024-07-17
+module.exports.api_91 = {
+    updated: '2024-07-17',
+    version: '1.91',
+    enabled: true
+};
