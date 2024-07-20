0af9a52c4a2cd704cb119c295f9b9ec7615ee987
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-07-20 17:47:21 +0500

    chore: clean up files

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -19007,3 +19007,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-07-20
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
