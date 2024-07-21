a14301ea9d625da67521a034e5261b9ec40c000f
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-07-22 01:04:13 +0500

    docs: update README

--- src/config/security.js
+++ src/config/security.js
@@ -2645,3 +2645,10 @@ module.exports.security_99 = {
     version: '1.99',
     enabled: true
 };
+
+// security config update - 2024-07-22
+module.exports.security_19 = {
+    updated: '2024-07-22',
+    version: '1.19',
+    enabled: true
+};
