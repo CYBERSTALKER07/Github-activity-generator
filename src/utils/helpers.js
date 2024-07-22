ca6430a30e6eed58c8a2ac60f53e65f21cac6d63
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-07-23 04:23:43 +0500

    perf: reduce memory usage

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -19039,3 +19039,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2024-07-23
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
