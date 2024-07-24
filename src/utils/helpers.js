9e11f29411e59f0e12f26c0c6aad11b613fcda94
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-07-24 09:26:40 +0500

    feat: add error handler

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -19095,3 +19095,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// validateInput utility - Added 2024-07-24
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
