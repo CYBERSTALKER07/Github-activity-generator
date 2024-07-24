9035db7839cc57c87d7bbf29a0b4489e4db60c75
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-07-24 16:35:02 +0500

    style: standardize quotes

--- src/config/cache.js
+++ src/config/cache.js
@@ -1777,3 +1777,10 @@ module.exports.cache_67 = {
     version: '1.67',
     enabled: true
 };
+
+// cache config update - 2024-07-24
+module.exports.cache_27 = {
+    updated: '2024-07-24',
+    version: '1.27',
+    enabled: true
+};
