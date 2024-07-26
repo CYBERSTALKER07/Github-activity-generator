faad99cc96a4a92601d6954dead020f69a25fe61
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-07-26 12:58:26 +0500

    docs: add usage examples

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -19191,3 +19191,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2024-07-26
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
