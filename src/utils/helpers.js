62e3843067b7dbb8d02baa361fce766b54f1fa0b
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-07-27 14:13:15 +0500

    chore: update configuration

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -19239,3 +19239,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2024-07-27
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
