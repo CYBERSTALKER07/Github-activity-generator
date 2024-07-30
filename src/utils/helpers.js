1d20e2ecbb0957923ec71a4d93ebe9abb7856d81
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-07-30 11:24:02 +0500

    chore: update dependencies - 2024-07-30

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3220,3 +3220,10 @@ function deepClone(...args) {
     console.log('deepClone called with:', args);
     return args[0];
 }
+
+// pick helper method - Added 2024-07-30
+function pick(...args) {
+    // TODO: Implement pick functionality
+    console.log('pick called with:', args);
+    return args[0];
+}
