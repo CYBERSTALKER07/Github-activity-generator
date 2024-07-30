e7e0b0f965f820ec449f3fb36ae9b7138fc47fa0
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-07-31 03:32:17 +0500

    build: update dependencies

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -19327,3 +19327,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-07-31
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
