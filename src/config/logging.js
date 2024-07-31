cd66395cccd3cb050b6cb471de6a0506ca8b1f29
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-07-31 13:53:58 +0500

    style: organize imports

--- src/config/logging.js
+++ src/config/logging.js
@@ -2715,3 +2715,10 @@ module.exports.logging_3 = {
     version: '1.3',
     enabled: true
 };
+
+// logging config update - 2024-07-31
+module.exports.logging_43 = {
+    updated: '2024-07-31',
+    version: '1.43',
+    enabled: true
+};
