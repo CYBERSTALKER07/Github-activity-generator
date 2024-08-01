4331057538fcffc4f0a50ddbb900a03fb3a5dbe8
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-01 13:51:01 +0500

    style: improve code structure

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -19383,3 +19383,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-08-01
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
