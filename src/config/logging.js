b7bb8fa355cd4414bef0ef4efd2f130e97a284f8
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-03 09:28:14 +0500

    style: clean up comments

--- src/config/logging.js
+++ src/config/logging.js
@@ -2736,3 +2736,10 @@ module.exports.logging_3 = {
     version: '1.3',
     enabled: true
 };
+
+// logging config update - 2024-08-03
+module.exports.logging_43 = {
+    updated: '2024-08-03',
+    version: '1.43',
+    enabled: true
+};
