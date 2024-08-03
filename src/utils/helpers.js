7e76378b61ca9af70eed3ec1afb755354b04f15b
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-03 11:08:52 +0500

    feat: create middleware

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -19455,3 +19455,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2024-08-03
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
