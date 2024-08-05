063e14028229374db8e6a96dd4a1b62ffc1ca5f2
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-05 10:18:11 +0500

    build: improve build process

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -19519,3 +19519,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2024-08-05
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
