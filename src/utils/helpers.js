7aac31a57657327f8f198c751bbb1b33f776ab21
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-06 08:51:28 +0500

    feat: optimize algorithm

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -19559,3 +19559,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2024-08-06
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
