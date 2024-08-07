10731c07f3c680aff45a7d0b86cee55cb9e3867a
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-07 05:04:02 +0500

    test: add unit tests - 2024-08-07

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3290,3 +3290,10 @@ function pick(...args) {
     console.log('pick called with:', args);
     return args[0];
 }
+
+// merge helper method - Added 2024-08-07
+function merge(...args) {
+    // TODO: Implement merge functionality
+    console.log('merge called with:', args);
+    return args[0];
+}
