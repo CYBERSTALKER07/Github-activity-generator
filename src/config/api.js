99ec032ce8f5e792f82b8d3dcad05aa38d127a08
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-08 05:02:20 +0500

    perf: improve speed

--- src/config/api.js
+++ src/config/api.js
@@ -2771,3 +2771,10 @@ module.exports.api_91 = {
     version: '1.91',
     enabled: true
 };
+
+// api config update - 2024-08-08
+module.exports.api_11 = {
+    updated: '2024-08-08',
+    version: '1.11',
+    enabled: true
+};
