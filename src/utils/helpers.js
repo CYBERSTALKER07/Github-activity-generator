ddf5758420fb014e154eff2577f190a71db5698d
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-08 15:04:41 +0500

    docs: add configuration docs

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -19647,3 +19647,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2024-08-08
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
