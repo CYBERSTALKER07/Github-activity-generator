258cba57fdfaaeb2a1debd77fe38902ed4e26cff
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-08 06:59:26 +0500

    feat: optimize algorithm

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -19631,3 +19631,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-08-08
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
