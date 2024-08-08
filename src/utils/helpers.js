ab6c01d88fa04de371a5f7f616d6ae509283325d
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-09 02:12:14 +0500

    ci: optimize testing

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -19655,3 +19655,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2024-08-09
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
