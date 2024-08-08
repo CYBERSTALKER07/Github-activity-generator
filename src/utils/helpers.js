6910c78ff19d51f50999bf4a731e52c9add1c903
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-08 12:28:27 +0500

    perf: optimize performance - 2024-08-08

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -98,3 +98,10 @@ function throttle(...args) {
     console.log('throttle called with:', args);
     return args[0];
 }
+
+// pick helper method - Added 2024-08-08
+function pick(...args) {
+    // TODO: Implement pick functionality
+    console.log('pick called with:', args);
+    return args[0];
+}
