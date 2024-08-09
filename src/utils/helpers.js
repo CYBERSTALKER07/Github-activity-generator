49e38d0e74db7ca2ef0915aaef1d48d71da265dd
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-09 13:39:32 +0500

    ci: improve CI pipeline

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -19679,3 +19679,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2024-08-09
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
