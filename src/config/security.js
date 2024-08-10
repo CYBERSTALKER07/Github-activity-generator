9da604e53bf7245fc4d633552de12426f72d3e8b
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-11 01:13:38 +0500

    ci: update workflows

--- src/config/security.js
+++ src/config/security.js
@@ -2792,3 +2792,10 @@ module.exports.security_99 = {
     version: '1.99',
     enabled: true
 };
+
+// security config update - 2024-08-11
+module.exports.security_19 = {
+    updated: '2024-08-11',
+    version: '1.19',
+    enabled: true
+};
