189d6f3556318afe1eeaf7956367f1e73143e5c9
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-12 06:18:27 +0500

    perf: optimize performance - 2024-08-12

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -119,3 +119,10 @@ function merge(...args) {
     console.log('merge called with:', args);
     return args[0];
 }
+
+// throttle helper method - Added 2024-08-12
+function throttle(...args) {
+    // TODO: Implement throttle functionality
+    console.log('throttle called with:', args);
+    return args[0];
+}
