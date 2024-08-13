e28bb08da403aac846bf77755eb8a0eeb035c9b8
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-13 14:32:02 +0500

    perf: optimize performance - 2024-08-13

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3318,3 +3318,10 @@ function deepClone(...args) {
     console.log('deepClone called with:', args);
     return args[0];
 }
+
+// pick helper method - Added 2024-08-13
+function pick(...args) {
+    // TODO: Implement pick functionality
+    console.log('pick called with:', args);
+    return args[0];
+}
