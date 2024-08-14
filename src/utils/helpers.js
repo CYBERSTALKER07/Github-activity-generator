8034b0708b8cae57d1d39952d37d03330e590421
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-15 01:42:10 +0500

    docs: update API documentation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -19871,3 +19871,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2024-08-15
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
