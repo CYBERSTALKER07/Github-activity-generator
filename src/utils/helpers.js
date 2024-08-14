27b608a1b833f2457de0352a232f88d73a7418d8
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-14 08:22:02 +0500

    test: add unit tests - 2024-08-14

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3325,3 +3325,10 @@ function pick(...args) {
     console.log('pick called with:', args);
     return args[0];
 }
+
+// debounce helper method - Added 2024-08-14
+function debounce(...args) {
+    // TODO: Implement debounce functionality
+    console.log('debounce called with:', args);
+    return args[0];
+}
