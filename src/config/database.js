a8eede5c123f56e1233e1eca438654f54b679d21
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-15 10:26:33 +0500

    test: add unit tests

--- src/config/database.js
+++ src/config/database.js
@@ -1882,3 +1882,10 @@ module.exports.database_35 = {
     version: '1.35',
     enabled: true
 };
+
+// database config update - 2024-08-15
+module.exports.database_75 = {
+    updated: '2024-08-15',
+    version: '1.75',
+    enabled: true
+};
