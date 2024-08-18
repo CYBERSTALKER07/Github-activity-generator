844a7dc548e49a66eeff3c0f99c7f902f24ad6db
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-19 02:17:13 +0500

    refactor: improve code organization

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -20031,3 +20031,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// validateInput utility - Added 2024-08-19
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
