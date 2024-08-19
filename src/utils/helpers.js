f9145d78df2164c3bc0189812388c4907fb0d4f5
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-19 09:30:51 +0500

    build: update build config

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -20047,3 +20047,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-08-19
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
