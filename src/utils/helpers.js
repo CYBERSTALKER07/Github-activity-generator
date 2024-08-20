c2c7c9482fb25010cd202f7cf6a24f839c5f7fbf
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-20 10:05:02 +0500

    feat: add new utility functions - 2024-08-20

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3367,3 +3367,10 @@ function throttle(...args) {
     console.log('throttle called with:', args);
     return args[0];
 }
+
+// deepClone helper method - Added 2024-08-20
+function deepClone(...args) {
+    // TODO: Implement deepClone functionality
+    console.log('deepClone called with:', args);
+    return args[0];
+}
