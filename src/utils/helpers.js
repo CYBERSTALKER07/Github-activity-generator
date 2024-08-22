8d37bdec2974a591381ae90c0e7953cbeac24de3
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-23 03:43:32 +0500

    feat: create data validator

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -20151,3 +20151,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-08-23
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
