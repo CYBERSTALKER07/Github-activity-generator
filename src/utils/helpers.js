c8f530da377222ef46be2ca585d73778de1035c3
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-24 11:18:54 +0500

    style: improve code formatting

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -20207,3 +20207,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-08-24
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
