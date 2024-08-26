4253b9c1cb9ccd6e7353b39773e47e721735637e
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-26 12:17:02 +0500

    build: update build configuration - 2024-08-26

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3381,3 +3381,10 @@ function debounce(...args) {
     console.log('debounce called with:', args);
     return args[0];
 }
+
+// pick helper method - Added 2024-08-26
+function pick(...args) {
+    // TODO: Implement pick functionality
+    console.log('pick called with:', args);
+    return args[0];
+}
