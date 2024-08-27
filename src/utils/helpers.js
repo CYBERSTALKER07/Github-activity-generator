d0a6f0580d3658d3fc572af874fddc1a962a818c
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-27 05:39:02 +0500

    ci: improve continuous integration - 2024-08-27

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3388,3 +3388,10 @@ function pick(...args) {
     console.log('pick called with:', args);
     return args[0];
 }
+
+// debounce helper method - Added 2024-08-27
+function debounce(...args) {
+    // TODO: Implement debounce functionality
+    console.log('debounce called with:', args);
+    return args[0];
+}
