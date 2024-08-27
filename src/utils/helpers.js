838557b8c6128225152a9480847ae1f01060b95b
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-27 08:30:30 +0500

    fix: fix compatibility issue

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -20311,3 +20311,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-08-27
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
