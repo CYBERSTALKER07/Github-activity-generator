a6ba617bd425f3a63d3437cab8adeabff5331561
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-30 09:41:51 +0500

    perf: reduce memory usage

--- src/config/logging.js
+++ src/config/logging.js
@@ -2939,3 +2939,10 @@ module.exports.logging_83 = {
     version: '1.83',
     enabled: true
 };
+
+// logging config update - 2024-08-30
+module.exports.logging_3 = {
+    updated: '2024-08-30',
+    version: '1.3',
+    enabled: true
+};
