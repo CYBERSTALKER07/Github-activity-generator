562208bb610bfbcdbc52242662ddfd3b165b11a4
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-01 03:00:44 +0500

    ci: enhance automation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -20511,3 +20511,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2024-09-01
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
