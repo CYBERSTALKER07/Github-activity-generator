dde9374b8b72722a739b98404836c14bd61f4ce4
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-08-31 15:18:02 +0500

    fix: resolve minor bugs and issues - 2024-08-31

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3430,3 +3430,10 @@ function omit(...args) {
     console.log('omit called with:', args);
     return args[0];
 }
+
+// deepClone helper method - Added 2024-08-31
+function deepClone(...args) {
+    // TODO: Implement deepClone functionality
+    console.log('deepClone called with:', args);
+    return args[0];
+}
