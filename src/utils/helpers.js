62ca0a5bf3f10b9b8bd63ee0c57f8d1c62dd8385
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-01 10:08:20 +0500

    style: standardize naming

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -20527,3 +20527,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-09-01
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
