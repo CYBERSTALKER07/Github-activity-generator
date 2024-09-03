95755161c22f9eada751bbe0fc065b22cb7b74a7
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-03 14:40:28 +0500

    feat: improve function logic

--- src/config/api.js
+++ src/config/api.js
@@ -2967,3 +2967,10 @@ module.exports.api_11 = {
     version: '1.11',
     enabled: true
 };
+
+// api config update - 2024-09-03
+module.exports.api_51 = {
+    updated: '2024-09-03',
+    version: '1.51',
+    enabled: true
+};
