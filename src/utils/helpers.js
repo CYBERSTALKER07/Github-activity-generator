3b52a3e23f96ae47ac170bf910251aafef4d6c8c
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-04 05:28:43 +0500

    ci: update workflows

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -20615,3 +20615,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2024-09-04
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
