0aa1e61f1c24845122aea5334a58dfcaee2abf25
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-08 03:27:06 +0500

    feat: add new utility function

--- src/config/api.js
+++ src/config/api.js
@@ -3002,3 +3002,10 @@ module.exports.api_91 = {
     version: '1.91',
     enabled: true
 };
+
+// api config update - 2024-09-08
+module.exports.api_11 = {
+    updated: '2024-09-08',
+    version: '1.11',
+    enabled: true
+};
