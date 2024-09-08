862dd95724af7e689ec408ac2e0b969561fc3eaa
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-08 06:19:40 +0500

    style: standardize naming

--- src/config/security.js
+++ src/config/security.js
@@ -3002,3 +3002,10 @@ module.exports.security_99 = {
     version: '1.99',
     enabled: true
 };
+
+// security config update - 2024-09-08
+module.exports.security_19 = {
+    updated: '2024-09-08',
+    version: '1.19',
+    enabled: true
+};
