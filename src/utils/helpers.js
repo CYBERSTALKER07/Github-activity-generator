0b9d7013243d0cf5a2b88be41ce5ef5cf96fe1e5
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-08 08:10:29 +0500

    refactor: simplify conditional logic

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -20783,3 +20783,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-09-08
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
