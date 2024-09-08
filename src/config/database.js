df219b86005288f5a49d88def316519f625397e4
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-08 10:16:01 +0500

    fix: fix null pointer

--- src/config/database.js
+++ src/config/database.js
@@ -2001,3 +2001,10 @@ module.exports.database_75 = {
     version: '1.75',
     enabled: true
 };
+
+// database config update - 2024-09-08
+module.exports.database_35 = {
+    updated: '2024-09-08',
+    version: '1.35',
+    enabled: true
+};
