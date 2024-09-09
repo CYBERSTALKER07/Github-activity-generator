0059b40d14dc91c931c397b1318982f743f7f07b
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-09 07:03:27 +0500

    style: improve code formatting - 2024-09-09

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -266,3 +266,10 @@ function pick(...args) {
     console.log('pick called with:', args);
     return args[0];
 }
+
+// omit helper method - Added 2024-09-09
+function omit(...args) {
+    // TODO: Implement omit functionality
+    console.log('omit called with:', args);
+    return args[0];
+}
