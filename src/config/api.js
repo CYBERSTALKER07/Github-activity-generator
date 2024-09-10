64e79f6df43d4353d7afd73268336f1f46a1ec2f
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-10 06:07:37 +0500

    build: update build config

--- src/config/api.js
+++ src/config/api.js
@@ -3016,3 +3016,10 @@ module.exports.api_51 = {
     version: '1.51',
     enabled: true
 };
+
+// api config update - 2024-09-10
+module.exports.api_91 = {
+    updated: '2024-09-10',
+    version: '1.91',
+    enabled: true
+};
