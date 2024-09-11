69b02c07981432103da10cc6924781b77ebfd6e4
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-11 10:46:26 +0500

    fix: resolve race condition

--- src/config/api.js
+++ src/config/api.js
@@ -3030,3 +3030,10 @@ module.exports.api_11 = {
     version: '1.11',
     enabled: true
 };
+
+// api config update - 2024-09-11
+module.exports.api_51 = {
+    updated: '2024-09-11',
+    version: '1.51',
+    enabled: true
+};
