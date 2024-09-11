f5b5c54a27a9125bf825a157d0815e7d326328c4
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-11 10:22:43 +0500

    perf: improve speed

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -20911,3 +20911,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2024-09-11
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
