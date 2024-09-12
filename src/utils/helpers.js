cf224f20fb5029c5fd69a87f885531a7609c4a2b
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-12 12:35:52 +0500

    test: add unit tests

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -20967,3 +20967,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// validateInput utility - Added 2024-09-12
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
