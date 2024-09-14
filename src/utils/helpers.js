217f14a83f4ae24e731d74e3d724ab1a72bf5348
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-14 17:38:04 +0500

    refactor: improve error handling

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -21047,3 +21047,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-09-14
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
