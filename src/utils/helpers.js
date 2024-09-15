9a2679e9dcdb8aae40a886573047e2627c1490de
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-15 09:30:15 +0500

    style: improve code structure

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -21079,3 +21079,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2024-09-15
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
