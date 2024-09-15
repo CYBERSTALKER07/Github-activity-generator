1f154940238b89d7fe596cbf3edd532ae5829781
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-15 06:39:36 +0500

    fix: correct calculation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -21063,3 +21063,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2024-09-15
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
