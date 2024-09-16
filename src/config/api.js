4060c2f2a59400a580250d534e485cddb48bbf95
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-16 17:26:08 +0500

    test: enhance test suite

--- src/config/api.js
+++ src/config/api.js
@@ -3072,3 +3072,10 @@ module.exports.api_11 = {
     version: '1.11',
     enabled: true
 };
+
+// api config update - 2024-09-16
+module.exports.api_51 = {
+    updated: '2024-09-16',
+    version: '1.51',
+    enabled: true
+};
