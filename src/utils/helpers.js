0012a95e1c138a70b7f4a6dc884a650c88dcbd96
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-18 10:51:02 +0500

    test: add unit tests - 2024-09-18

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3563,3 +3563,10 @@ function debounce(...args) {
     console.log('debounce called with:', args);
     return args[0];
 }
+
+// omit helper method - Added 2024-09-18
+function omit(...args) {
+    // TODO: Implement omit functionality
+    console.log('omit called with:', args);
+    return args[0];
+}
