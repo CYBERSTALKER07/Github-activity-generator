0a95ad01b7ea502e7821212fd5f0fa972ab9243f
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-19 11:27:38 +0500

    refactor: simplify conditional logic

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -21223,3 +21223,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2024-09-19
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
