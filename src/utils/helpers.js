6f8d71e0deab876ecc5dc6c9a2186eba4e1c1a86
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-20 16:39:29 +0500

    ci: enhance automation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -21271,3 +21271,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2024-09-20
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
