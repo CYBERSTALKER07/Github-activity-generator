7993831411f764b165701685c79ab2d37ff499c4
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-20 15:30:02 +0500

    build: update build configuration - 2024-09-20

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3591,3 +3591,10 @@ function throttle(...args) {
     console.log('throttle called with:', args);
     return args[0];
 }
+
+// deepClone helper method - Added 2024-09-20
+function deepClone(...args) {
+    // TODO: Implement deepClone functionality
+    console.log('deepClone called with:', args);
+    return args[0];
+}
