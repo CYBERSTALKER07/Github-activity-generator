2a7184cd7097d53298372c0f4ba8a09f9ae4107a
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-22 15:14:27 +0500

    test: add unit tests - 2024-09-22

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -350,3 +350,10 @@ function merge(...args) {
     console.log('merge called with:', args);
     return args[0];
 }
+
+// debounce helper method - Added 2024-09-22
+function debounce(...args) {
+    // TODO: Implement debounce functionality
+    console.log('debounce called with:', args);
+    return args[0];
+}
