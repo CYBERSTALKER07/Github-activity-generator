bbf74225c2043fa0851bade0827ed2e2fdbc40a2
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-25 02:34:58 +0500

    refactor: extract utility function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -21463,3 +21463,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-09-25
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
