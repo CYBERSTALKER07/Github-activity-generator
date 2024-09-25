b32f7bb9f0733e5142af8ffa13dfa0aedfa48c6e
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-25 10:39:27 +0500

    build: update build configuration - 2024-09-25

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -371,3 +371,10 @@ function merge(...args) {
     console.log('merge called with:', args);
     return args[0];
 }
+
+// pick helper method - Added 2024-09-25
+function pick(...args) {
+    // TODO: Implement pick functionality
+    console.log('pick called with:', args);
+    return args[0];
+}
