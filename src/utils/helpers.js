80a3a0a0c3049947c3b3726b91b833c266685498
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-27 04:36:27 +0500

    perf: optimize performance - 2024-09-27

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -399,3 +399,10 @@ function deepClone(...args) {
     console.log('deepClone called with:', args);
     return args[0];
 }
+
+// merge helper method - Added 2024-09-27
+function merge(...args) {
+    // TODO: Implement merge functionality
+    console.log('merge called with:', args);
+    return args[0];
+}
