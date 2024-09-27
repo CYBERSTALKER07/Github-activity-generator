c20bcf8ffcc889c39c381c7cb4b07e5ec17dd806
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-27 07:23:05 +0500

    fix: correct calculation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -21559,3 +21559,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-09-27
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
