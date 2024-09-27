a4e1d88f25422669f7aafb01ab851b4a357b02c0
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-27 12:32:31 +0500

    refactor: reduce code duplication

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -21567,3 +21567,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-09-27
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
