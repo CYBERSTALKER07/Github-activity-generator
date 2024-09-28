b279906ed2c7ce7e40c1560952bc8edd3e6d0bf4
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-28 11:28:17 +0500

    ci: improve CI pipeline

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -21607,3 +21607,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-09-28
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
