18c311668c792c2fd0c9bd7d8c2eb6a8671bf76e
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-09-29 10:03:01 +0500

    ci: update workflows

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -21631,3 +21631,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2024-09-29
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
