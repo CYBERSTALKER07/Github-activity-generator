87ff052ca5e5e4b8f9ee3a573c0df253f0063af1
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-10-01 09:37:52 +0500

    refactor: enhance code clarity

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -21719,3 +21719,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-10-01
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
