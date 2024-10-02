523999abc3abd59bed342996e7f44f650f3f1f6e
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-10-02 17:31:37 +0500

    fix: resolve race condition

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -21775,3 +21775,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-10-02
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
