2b5f651475d5c74134999697bd5f4af3c8d038a2
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-10-02 05:15:25 +0500

    style: improve code structure

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -21735,3 +21735,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2024-10-02
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
