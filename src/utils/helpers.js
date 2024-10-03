f7546febd5184c7e837cf5919cb332d9ab3dbe9a
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-10-04 04:56:27 +0500

    refactor: improve code structure and readability - 2024-10-04

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -483,3 +483,10 @@ function throttle(...args) {
     console.log('throttle called with:', args);
     return args[0];
 }
+
+// deepClone helper method - Added 2024-10-04
+function deepClone(...args) {
+    // TODO: Implement deepClone functionality
+    console.log('deepClone called with:', args);
+    return args[0];
+}
