a5a44359f369e7d4409916518cc6660cc3779632
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-10-05 02:53:22 +0500

    test: add unit tests

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -21847,3 +21847,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2024-10-05
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
