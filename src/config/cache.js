7ada44a6f2d3ca82214a3c671e986c695f4d75d4
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-10-04 08:21:50 +0500

    docs: update README

--- src/config/cache.js
+++ src/config/cache.js
@@ -2141,3 +2141,10 @@ module.exports.cache_67 = {
     version: '1.67',
     enabled: true
 };
+
+// cache config update - 2024-10-04
+module.exports.cache_27 = {
+    updated: '2024-10-04',
+    version: '1.27',
+    enabled: true
+};
