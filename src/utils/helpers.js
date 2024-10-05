aba87b97f5d23c7424c1e0fd3be9c474c301d4b8
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-10-05 11:49:11 +0500

    refactor: improve method signatures

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -21871,3 +21871,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-10-05
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
