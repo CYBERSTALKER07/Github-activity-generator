65bc44f6442e4855c5583c75da63c5b5f82eac64
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-10-07 13:44:07 +0500

    refactor: improve method signatures

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -21967,3 +21967,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2024-10-07
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
