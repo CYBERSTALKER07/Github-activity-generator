fa4bed789d54bb93d94dc2b23efdf6c809044c8d
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-10-09 05:27:27 +0500

    feat: add new utility functions - 2024-10-09

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -490,3 +490,10 @@ function deepClone(...args) {
     console.log('deepClone called with:', args);
     return args[0];
 }
+
+// deepClone helper method - Added 2024-10-09
+function deepClone(...args) {
+    // TODO: Implement deepClone functionality
+    console.log('deepClone called with:', args);
+    return args[0];
+}
