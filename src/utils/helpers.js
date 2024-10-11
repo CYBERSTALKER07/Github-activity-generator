8530a71a5520228f5eb7364059dcd3d4ce0e7576
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-10-11 05:39:29 +0500

    build: update dependencies

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -22119,3 +22119,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2024-10-11
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
