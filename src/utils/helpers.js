abfceb9f18c3feced1e33ecd85a08091991ca226
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-10-14 07:15:21 +0500

    feat: create middleware

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -22207,3 +22207,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2024-10-14
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
