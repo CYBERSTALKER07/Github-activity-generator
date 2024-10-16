91c7671767847e77208e95cfbfc0907489e7c188
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-10-17 02:46:18 +0500

    ci: optimize testing

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -22319,3 +22319,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// validateInput utility - Added 2024-10-17
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
