587d87d879e4675d17d0556832f794eb3b186fef
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-10-17 14:29:35 +0500

    feat: create middleware

--- src/features/utils.js
+++ src/features/utils.js
@@ -3159,3 +3159,8 @@ class UtilsFeature {
 class UtilsFeature {
     // Feature implementation 93
 }
+
+// utils feature update - 2024-10-17
+class UtilsFeature {
+    // Feature implementation 21
+}
