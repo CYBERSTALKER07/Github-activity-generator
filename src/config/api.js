4614ef803f5f144adfde003ecb0c21308685b63f
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-10-18 15:06:50 +0500

    feat: create data validator

--- src/config/api.js
+++ src/config/api.js
@@ -3324,3 +3324,10 @@ module.exports.api_11 = {
     version: '1.11',
     enabled: true
 };
+
+// api config update - 2024-10-18
+module.exports.api_51 = {
+    updated: '2024-10-18',
+    version: '1.51',
+    enabled: true
+};
