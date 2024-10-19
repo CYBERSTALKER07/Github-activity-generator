a6c029704842cd7b87dacb968729b1ee85412cc9
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-10-20 01:11:04 +0500

    feat: add type checking

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -22415,3 +22415,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2024-10-20
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
