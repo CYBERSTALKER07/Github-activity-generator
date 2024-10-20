a6647a5e3738fd1c8d4dfda5c6e00b62f610fd45
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-10-21 02:22:32 +0500

    feat: enhance performance

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -22471,3 +22471,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2024-10-21
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
