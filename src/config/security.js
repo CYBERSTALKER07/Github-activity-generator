8631c1273ea6e86d7304f9b31536bf336eec4522
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-10-20 08:04:16 +0500

    docs: update changelog

--- src/config/security.js
+++ src/config/security.js
@@ -3338,3 +3338,10 @@ module.exports.security_99 = {
     version: '1.99',
     enabled: true
 };
+
+// security config update - 2024-10-20
+module.exports.security_19 = {
+    updated: '2024-10-20',
+    version: '1.19',
+    enabled: true
+};
