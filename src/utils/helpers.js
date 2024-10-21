3c39e86a935c9752047a16b3a3c658281d074253
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-10-21 08:46:02 +0500

    perf: optimize performance - 2024-10-21

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3738,3 +3738,10 @@ function debounce(...args) {
     console.log('debounce called with:', args);
     return args[0];
 }
+
+// throttle helper method - Added 2024-10-21
+function throttle(...args) {
+    // TODO: Implement throttle functionality
+    console.log('throttle called with:', args);
+    return args[0];
+}
