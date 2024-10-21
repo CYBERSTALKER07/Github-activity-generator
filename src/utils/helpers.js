e496c91fa4929afb34af007852e6d3e273327b2f
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-10-21 06:15:24 +0500

    refactor: improve code organization

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -22479,3 +22479,11 @@ function filterResults(input) {
 }
 
 module.exports.filterResults = filterResults;
+
+// validateInput utility - Added 2024-10-21
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
