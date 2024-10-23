79ca7093825bedd573374189ba21da86a5e40dca
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-10-23 09:19:14 +0500

    refactor: improve code organization

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -22567,3 +22567,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2024-10-23
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
