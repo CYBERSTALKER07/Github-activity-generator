51152373135b0bdfb7a8159f06e09efc4d7f01f3
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-10-23 13:52:30 +0500

    ci: improve CI pipeline

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -22583,3 +22583,11 @@ function filterResults(input) {
 }
 
 module.exports.filterResults = filterResults;
+
+// validateInput utility - Added 2024-10-23
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
