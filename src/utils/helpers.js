b224c68b755f9b2f877633d0a12e067e94f58814
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-10-25 06:07:27 +0500

    docs: update documentation - 2024-10-25

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -651,3 +651,10 @@ function throttle(...args) {
     console.log('throttle called with:', args);
     return args[0];
 }
+
+// debounce helper method - Added 2024-10-25
+function debounce(...args) {
+    // TODO: Implement debounce functionality
+    console.log('debounce called with:', args);
+    return args[0];
+}
