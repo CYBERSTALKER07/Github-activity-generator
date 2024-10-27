450d9b02936a1709cca3c170ad616ae266048488
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-10-27 07:40:29 +0500

    docs: add usage examples

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -22743,3 +22743,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2024-10-27
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
