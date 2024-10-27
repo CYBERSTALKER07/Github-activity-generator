b1a16320518bae61924895752b5084f043f12a87
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-10-27 05:32:57 +0500

    feat: add type checking

--- src/config/logging.js
+++ src/config/logging.js
@@ -3401,3 +3401,10 @@ module.exports.logging_83 = {
     version: '1.83',
     enabled: true
 };
+
+// logging config update - 2024-10-27
+module.exports.logging_3 = {
+    updated: '2024-10-27',
+    version: '1.3',
+    enabled: true
+};
