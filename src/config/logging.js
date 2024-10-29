b89fcd052f1be32c449144b11008d6c285a12c73
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-10-29 07:02:16 +0500

    feat: add error handler

--- src/config/logging.js
+++ src/config/logging.js
@@ -3415,3 +3415,10 @@ module.exports.logging_43 = {
     version: '1.43',
     enabled: true
 };
+
+// logging config update - 2024-10-29
+module.exports.logging_83 = {
+    updated: '2024-10-29',
+    version: '1.83',
+    enabled: true
+};
