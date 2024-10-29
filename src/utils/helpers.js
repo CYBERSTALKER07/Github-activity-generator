ec21be40d78543d1bb6e616daa9b7dfccf2ceff5
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-10-30 01:59:28 +0500

    style: format JSON files

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -22847,3 +22847,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2024-10-30
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
