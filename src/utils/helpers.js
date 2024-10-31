3107d67d4f4624af3a63d58d2dc07d7651ea326d
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-10-31 07:30:04 +0500

    fix: correct calculation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -22903,3 +22903,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2024-10-31
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
