fe39b74074a6e2d51f59a12b28789b5efa6adcdc
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-11-01 10:10:00 +0500

    chore: organize project

--- src/config/security.js
+++ src/config/security.js
@@ -3443,3 +3443,10 @@ module.exports.security_99 = {
     version: '1.99',
     enabled: true
 };
+
+// security config update - 2024-11-01
+module.exports.security_19 = {
+    updated: '2024-11-01',
+    version: '1.19',
+    enabled: true
+};
