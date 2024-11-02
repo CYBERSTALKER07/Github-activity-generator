088459241570fc1446ae249fe095151262720031
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-11-02 05:44:48 +0500

    docs: improve code comments

--- src/config/api.js
+++ src/config/api.js
@@ -3450,3 +3450,10 @@ module.exports.api_11 = {
     version: '1.11',
     enabled: true
 };
+
+// api config update - 2024-11-02
+module.exports.api_51 = {
+    updated: '2024-11-02',
+    version: '1.51',
+    enabled: true
+};
