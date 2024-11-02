54ade8321752aaacaf83a569b867274b833c6c03
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-11-02 05:38:01 +0500

    fix: fix validation logic

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -22991,3 +22991,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2024-11-02
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
