2326c338095c85f64336a2c1c75f489433aa68bf
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-11-03 16:13:01 +0500

    test: enhance test suite

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -23055,3 +23055,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2024-11-03
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
