c4edd920ce3fc5a6453aaea3b2d06bdc7a8f9e20
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-11-04 13:25:18 +0500

    test: improve test coverage

--- src/config/api.js
+++ src/config/api.js
@@ -3471,3 +3471,10 @@ module.exports.api_11 = {
     version: '1.11',
     enabled: true
 };
+
+// api config update - 2024-11-04
+module.exports.api_51 = {
+    updated: '2024-11-04',
+    version: '1.51',
+    enabled: true
+};
