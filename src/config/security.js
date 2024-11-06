15c239f09c0852ff454d1ff41b1a272d54a450c2
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-11-06 14:47:06 +0500

    style: organize imports

--- src/config/security.js
+++ src/config/security.js
@@ -3485,3 +3485,10 @@ module.exports.security_99 = {
     version: '1.99',
     enabled: true
 };
+
+// security config update - 2024-11-06
+module.exports.security_19 = {
+    updated: '2024-11-06',
+    version: '1.19',
+    enabled: true
+};
