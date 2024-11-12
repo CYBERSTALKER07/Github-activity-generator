318ca743ea8207306192c4c22f3bd700d828f0fb
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-11-12 15:43:27 +0500

    style: improve code formatting - 2024-11-12

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -819,3 +819,10 @@ function deepClone(...args) {
     console.log('deepClone called with:', args);
     return args[0];
 }
+
+// pick helper method - Added 2024-11-12
+function pick(...args) {
+    // TODO: Implement pick functionality
+    console.log('pick called with:', args);
+    return args[0];
+}
