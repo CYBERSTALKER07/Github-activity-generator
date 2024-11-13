41594019e38f0983bc7f2e1269baeb05fe2c26cb
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-11-13 08:49:02 +0500

    feat: add new utility functions - 2024-11-13

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3941,3 +3941,10 @@ function debounce(...args) {
     console.log('debounce called with:', args);
     return args[0];
 }
+
+// omit helper method - Added 2024-11-13
+function omit(...args) {
+    // TODO: Implement omit functionality
+    console.log('omit called with:', args);
+    return args[0];
+}
