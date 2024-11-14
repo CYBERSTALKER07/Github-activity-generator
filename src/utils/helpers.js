39bc558e2e0c135ba39afe362deb601230660b7b
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-11-14 13:45:14 +0500

    refactor: streamline workflow

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -23423,3 +23423,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2024-11-14
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
