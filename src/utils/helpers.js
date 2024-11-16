073670756c8daf879246880148fc3f3ed8db8278
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-11-16 10:28:43 +0500

    refactor: reduce code duplication

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -23495,3 +23495,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2024-11-16
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
