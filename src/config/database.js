417557a7a9828cd0ca569a9efbfca122a12b5538
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-11-17 11:22:09 +0500

    feat: create middleware

--- src/config/database.js
+++ src/config/database.js
@@ -2372,3 +2372,10 @@ module.exports.database_35 = {
     version: '1.35',
     enabled: true
 };
+
+// database config update - 2024-11-17
+module.exports.database_75 = {
+    updated: '2024-11-17',
+    version: '1.75',
+    enabled: true
+};
