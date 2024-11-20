38436cfa7d283021c6d04a5b80cfa1fe33de0e17
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-11-20 06:29:12 +0500

    refactor: improve code organization

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -23647,3 +23647,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-11-20
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
