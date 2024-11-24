0db58c4f5da1f8fc68d409812fdfd754c4a72776
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-11-24 05:04:22 +0500

    chore: organize project

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -23791,3 +23791,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-11-24
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
