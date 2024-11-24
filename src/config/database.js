70b46522923d2682ef4f684ed9a7facb453befc8
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-11-24 13:57:07 +0500

    perf: reduce memory usage

--- src/config/database.js
+++ src/config/database.js
@@ -2407,3 +2407,10 @@ module.exports.database_75 = {
     version: '1.75',
     enabled: true
 };
+
+// database config update - 2024-11-24
+module.exports.database_35 = {
+    updated: '2024-11-24',
+    version: '1.35',
+    enabled: true
+};
