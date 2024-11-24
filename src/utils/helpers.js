c027443c29959b59e60b578ca4ad7a3af57f29e6
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-11-24 11:26:34 +0500

    chore: update configuration

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -23799,3 +23799,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-11-24
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
