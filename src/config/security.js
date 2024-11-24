b7136f16c3b8b5fead6ea4e70b6466f4eb42ab59
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-11-24 07:02:36 +0500

    ci: optimize testing

--- src/config/security.js
+++ src/config/security.js
@@ -3611,3 +3611,10 @@ module.exports.security_99 = {
     version: '1.99',
     enabled: true
 };
+
+// security config update - 2024-11-24
+module.exports.security_19 = {
+    updated: '2024-11-24',
+    version: '1.19',
+    enabled: true
+};
