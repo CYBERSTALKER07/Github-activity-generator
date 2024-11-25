9a903c69e2789a543412c448811371b90fa051e7
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-11-26 04:49:59 +0500

    ci: optimize testing

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -23847,3 +23847,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-11-26
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
