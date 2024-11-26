bc7d45c0badb5cc4e0ad3c9168be5fcbc0ee75c9
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-11-26 12:57:09 +0500

    feat: implement helper method

--- src/features/utils.js
+++ src/features/utils.js
@@ -3454,3 +3454,8 @@ class UtilsFeature {
 class UtilsFeature {
     // Feature implementation 69
 }
+
+// utils feature update - 2024-11-26
+class UtilsFeature {
+    // Feature implementation 93
+}
