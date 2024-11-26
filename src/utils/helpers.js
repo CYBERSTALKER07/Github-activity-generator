c31cb1e9072dbb0a2d5341af883ef15efad9cdae
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-11-26 07:52:27 +0500

    docs: update documentation - 2024-11-26

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -966,3 +966,10 @@ function pick(...args) {
     console.log('pick called with:', args);
     return args[0];
 }
+
+// pick helper method - Added 2024-11-26
+function pick(...args) {
+    // TODO: Implement pick functionality
+    console.log('pick called with:', args);
+    return args[0];
+}
