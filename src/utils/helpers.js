8264257d92a1bf9afffab60026901d1dc7844a5a
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-11-29 06:01:02 +0500

    fix: resolve minor bugs and issues - 2024-11-29

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4046,3 +4046,10 @@ function deepClone(...args) {
     console.log('deepClone called with:', args);
     return args[0];
 }
+
+// pick helper method - Added 2024-11-29
+function pick(...args) {
+    // TODO: Implement pick functionality
+    console.log('pick called with:', args);
+    return args[0];
+}
