6f3ee1a1d77df116d78160b07eaad260212028e7
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-12-01 13:23:53 +0500

    style: improve readability

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -24063,3 +24063,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-12-01
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
