2624327794960fd9da3cf1a59c2ef3e08ab09ad4
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-12-03 12:50:53 +0500

    refactor: streamline workflow

--- src/config/api.js
+++ src/config/api.js
@@ -3681,3 +3681,10 @@ module.exports.api_11 = {
     version: '1.11',
     enabled: true
 };
+
+// api config update - 2024-12-03
+module.exports.api_51 = {
+    updated: '2024-12-03',
+    version: '1.51',
+    enabled: true
+};
