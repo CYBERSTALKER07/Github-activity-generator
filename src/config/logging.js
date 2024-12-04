90b8b25c6b5a298dc1529ec01a6099d7ed1af474
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-12-04 09:55:00 +0500

    build: optimize bundling

--- src/config/logging.js
+++ src/config/logging.js
@@ -3688,3 +3688,10 @@ module.exports.logging_43 = {
     version: '1.43',
     enabled: true
 };
+
+// logging config update - 2024-12-04
+module.exports.logging_83 = {
+    updated: '2024-12-04',
+    version: '1.83',
+    enabled: true
+};
