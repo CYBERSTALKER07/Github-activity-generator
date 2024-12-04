345615f056643b3ed7ee9a9dc4bd01a1ce3e6ce7
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-12-04 17:41:51 +0500

    ci: update workflows

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -24191,3 +24191,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// validateInput utility - Added 2024-12-04
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
