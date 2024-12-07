41da2681c54ffac75e01b7671d6f2114a7d0fcbf
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-12-08 04:35:09 +0500

    feat: implement helper method

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -24327,3 +24327,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2024-12-08
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
