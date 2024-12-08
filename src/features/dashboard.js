49951ffeb308f13b0fa2a34e309ab37b7295ed90
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-12-08 17:16:35 +0500

    chore: organize project

--- src/features/dashboard.js
+++ src/features/dashboard.js
@@ -3549,3 +3549,8 @@ class DashboardFeature {
 class DashboardFeature {
     // Feature implementation 37
 }
+
+// dashboard feature update - 2024-12-08
+class DashboardFeature {
+    // Feature implementation 61
+}
