c48e1ef9846618ed3333e7e0a5df2f7f1526a666
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-12-09 04:26:50 +0500

    refactor: improve code organization

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -24367,3 +24367,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-12-09
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
