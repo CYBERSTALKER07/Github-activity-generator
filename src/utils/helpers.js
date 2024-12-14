7d957b03ecedc555eb14ddee6aaaf7f948ba0a0b
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-12-14 15:30:27 +0500

    style: improve code formatting - 2024-12-14

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1078,3 +1078,10 @@ function pick(...args) {
     console.log('pick called with:', args);
     return args[0];
 }
+
+// throttle helper method - Added 2024-12-14
+function throttle(...args) {
+    // TODO: Implement throttle functionality
+    console.log('throttle called with:', args);
+    return args[0];
+}
