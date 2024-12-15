0d59e43a8e1c0421258fe2a3715761d869c0a486
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-12-15 10:53:43 +0500

    fix: fix async operation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -24575,3 +24575,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-12-15
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
