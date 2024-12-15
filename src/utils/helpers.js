fdb7b9ad9799781eafe55be98014b267e7baf7ba
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-12-15 05:36:46 +0500

    chore: clean up files

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -24559,3 +24559,11 @@ function filterResults(input) {
 }
 
 module.exports.filterResults = filterResults;
+
+// validateInput utility - Added 2024-12-15
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
