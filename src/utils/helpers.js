60d7636d8d43aa23fd66f2cb9c3c4f1f60e4bb9a
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-12-17 03:51:26 +0500

    docs: add troubleshooting guide

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -24647,3 +24647,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2024-12-17
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
