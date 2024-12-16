639b5ccf1443113a89c6e06130208ca03de1834e
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-12-16 09:11:30 +0500

    docs: update README

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -24615,3 +24615,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2024-12-16
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
