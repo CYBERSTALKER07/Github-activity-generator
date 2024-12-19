a5b073fe6e8f9cd82e15c9da70b10f85af30a507
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-12-19 06:25:27 +0500

    style: improve code formatting - 2024-12-19

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1148,3 +1148,10 @@ function debounce(...args) {
     console.log('debounce called with:', args);
     return args[0];
 }
+
+// merge helper method - Added 2024-12-19
+function merge(...args) {
+    // TODO: Implement merge functionality
+    console.log('merge called with:', args);
+    return args[0];
+}
