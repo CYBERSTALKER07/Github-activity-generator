15877700eda6ea30fe55b779927a4e8791a2b6ca
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-12-19 10:16:02 +0500

    fix: resolve minor bugs and issues - 2024-12-19

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4235,3 +4235,10 @@ function merge(...args) {
     console.log('merge called with:', args);
     return args[0];
 }
+
+// omit helper method - Added 2024-12-19
+function omit(...args) {
+    // TODO: Implement omit functionality
+    console.log('omit called with:', args);
+    return args[0];
+}
