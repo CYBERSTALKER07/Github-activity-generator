3f8b9a56a908b38884c32a59f01b5be88df61bb1
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-12-21 03:57:57 +0500

    fix: resolve race condition

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -24823,3 +24823,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2024-12-21
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
