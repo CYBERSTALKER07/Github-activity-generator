dbfb23566361eb917a4c4749a1241f6f863fc5ae
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-12-20 07:29:57 +0500

    style: clean up whitespace

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -24791,3 +24791,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-12-20
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
