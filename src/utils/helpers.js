81a5c8b62facfbd507759beba9d1c953be3cb341
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-12-21 08:51:56 +0500

    test: enhance test suite

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -24839,3 +24839,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2024-12-21
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
