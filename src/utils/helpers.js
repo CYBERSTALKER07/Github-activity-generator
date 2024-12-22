5ec423950be29dbe4385d5f1bcab80238d2cedc0
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-12-23 03:11:32 +0500

    chore: clean up files

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -24863,3 +24863,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2024-12-23
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
