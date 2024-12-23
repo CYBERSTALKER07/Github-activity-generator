40782faaaffa88a4050760c90028cde777e4fc33
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-12-23 15:20:15 +0500

    perf: reduce memory usage

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -24887,3 +24887,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-12-23
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
