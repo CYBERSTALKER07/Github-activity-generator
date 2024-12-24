8d1b9132c5e3043538460fa69cda65eea9d75c2e
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-12-25 04:14:02 +0500

    fix: resolve minor bugs and issues - 2024-12-25

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4277,3 +4277,10 @@ function debounce(...args) {
     console.log('debounce called with:', args);
     return args[0];
 }
+
+// pick helper method - Added 2024-12-25
+function pick(...args) {
+    // TODO: Implement pick functionality
+    console.log('pick called with:', args);
+    return args[0];
+}
