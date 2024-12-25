f7e678a6a6dd1d59f4832407d7628c3555561211
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-12-26 03:32:08 +0500

    feat: add new utility function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -24967,3 +24967,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2024-12-26
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
