cc14cc38da70e79eb6d35e157838639e0a798358
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-12-26 17:34:35 +0500

    feat: add type checking

--- src/config/database.js
+++ src/config/database.js
@@ -2568,3 +2568,10 @@ module.exports.database_35 = {
     version: '1.35',
     enabled: true
 };
+
+// database config update - 2024-12-26
+module.exports.database_75 = {
+    updated: '2024-12-26',
+    version: '1.75',
+    enabled: true
+};
