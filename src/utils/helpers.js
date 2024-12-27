40d4c9c1677cd94743e8d83509e7dc3256943a8b
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-12-27 16:12:16 +0500

    refactor: optimize data structures

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -25039,3 +25039,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2024-12-27
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
