a3af9bad2bf121dbdc731bfd90334959ce838a44
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-12-28 14:38:02 +0500

    perf: optimize performance - 2024-12-28

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4326,3 +4326,10 @@ function deepClone(...args) {
     console.log('deepClone called with:', args);
     return args[0];
 }
+
+// throttle helper method - Added 2024-12-28
+function throttle(...args) {
+    // TODO: Implement throttle functionality
+    console.log('throttle called with:', args);
+    return args[0];
+}
