2cdfcb2d722663062d9c755ee9fb37f38351ffb0
GitHub Contributor <shsoliyev@aut-edu.uz>
2024-12-31 07:33:34 +0500

    chore: clean up files

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -25167,3 +25167,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2024-12-31
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
