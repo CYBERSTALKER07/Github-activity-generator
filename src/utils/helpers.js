05354dccf139f2d70452c56a996a101d56deda1d
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-01 16:17:59 +0500

    feat: create data validator

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -25239,3 +25239,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2025-01-01
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
