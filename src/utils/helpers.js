1acf891613c8116e7908b6344ea9a8e2b2fda10d
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-02 11:43:55 +0500

    test: enhance test suite

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -25271,3 +25271,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2025-01-02
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
