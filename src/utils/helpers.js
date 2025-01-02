1188a140c1b7187d66635154122135cdf919b31a
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-02 06:18:02 +0500

    build: update build configuration - 2025-01-02

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4396,3 +4396,10 @@ function deepClone(...args) {
     console.log('deepClone called with:', args);
     return args[0];
 }
+
+// merge helper method - Added 2025-01-02
+function merge(...args) {
+    // TODO: Implement merge functionality
+    console.log('merge called with:', args);
+    return args[0];
+}
