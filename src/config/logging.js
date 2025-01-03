9bfdb03dc9a6c0f25aaa2f1e389231d4178f85a9
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-03 12:04:07 +0500

    test: improve test coverage

--- src/config/logging.js
+++ src/config/logging.js
@@ -3919,3 +3919,10 @@ module.exports.logging_43 = {
     version: '1.43',
     enabled: true
 };
+
+// logging config update - 2025-01-03
+module.exports.logging_83 = {
+    updated: '2025-01-03',
+    version: '1.83',
+    enabled: true
+};
