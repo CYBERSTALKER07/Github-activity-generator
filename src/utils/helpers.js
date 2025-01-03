397d562ec5119746d717be19e794dc802221328c
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-03 10:50:08 +0500

    fix: correct calculation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -25311,3 +25311,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2025-01-03
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
