99b914613f03bf1f95b5b9d055890c2023444dad
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-03 15:42:08 +0500

    feat: improve function logic

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -25327,3 +25327,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2025-01-03
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
