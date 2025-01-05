9be0883d91ea89342070342f5d959c2becea4830
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-05 13:16:04 +0500

    feat: create middleware

--- src/config/cache.js
+++ src/config/cache.js
@@ -2624,3 +2624,10 @@ module.exports.cache_27 = {
     version: '1.27',
     enabled: true
 };
+
+// cache config update - 2025-01-05
+module.exports.cache_67 = {
+    updated: '2025-01-05',
+    version: '1.67',
+    enabled: true
+};
