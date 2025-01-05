61d2079e062aca2cb17b0291ba170c0f786d2d34
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-05 10:13:15 +0500

    ci: enhance automation

--- src/config/security.js
+++ src/config/security.js
@@ -3933,3 +3933,10 @@ module.exports.security_19 = {
     version: '1.19',
     enabled: true
 };
+
+// security config update - 2025-01-05
+module.exports.security_59 = {
+    updated: '2025-01-05',
+    version: '1.59',
+    enabled: true
+};
