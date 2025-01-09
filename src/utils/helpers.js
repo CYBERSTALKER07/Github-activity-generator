dba33a010d492d51d8bd23ad8b8be8fd50a70f38
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-10 02:11:52 +0500

    feat: add new utility function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -25527,3 +25527,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2025-01-10
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
