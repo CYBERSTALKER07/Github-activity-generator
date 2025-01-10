7abe64913bb9b1acc9cccb4604d970ab3e3cb327
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-11 04:06:24 +0500

    fix: fix validation logic

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -25567,3 +25567,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2025-01-11
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
