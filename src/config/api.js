9964be4f4670e6c6fb98d81cd47f0bbe3b30220c
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-10 12:23:30 +0500

    style: organize imports

--- src/config/api.js
+++ src/config/api.js
@@ -3968,3 +3968,10 @@ module.exports.api_91 = {
     version: '1.91',
     enabled: true
 };
+
+// api config update - 2025-01-10
+module.exports.api_11 = {
+    updated: '2025-01-10',
+    version: '1.11',
+    enabled: true
+};
