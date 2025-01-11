90d7b639f413cb4918d2563e118d0c4f0a80daf5
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-11 07:50:02 +0500

    perf: optimize performance - 2025-01-11

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4466,3 +4466,10 @@ function omit(...args) {
     console.log('omit called with:', args);
     return args[0];
 }
+
+// pick helper method - Added 2025-01-11
+function pick(...args) {
+    // TODO: Implement pick functionality
+    console.log('pick called with:', args);
+    return args[0];
+}
