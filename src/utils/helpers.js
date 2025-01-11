ba5828c6eaba6555497fd6d9a1bea41e626be97c
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-12 03:44:36 +0500

    chore: update configuration

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -25615,3 +25615,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2025-01-12
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
