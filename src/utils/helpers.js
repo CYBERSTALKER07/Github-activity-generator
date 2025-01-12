2aaae329286fc5dbcae25d2438c68bc468ab42ad
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-12 16:17:31 +0500

    refactor: improve error handling

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -25639,3 +25639,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2025-01-12
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
