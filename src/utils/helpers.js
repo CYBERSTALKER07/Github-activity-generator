49316e526977e7a970bbe1c60e9cfb30c35836c2
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-12 17:43:02 +0500

    fix: fix async operation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -25647,3 +25647,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// validateInput utility - Added 2025-01-12
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
