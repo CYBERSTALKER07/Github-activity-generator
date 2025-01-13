6f2fc5457317abbe03a76dc2c3ceb83992389a43
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-13 06:36:27 +0500

    style: improve code formatting - 2025-01-13

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1323,3 +1323,10 @@ function deepClone(...args) {
     console.log('deepClone called with:', args);
     return args[0];
 }
+
+// omit helper method - Added 2025-01-13
+function omit(...args) {
+    // TODO: Implement omit functionality
+    console.log('omit called with:', args);
+    return args[0];
+}
