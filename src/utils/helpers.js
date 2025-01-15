0ea5530097dde4ca76cb6ac908beb775156d514c
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-15 13:21:42 +0500

    refactor: extract utility function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -25751,3 +25751,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// validateInput utility - Added 2025-01-15
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
