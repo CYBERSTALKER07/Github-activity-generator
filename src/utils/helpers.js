f9984617105ae969c1c2e9ded180cbb767fde2af
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-16 08:59:31 +0500

    refactor: optimize data structures

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -25775,3 +25775,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2025-01-16
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
