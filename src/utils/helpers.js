fcd7a7e2fc043b93c7e7a11abef217fda1220796
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-17 05:12:07 +0500

    ci: update workflows

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -25799,3 +25799,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2025-01-17
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
