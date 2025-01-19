001c5a36243019b67703a069ad0162cf97841c5d
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-19 17:33:42 +0500

    fix: fix compatibility issue

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -25895,3 +25895,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2025-01-19
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
