c69d763e54ab7caa7b5054bd4bd662f801eb9b4b
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-22 17:15:44 +0500

    refactor: improve code organization

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -25991,3 +25991,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2025-01-22
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
