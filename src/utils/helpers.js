f9a019b67f427ba65cef74723d812b4087b1f025
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-24 09:03:09 +0500

    fix: resolve edge case

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -25999,3 +25999,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2025-01-24
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
