a87aa2607154a179f7aee439a9ff5baf8d065f58
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-27 04:54:32 +0500

    refactor: streamline workflow

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -26103,3 +26103,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2025-01-27
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
