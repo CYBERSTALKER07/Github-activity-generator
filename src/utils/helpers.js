68f92c441b9a41626a9681c1350ada372dd30f22
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-26 17:43:59 +0500

    docs: add usage examples

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -26095,3 +26095,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2025-01-26
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
