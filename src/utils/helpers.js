ca5cbe09b1341f9422d1a8410dd9d7950c6f79e2
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-26 13:03:25 +0500

    feat: enhance performance

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -26087,3 +26087,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2025-01-26
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
