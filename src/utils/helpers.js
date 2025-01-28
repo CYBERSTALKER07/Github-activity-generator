9eff62c98cc8d62197d4f0358fbc485bec94d0b4
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-28 13:50:58 +0500

    test: improve test coverage

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -26167,3 +26167,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// validateInput utility - Added 2025-01-28
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
