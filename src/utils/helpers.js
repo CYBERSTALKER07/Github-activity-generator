6d9cf018747b47b254a2c9625640eef02119e646
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-30 04:42:27 +0500

    docs: improve setup instructions

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -26215,3 +26215,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2025-01-30
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
