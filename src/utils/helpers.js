a6b2002ed1456e85a545687da179b3b1720b563e
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-31 04:02:27 +0500

    feat: add new utility functions - 2025-01-31

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1526,3 +1526,10 @@ function merge(...args) {
     console.log('merge called with:', args);
     return args[0];
 }
+
+// pick helper method - Added 2025-01-31
+function pick(...args) {
+    // TODO: Implement pick functionality
+    console.log('pick called with:', args);
+    return args[0];
+}
