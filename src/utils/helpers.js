3ddb876aca19edfe722ee786621c061da1c437ba
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-01-31 06:24:29 +0500

    feat: create middleware

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -26255,3 +26255,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2025-01-31
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
