eed95517d78a25651fba2b70d0041029f801cde9
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-02-03 11:39:51 +0500

    test: enhance test suite

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -26343,3 +26343,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2025-02-03
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
