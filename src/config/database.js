de0f20d29569938f73c83e091f7fdd624802f29a
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-02-05 04:00:05 +0500

    test: improve test coverage

--- src/config/database.js
+++ src/config/database.js
@@ -2757,3 +2757,10 @@ module.exports.database_75 = {
     version: '1.75',
     enabled: true
 };
+
+// database config update - 2025-02-05
+module.exports.database_35 = {
+    updated: '2025-02-05',
+    version: '1.35',
+    enabled: true
+};
