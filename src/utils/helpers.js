426b58b358a64eb5bfc1f8659356c8599c0c7b7b
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-02-04 12:02:29 +0500

    style: standardize naming

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -26383,3 +26383,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2025-02-04
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
