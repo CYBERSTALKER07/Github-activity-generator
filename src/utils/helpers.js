9b2aca003d671d7cb371001ffd93910fd423f4c0
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-02-04 08:53:27 +0500

    feat: add new utility functions - 2025-02-04

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1554,3 +1554,10 @@ function deepClone(...args) {
     console.log('deepClone called with:', args);
     return args[0];
 }
+
+// merge helper method - Added 2025-02-04
+function merge(...args) {
+    // TODO: Implement merge functionality
+    console.log('merge called with:', args);
+    return args[0];
+}
