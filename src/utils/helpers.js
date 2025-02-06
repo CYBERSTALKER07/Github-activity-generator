5e1a8054b72d5b016913c8e3d41f52b50702be0c
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-02-06 14:33:23 +0500

    feat: improve function logic

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -26479,3 +26479,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// validateInput utility - Added 2025-02-06
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
