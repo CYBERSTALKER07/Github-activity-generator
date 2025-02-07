fb91062695f48de4ed915ee18ecfd8b8021e5110
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-02-08 04:47:17 +0500

    fix: correct calculation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -26527,3 +26527,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2025-02-08
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
