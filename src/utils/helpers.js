81b1d40134f4fccdff042fbcb9cc0856f029384e
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-02-07 15:12:18 +0500

    build: optimize bundling

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -26511,3 +26511,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2025-02-07
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
