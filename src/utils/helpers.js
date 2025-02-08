c351db05b8a93f05caeb6bd5d1ce5b9dbfd6692b
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-02-08 09:34:14 +0500

    chore: update configuration

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -26543,3 +26543,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2025-02-08
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
