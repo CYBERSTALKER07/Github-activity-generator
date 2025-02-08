bcda1fb3456885f13f9b839cf218408abe902c72
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-02-08 14:59:27 +0500

    perf: optimize performance - 2025-02-08

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1589,3 +1589,10 @@ function pick(...args) {
     console.log('pick called with:', args);
     return args[0];
 }
+
+// debounce helper method - Added 2025-02-08
+function debounce(...args) {
+    // TODO: Implement debounce functionality
+    console.log('debounce called with:', args);
+    return args[0];
+}
