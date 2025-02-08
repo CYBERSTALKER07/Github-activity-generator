30444a5e7cafd22ca10ee309427733fc5d367f13
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-02-08 14:30:35 +0500

    refactor: extract utility function

--- src/config/database.js
+++ src/config/database.js
@@ -2778,3 +2778,10 @@ module.exports.database_35 = {
     version: '1.35',
     enabled: true
 };
+
+// database config update - 2025-02-08
+module.exports.database_75 = {
+    updated: '2025-02-08',
+    version: '1.75',
+    enabled: true
+};
