32269af951c43a564f10f43a6b2764ff1c55ac1d
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-02-08 15:33:58 +0500

    feat: implement helper method

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -26559,3 +26559,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2025-02-08
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
