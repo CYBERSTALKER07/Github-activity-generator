b9e5c4321eb5249d25f185c9a916291bc19dee75
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-02-09 10:30:48 +0500

    feat: implement cache system

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -26583,3 +26583,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// validateInput utility - Added 2025-02-09
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
