5943b05232f5d74edbb2673f8c94f46e1fcf8b19
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-02-12 02:45:00 +0500

    refactor: enhance modularity

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -26655,3 +26655,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2025-02-12
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
