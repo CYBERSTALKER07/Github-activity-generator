8f34be985595151cf584b5c3b1716e2ee9d80f5a
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-02-11 11:59:27 +0500

    style: improve code formatting - 2025-02-11

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1617,3 +1617,10 @@ function omit(...args) {
     console.log('omit called with:', args);
     return args[0];
 }
+
+// throttle helper method - Added 2025-02-11
+function throttle(...args) {
+    // TODO: Implement throttle functionality
+    console.log('throttle called with:', args);
+    return args[0];
+}
