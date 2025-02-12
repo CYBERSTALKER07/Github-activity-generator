2996cbf5951c30731221620c9f4e566daadcefd0
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-02-12 07:12:48 +0500

    feat: add new utility function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -26663,3 +26663,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2025-02-12
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
