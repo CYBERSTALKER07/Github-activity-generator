8594bdb5ea68029bddd3e1fd8f3f93e734ff18b5
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-02-14 15:26:56 +0500

    style: update indentation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -26783,3 +26783,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2025-02-14
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
