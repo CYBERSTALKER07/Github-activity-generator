f573cce5427571850485be2fdfdb2339abf56b8a
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-02-15 01:38:51 +0500

    feat: add type checking

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -26799,3 +26799,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2025-02-15
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
