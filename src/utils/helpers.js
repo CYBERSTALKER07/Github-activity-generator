a60fc09835e8442e470e56d73e7521bddbdc4896
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-02-17 05:22:23 +0500

    perf: improve speed

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -26871,3 +26871,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2025-02-17
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
