847451f6bf96886561acc8d16c364c84a28c517e
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-02-18 07:05:40 +0500

    feat: add error handler

--- src/config/cache.js
+++ src/config/cache.js
@@ -2827,3 +2827,10 @@ module.exports.cache_67 = {
     version: '1.67',
     enabled: true
 };
+
+// cache config update - 2025-02-18
+module.exports.cache_27 = {
+    updated: '2025-02-18',
+    version: '1.27',
+    enabled: true
+};
