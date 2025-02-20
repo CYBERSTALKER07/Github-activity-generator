3c309c8d37a817e7a312d5f496c3bdea2b818745
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-02-20 12:57:27 +0500

    feat: add new utility functions - 2025-02-20

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1666,3 +1666,10 @@ function throttle(...args) {
     console.log('throttle called with:', args);
     return args[0];
 }
+
+// merge helper method - Added 2025-02-20
+function merge(...args) {
+    // TODO: Implement merge functionality
+    console.log('merge called with:', args);
+    return args[0];
+}
