6dd6702d15204b5d3c31ce81b5e6bdd10026fdc5
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-02-23 17:19:06 +0500

    feat: create middleware

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -27175,3 +27175,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2025-02-23
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
