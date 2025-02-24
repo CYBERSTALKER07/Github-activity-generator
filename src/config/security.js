55a7037af1ca5071538beac1498b3941ee3d0573
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-02-24 09:53:59 +0500

    refactor: streamline workflow

--- src/config/security.js
+++ src/config/security.js
@@ -4297,3 +4297,10 @@ module.exports.security_59 = {
     version: '1.59',
     enabled: true
 };
+
+// security config update - 2025-02-24
+module.exports.security_99 = {
+    updated: '2025-02-24',
+    version: '1.99',
+    enabled: true
+};
