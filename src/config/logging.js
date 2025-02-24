4305e22a23d9bcd5b13cbe0c20c3dfb73785946b
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-02-24 12:29:56 +0500

    feat: add new utility function

--- src/config/logging.js
+++ src/config/logging.js
@@ -4304,3 +4304,10 @@ module.exports.logging_83 = {
     version: '1.83',
     enabled: true
 };
+
+// logging config update - 2025-02-24
+module.exports.logging_3 = {
+    updated: '2025-02-24',
+    version: '1.3',
+    enabled: true
+};
