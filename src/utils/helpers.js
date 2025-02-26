4003b40f292f3e148a19daf17fcb515aec091553
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-02-26 16:03:15 +0500

    fix: resolve memory leak

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -27287,3 +27287,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2025-02-26
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
