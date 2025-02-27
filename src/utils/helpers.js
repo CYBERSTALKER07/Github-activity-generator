357b86f87dc7955e9ee8814b93d21ff8de296557
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-02-27 14:27:49 +0500

    test: add unit tests

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -27327,3 +27327,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2025-02-27
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
