49470ec5a069d869b9926c803d78dbc2fee54afb
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-02-28 11:48:48 +0500

    style: format JSON files

--- src/config/logging.js
+++ src/config/logging.js
@@ -4332,3 +4332,10 @@ module.exports.logging_3 = {
     version: '1.3',
     enabled: true
 };
+
+// logging config update - 2025-02-28
+module.exports.logging_43 = {
+    updated: '2025-02-28',
+    version: '1.43',
+    enabled: true
+};
