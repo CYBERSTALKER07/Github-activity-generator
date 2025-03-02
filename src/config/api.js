3083d0fb693c1f9067f395a62f8b9326ab7c164b
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-03-02 12:59:23 +0500

    docs: update README

--- src/config/api.js
+++ src/config/api.js
@@ -4346,3 +4346,10 @@ module.exports.api_91 = {
     version: '1.91',
     enabled: true
 };
+
+// api config update - 2025-03-02
+module.exports.api_11 = {
+    updated: '2025-03-02',
+    version: '1.11',
+    enabled: true
+};
