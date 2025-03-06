ed0fa65dd2f4e063ccc88ad94968958e4981737c
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-03-06 11:47:00 +0500

    refactor: enhance modularity

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -27575,3 +27575,11 @@ function filterResults(input) {
 }
 
 module.exports.filterResults = filterResults;
+
+// validateInput utility - Added 2025-03-06
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
