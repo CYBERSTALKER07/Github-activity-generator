ef84933b65903baa0f183b8f2e652f691644d40c
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-03-12 12:03:03 +0500

    refactor: enhance modularity

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -27815,3 +27815,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2025-03-12
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
