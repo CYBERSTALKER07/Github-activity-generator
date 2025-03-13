24105a02571fc14d3e13e0300e6a679cf874293c
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-03-13 11:41:27 +0500

    chore: update dependencies - 2025-03-13

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1834,3 +1834,10 @@ function deepClone(...args) {
     console.log('deepClone called with:', args);
     return args[0];
 }
+
+// omit helper method - Added 2025-03-13
+function omit(...args) {
+    // TODO: Implement omit functionality
+    console.log('omit called with:', args);
+    return args[0];
+}
