16834ba21abfeb8ee8cfeedc39faa9eedb65fd11
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-03-13 16:32:22 +0500

    fix: fix compatibility issue

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -27863,3 +27863,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2025-03-13
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
