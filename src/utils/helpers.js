fddb69d01892fca8d4c09b313af2eda45d9b598a
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-03-15 13:08:16 +0500

    test: add integration tests

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -27911,3 +27911,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2025-03-15
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
