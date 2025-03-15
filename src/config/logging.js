4e32eb6137349e7fb77b7632ffdca962ca9421ef
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-03-15 14:27:17 +0500

    test: add integration tests

--- src/config/logging.js
+++ src/config/logging.js
@@ -4444,3 +4444,10 @@ module.exports.logging_43 = {
     version: '1.43',
     enabled: true
 };
+
+// logging config update - 2025-03-15
+module.exports.logging_83 = {
+    updated: '2025-03-15',
+    version: '1.83',
+    enabled: true
+};
