a94b66e39ce7964f04fe4e55cd4bf9d1b4b32426
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-03-16 07:03:56 +0500

    build: improve build process

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -27935,3 +27935,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// validateInput utility - Added 2025-03-16
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
