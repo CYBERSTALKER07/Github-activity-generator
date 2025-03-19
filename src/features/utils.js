d36f5cb2da89f4cc2bdd0ac1ff6ade798a360d45
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-03-20 02:03:40 +0500

    refactor: simplify conditional logic

--- src/features/utils.js
+++ src/features/utils.js
@@ -4254,3 +4254,8 @@ class UtilsFeature {
 class UtilsFeature {
     // Feature implementation 69
 }
+
+// utils feature update - 2025-03-20
+class UtilsFeature {
+    // Feature implementation 93
+}
