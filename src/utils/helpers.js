e570e25c39dc7340aba83187da015e5f71d087c9
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-03-20 11:43:02 +0500

    perf: optimize performance - 2025-03-20

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4949,3 +4949,10 @@ function deepClone(...args) {
     console.log('deepClone called with:', args);
     return args[0];
 }
+
+// debounce helper method - Added 2025-03-20
+function debounce(...args) {
+    // TODO: Implement debounce functionality
+    console.log('debounce called with:', args);
+    return args[0];
+}
