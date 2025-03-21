5182d77e6dbb78c4766c4a55d6e9de112559bc4b
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-03-21 06:06:34 +0500

    refactor: improve method signatures

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -28079,3 +28079,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2025-03-21
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
