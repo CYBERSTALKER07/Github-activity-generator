bb7d8759e9964e2fdaa0e214bf2c48c4f6ce5d22
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-03-22 09:56:20 +0500

    ci: enhance automation

--- src/config/logging.js
+++ src/config/logging.js
@@ -4486,3 +4486,10 @@ module.exports.logging_43 = {
     version: '1.43',
     enabled: true
 };
+
+// logging config update - 2025-03-22
+module.exports.logging_83 = {
+    updated: '2025-03-22',
+    version: '1.83',
+    enabled: true
+};
