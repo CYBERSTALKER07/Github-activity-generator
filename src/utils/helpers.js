2f20432dfcba12ddfa84358aa0fde720ddf05e19
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-03-23 10:52:27 +0500

    ci: improve continuous integration - 2025-03-23

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1967,3 +1967,10 @@ function deepClone(...args) {
     console.log('deepClone called with:', args);
     return args[0];
 }
+
+// pick helper method - Added 2025-03-23
+function pick(...args) {
+    // TODO: Implement pick functionality
+    console.log('pick called with:', args);
+    return args[0];
+}
