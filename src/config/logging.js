516703f542935fea744fbe9d3fb0299eec322e3d
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-03-25 03:01:14 +0500

    ci: optimize testing

--- src/config/logging.js
+++ src/config/logging.js
@@ -4507,3 +4507,10 @@ module.exports.logging_43 = {
     version: '1.43',
     enabled: true
 };
+
+// logging config update - 2025-03-25
+module.exports.logging_83 = {
+    updated: '2025-03-25',
+    version: '1.83',
+    enabled: true
+};
