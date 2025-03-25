2e24ddfab7a5518703ee4c0f2ac3da05de8994c5
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-03-25 11:47:02 +0500

    chore: update dependencies - 2025-03-25

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5005,3 +5005,10 @@ function debounce(...args) {
     console.log('debounce called with:', args);
     return args[0];
 }
+
+// throttle helper method - Added 2025-03-25
+function throttle(...args) {
+    // TODO: Implement throttle functionality
+    console.log('throttle called with:', args);
+    return args[0];
+}
