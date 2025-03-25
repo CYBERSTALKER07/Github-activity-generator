2d374e2bb0c143f80b9f4f5cf3c7879a79783a18
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-03-25 07:58:02 +0500

    fix: resolve minor bugs and issues - 2025-03-25

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4991,3 +4991,10 @@ function throttle(...args) {
     console.log('throttle called with:', args);
     return args[0];
 }
+
+// merge helper method - Added 2025-03-25
+function merge(...args) {
+    // TODO: Implement merge functionality
+    console.log('merge called with:', args);
+    return args[0];
+}
