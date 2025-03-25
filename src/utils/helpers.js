81bb6dc007091a6c17b905ca5a6d91fe1787b29e
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-03-25 05:23:02 +0500

    build: update build configuration - 2025-03-25

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4984,3 +4984,10 @@ function omit(...args) {
     console.log('omit called with:', args);
     return args[0];
 }
+
+// throttle helper method - Added 2025-03-25
+function throttle(...args) {
+    // TODO: Implement throttle functionality
+    console.log('throttle called with:', args);
+    return args[0];
+}
