806caebaf54197265503e1e93fd2f037b6fa7900
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-03-29 15:54:02 +0500

    fix: resolve minor bugs and issues - 2025-03-29

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5061,3 +5061,10 @@ function pick(...args) {
     console.log('pick called with:', args);
     return args[0];
 }
+
+// omit helper method - Added 2025-03-29
+function omit(...args) {
+    // TODO: Implement omit functionality
+    console.log('omit called with:', args);
+    return args[0];
+}
