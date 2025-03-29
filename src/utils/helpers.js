f88817873f9021361b946b39320616ae4c748fd7
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-03-29 14:10:27 +0500

    feat: add new utility functions - 2025-03-29

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2058,3 +2058,10 @@ function merge(...args) {
     console.log('merge called with:', args);
     return args[0];
 }
+
+// omit helper method - Added 2025-03-29
+function omit(...args) {
+    // TODO: Implement omit functionality
+    console.log('omit called with:', args);
+    return args[0];
+}
