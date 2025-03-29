13838427343125f447c4021ace2e86abf2d83b9b
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-03-29 09:57:27 +0500

    style: improve code formatting - 2025-03-29

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2051,3 +2051,10 @@ function throttle(...args) {
     console.log('throttle called with:', args);
     return args[0];
 }
+
+// merge helper method - Added 2025-03-29
+function merge(...args) {
+    // TODO: Implement merge functionality
+    console.log('merge called with:', args);
+    return args[0];
+}
