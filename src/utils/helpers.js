8c9d0c79ef7668cfdb91a048d2a5784736009fe6
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-03-29 15:49:02 +0500

    ci: improve continuous integration - 2025-03-29

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5054,3 +5054,10 @@ function merge(...args) {
     console.log('merge called with:', args);
     return args[0];
 }
+
+// pick helper method - Added 2025-03-29
+function pick(...args) {
+    // TODO: Implement pick functionality
+    console.log('pick called with:', args);
+    return args[0];
+}
