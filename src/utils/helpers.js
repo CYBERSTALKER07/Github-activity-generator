240e7e43aca5f58be4e384d0178bbde6bf76353e
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-03-30 09:38:02 +0500

    docs: update documentation - 2025-03-30

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5089,3 +5089,10 @@ function pick(...args) {
     console.log('pick called with:', args);
     return args[0];
 }
+
+// throttle helper method - Added 2025-03-30
+function throttle(...args) {
+    // TODO: Implement throttle functionality
+    console.log('throttle called with:', args);
+    return args[0];
+}
