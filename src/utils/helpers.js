3df43bec6bd5fc321faa19cce4d8c221c20f4d73
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-03-30 06:35:39 +0500

    ci: optimize testing

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -28399,3 +28399,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2025-03-30
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
