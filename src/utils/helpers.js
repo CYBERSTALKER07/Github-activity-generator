6a63e822036ef94721bd6dcae6aedac4dc43d497
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-03 04:20:27 +0500

    build: update build configuration - 2025-04-03

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2072,3 +2072,10 @@ function omit(...args) {
     console.log('omit called with:', args);
     return args[0];
 }
+
+// omit helper method - Added 2025-04-03
+function omit(...args) {
+    // TODO: Implement omit functionality
+    console.log('omit called with:', args);
+    return args[0];
+}
