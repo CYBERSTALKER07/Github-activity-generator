6c5bdabd9cb4588fa65552397baf4898a47072b8
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-03 02:58:52 +0500

    style: format JSON files

--- src/config/database.js
+++ src/config/database.js
@@ -3044,3 +3044,10 @@ module.exports.database_35 = {
     version: '1.35',
     enabled: true
 };
+
+// database config update - 2025-04-03
+module.exports.database_75 = {
+    updated: '2025-04-03',
+    version: '1.75',
+    enabled: true
+};
