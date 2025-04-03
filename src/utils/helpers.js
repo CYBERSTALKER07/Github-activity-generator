050e5c3aaf3e530fdb18452585ad5969ecb68cca
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-03 14:09:31 +0500

    perf: optimize performance

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -28559,3 +28559,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// validateInput utility - Added 2025-04-03
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
