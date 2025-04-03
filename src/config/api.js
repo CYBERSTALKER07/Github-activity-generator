1dfe0fc8d1801d6d39f34b1031d44dc63971bbab
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-03 17:49:32 +0500

    docs: improve method descriptions

--- src/config/api.js
+++ src/config/api.js
@@ -4577,3 +4577,10 @@ module.exports.api_91 = {
     version: '1.91',
     enabled: true
 };
+
+// api config update - 2025-04-03
+module.exports.api_11 = {
+    updated: '2025-04-03',
+    version: '1.11',
+    enabled: true
+};
