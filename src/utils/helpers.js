3e7d0287bae0babaf460893afa28c84e5102baa3
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-04 11:28:02 +0500

    feat: improve function logic

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -28591,3 +28591,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2025-04-04
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
