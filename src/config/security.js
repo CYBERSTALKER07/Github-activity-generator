c941b136d2b00262a3920ebbfbede5dd487d78a3
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-07 14:20:39 +0500

    style: standardize quotes

--- src/config/security.js
+++ src/config/security.js
@@ -4605,3 +4605,10 @@ module.exports.security_19 = {
     version: '1.19',
     enabled: true
 };
+
+// security config update - 2025-04-07
+module.exports.security_59 = {
+    updated: '2025-04-07',
+    version: '1.59',
+    enabled: true
+};
