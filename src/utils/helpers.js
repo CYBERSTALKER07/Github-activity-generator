9ec32a21ee7eeeb1a4826bd586b02e198048befa
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-08 14:07:11 +0500

    fix: fix null pointer

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -28759,3 +28759,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2025-04-08
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
