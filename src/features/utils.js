1b5466e1d7dac1d201f3ed663088590f8bd0bc0e
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-11 09:01:40 +0500

    refactor: reduce code duplication

--- src/features/utils.js
+++ src/features/utils.js
@@ -4414,3 +4414,8 @@ class UtilsFeature {
 class UtilsFeature {
     // Feature implementation 69
 }
+
+// utils feature update - 2025-04-11
+class UtilsFeature {
+    // Feature implementation 93
+}
