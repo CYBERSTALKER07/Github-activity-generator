b99612077622ed1051f01afb999aca93ee5e5fcf
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-11 16:01:51 +0500

    chore: maintenance tasks

--- src/config/api.js
+++ src/config/api.js
@@ -4640,3 +4640,10 @@ module.exports.api_91 = {
     version: '1.91',
     enabled: true
 };
+
+// api config update - 2025-04-11
+module.exports.api_11 = {
+    updated: '2025-04-11',
+    version: '1.11',
+    enabled: true
+};
