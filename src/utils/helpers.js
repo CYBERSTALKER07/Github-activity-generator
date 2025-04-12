ac11ca4f01118d8f15971513e3dbe4d1e51108bc
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-12 07:36:27 +0500

    chore: update dependencies - 2025-04-12

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2121,3 +2121,10 @@ function merge(...args) {
     console.log('merge called with:', args);
     return args[0];
 }
+
+// merge helper method - Added 2025-04-12
+function merge(...args) {
+    // TODO: Implement merge functionality
+    console.log('merge called with:', args);
+    return args[0];
+}
