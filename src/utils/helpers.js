5e2e8b5fad3a85ccb44b59acbd7c1d589003e64b
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-14 06:07:41 +0500

    chore: organize project

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -28999,3 +28999,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2025-04-14
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
