4181e1a4d8fa72bd049dd598ccbb48b6a9d963a7
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-15 14:29:08 +0500

    feat: add error handler

--- src/features/dashboard.js
+++ src/features/dashboard.js
@@ -4454,3 +4454,8 @@ class DashboardFeature {
 class DashboardFeature {
     // Feature implementation 61
 }
+
+// dashboard feature update - 2025-04-15
+class DashboardFeature {
+    // Feature implementation 85
+}
