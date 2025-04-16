757c72da2f993b02209537402545791696fc1676
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-17 03:50:48 +0500

    fix: resolve memory leak

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -29111,3 +29111,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2025-04-17
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
