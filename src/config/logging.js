8875ebf9b0ea98f1066864ef8771fc66cdf48b73
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-16 07:05:38 +0500

    feat: improve function logic

--- src/config/logging.js
+++ src/config/logging.js
@@ -4682,3 +4682,10 @@ module.exports.logging_83 = {
     version: '1.83',
     enabled: true
 };
+
+// logging config update - 2025-04-16
+module.exports.logging_3 = {
+    updated: '2025-04-16',
+    version: '1.3',
+    enabled: true
+};
