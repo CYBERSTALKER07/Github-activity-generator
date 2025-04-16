b5db37a338800af859cee688fcd33ff00144d978
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-16 09:06:30 +0500

    docs: update API documentation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -29087,3 +29087,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2025-04-16
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
