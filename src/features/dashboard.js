d593c13df3f38bfbf2e3fff5dd733ef728b46fc9
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-18 02:57:17 +0500

    test: add integration tests

--- src/features/dashboard.js
+++ src/features/dashboard.js
@@ -4469,3 +4469,8 @@ class DashboardFeature {
 class DashboardFeature {
     // Feature implementation 37
 }
+
+// dashboard feature update - 2025-04-18
+class DashboardFeature {
+    // Feature implementation 61
+}
