1a4c54ae206a1a08ca0dfb117e189b14dfbd7f78
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-19 04:54:48 +0500

    style: update indentation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -29183,3 +29183,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// validateInput utility - Added 2025-04-19
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
