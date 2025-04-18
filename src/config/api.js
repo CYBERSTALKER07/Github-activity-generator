d60c4f63da073e491c38912504df00833eeddb65
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-18 15:49:28 +0500

    fix: fix validation logic

--- src/config/api.js
+++ src/config/api.js
@@ -4696,3 +4696,10 @@ module.exports.api_51 = {
     version: '1.51',
     enabled: true
 };
+
+// api config update - 2025-04-18
+module.exports.api_91 = {
+    updated: '2025-04-18',
+    version: '1.91',
+    enabled: true
+};
