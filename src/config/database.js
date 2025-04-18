5c93d8fd30659bac7f7eefe70019a2c75bfbb7ee
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-18 08:06:03 +0500

    style: standardize quotes

--- src/config/database.js
+++ src/config/database.js
@@ -3128,3 +3128,10 @@ module.exports.database_35 = {
     version: '1.35',
     enabled: true
 };
+
+// database config update - 2025-04-18
+module.exports.database_75 = {
+    updated: '2025-04-18',
+    version: '1.75',
+    enabled: true
+};
