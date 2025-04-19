4f9581dd0c204c8f00888c214de96cd2ddc0597a
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-19 05:42:15 +0500

    feat: create middleware

--- src/config/logging.js
+++ src/config/logging.js
@@ -4703,3 +4703,10 @@ module.exports.logging_83 = {
     version: '1.83',
     enabled: true
 };
+
+// logging config update - 2025-04-19
+module.exports.logging_3 = {
+    updated: '2025-04-19',
+    version: '1.3',
+    enabled: true
+};
