1fb28bf6cd6ce6c8cfe44d2235d92c67147e9e4b
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-21 04:13:27 +0500

    ci: improve continuous integration - 2025-04-21

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2177,3 +2177,10 @@ function throttle(...args) {
     console.log('throttle called with:', args);
     return args[0];
 }
+
+// merge helper method - Added 2025-04-21
+function merge(...args) {
+    // TODO: Implement merge functionality
+    console.log('merge called with:', args);
+    return args[0];
+}
