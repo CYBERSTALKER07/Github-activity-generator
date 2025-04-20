f068de4b39c94153a8f80dba66e43e875e0a9d8e
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-20 11:24:18 +0500

    perf: enhance efficiency

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -29231,3 +29231,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2025-04-20
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
