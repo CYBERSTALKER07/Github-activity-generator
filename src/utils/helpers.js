847e05891b22506349f8c846ec064130529b0b42
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-22 07:23:27 +0500

    test: add unit tests - 2025-04-22

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2212,3 +2212,10 @@ function deepClone(...args) {
     console.log('deepClone called with:', args);
     return args[0];
 }
+
+// merge helper method - Added 2025-04-22
+function merge(...args) {
+    // TODO: Implement merge functionality
+    console.log('merge called with:', args);
+    return args[0];
+}
