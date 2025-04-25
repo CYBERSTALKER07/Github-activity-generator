0033e31aea16ffa9ccfe6bc9c3e8a37c1dabee3a
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-25 05:17:22 +0500

    feat: add type checking

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -29407,3 +29407,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2025-04-25
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
