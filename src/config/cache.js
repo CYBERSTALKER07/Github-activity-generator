b0bd3224796c27238f2c9a91c15bfb17a3c279fb
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-28 02:03:04 +0500

    build: improve build process

--- src/config/cache.js
+++ src/config/cache.js
@@ -3177,3 +3177,10 @@ module.exports.cache_67 = {
     version: '1.67',
     enabled: true
 };
+
+// cache config update - 2025-04-28
+module.exports.cache_27 = {
+    updated: '2025-04-28',
+    version: '1.27',
+    enabled: true
+};
