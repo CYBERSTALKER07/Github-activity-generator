74ddafe8f0bb4b508221f74c3a0ffc71f44f0818
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-27 05:11:41 +0500

    refactor: reduce code duplication

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -29487,3 +29487,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2025-04-27
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
