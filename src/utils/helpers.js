c2076032c61301df92fe0dcca36fae26033d1292
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-29 04:35:35 +0500

    feat: improve function logic

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -29567,3 +29567,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2025-04-29
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
