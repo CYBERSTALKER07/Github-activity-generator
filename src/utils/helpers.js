e8dcb3e820f53208160aeea8f67ebf806b1bed0a
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-04-30 11:08:27 +0500

    feat: add type checking

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -29623,3 +29623,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2025-04-30
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
