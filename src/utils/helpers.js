31bf5eb6f51275c471ad86ca40e44f26c3d8a4e8
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-05-01 06:23:05 +0500

    refactor: optimize data structures

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -29647,3 +29647,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2025-05-01
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
