df0854abd1766232591620d2b557172b2ba2abe5
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-05-03 13:36:02 +0500

    fix: resolve minor bugs and issues - 2025-05-03

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5285,3 +5285,10 @@ function merge(...args) {
     console.log('merge called with:', args);
     return args[0];
 }
+
+// debounce helper method - Added 2025-05-03
+function debounce(...args) {
+    // TODO: Implement debounce functionality
+    console.log('debounce called with:', args);
+    return args[0];
+}
