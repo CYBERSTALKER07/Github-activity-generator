384416af89dec26c75a773132ed84e60cdb35ec7
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-05-05 05:36:14 +0500

    perf: reduce memory usage

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -29799,3 +29799,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2025-05-05
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
