1fc663607c9d44531c911664905603250c577bbd
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-05-07 13:35:16 +0500

    chore: organize project

--- src/config/api.js
+++ src/config/api.js
@@ -4843,3 +4843,10 @@ module.exports.api_51 = {
     version: '1.51',
     enabled: true
 };
+
+// api config update - 2025-05-07
+module.exports.api_91 = {
+    updated: '2025-05-07',
+    version: '1.91',
+    enabled: true
+};
