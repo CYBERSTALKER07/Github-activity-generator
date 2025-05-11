a2a9815dd0db851bf6883e30fa5b9786d7a4e275
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-05-11 08:21:00 +0500

    build: update dependencies

--- src/config/logging.js
+++ src/config/logging.js
@@ -4871,3 +4871,10 @@ module.exports.logging_83 = {
     version: '1.83',
     enabled: true
 };
+
+// logging config update - 2025-05-11
+module.exports.logging_3 = {
+    updated: '2025-05-11',
+    version: '1.3',
+    enabled: true
+};
