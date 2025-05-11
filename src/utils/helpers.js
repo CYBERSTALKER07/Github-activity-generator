7c35f615193bcfc5a70d49439bfbfafb74da534a
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-05-12 04:47:49 +0500

    feat: add error handler

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -30055,3 +30055,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2025-05-12
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
