2a9e4e6591e8c5ea452d565fe3d558f2c59f165f
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-05-12 11:09:27 +0500

    chore: update dependencies - 2025-05-12

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2366,3 +2366,10 @@ function deepClone(...args) {
     console.log('deepClone called with:', args);
     return args[0];
 }
+
+// merge helper method - Added 2025-05-12
+function merge(...args) {
+    // TODO: Implement merge functionality
+    console.log('merge called with:', args);
+    return args[0];
+}
