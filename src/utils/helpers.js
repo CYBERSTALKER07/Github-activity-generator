c08e49883ee0b5fcf8362e1a3d5652fc4a6c9a69
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-05-13 15:00:36 +0500

    chore: maintenance tasks

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -30119,3 +30119,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// validateInput utility - Added 2025-05-13
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
