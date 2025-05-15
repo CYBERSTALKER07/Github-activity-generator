939494fb0fd1e89971bbecf21f33a07fbfc88caf
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-05-15 17:26:42 +0500

    feat: create middleware

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -30175,3 +30175,11 @@ function filterResults(input) {
 }
 
 module.exports.filterResults = filterResults;
+
+// validateInput utility - Added 2025-05-15
+function validateInput(input) {
+    // Implementation for validateInput
+    return input;
+}
+
+module.exports.validateInput = validateInput;
