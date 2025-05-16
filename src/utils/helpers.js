d45eef4bc16b6d35b12f451e8866c10ebdda5f69
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-05-16 13:36:02 +0500

    ci: improve continuous integration - 2025-05-16

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5411,3 +5411,10 @@ function deepClone(...args) {
     console.log('deepClone called with:', args);
     return args[0];
 }
+
+// omit helper method - Added 2025-05-16
+function omit(...args) {
+    // TODO: Implement omit functionality
+    console.log('omit called with:', args);
+    return args[0];
+}
