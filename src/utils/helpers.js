0669f14149e5fcc308cdda20546e914a0ba0583e
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-05-19 13:02:17 +0500

    chore: organize project

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -30263,3 +30263,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2025-05-19
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
