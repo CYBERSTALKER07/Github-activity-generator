ca97a720816681f0cf312ac6d34cf219279ce27c
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-05-19 14:38:02 +0500

    refactor: improve code structure and readability - 2025-05-19

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5453,3 +5453,10 @@ function debounce(...args) {
     console.log('debounce called with:', args);
     return args[0];
 }
+
+// deepClone helper method - Added 2025-05-19
+function deepClone(...args) {
+    // TODO: Implement deepClone functionality
+    console.log('deepClone called with:', args);
+    return args[0];
+}
