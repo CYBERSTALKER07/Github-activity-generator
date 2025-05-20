7f60b6b9f11fa5feeb52b04625111009a8205de8
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-05-20 06:28:02 +0500

    docs: update documentation - 2025-05-20

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5467,3 +5467,10 @@ function merge(...args) {
     console.log('merge called with:', args);
     return args[0];
 }
+
+// omit helper method - Added 2025-05-20
+function omit(...args) {
+    // TODO: Implement omit functionality
+    console.log('omit called with:', args);
+    return args[0];
+}
