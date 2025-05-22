8cab0a12fc2640db304647c7dba46c588f96fb74
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-05-22 16:56:33 +0500

    chore: clean up files

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -30407,3 +30407,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2025-05-22
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
