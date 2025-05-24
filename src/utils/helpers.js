d4465da8ef9744534ac9dc9a67087534b8cc4785
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-05-25 04:51:27 +0500

    ci: improve continuous integration - 2025-05-25

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2471,3 +2471,10 @@ function omit(...args) {
     console.log('omit called with:', args);
     return args[0];
 }
+
+// throttle helper method - Added 2025-05-25
+function throttle(...args) {
+    // TODO: Implement throttle functionality
+    console.log('throttle called with:', args);
+    return args[0];
+}
