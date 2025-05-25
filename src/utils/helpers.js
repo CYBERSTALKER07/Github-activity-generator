edb8dccc00836d000b260f13ec28b528797c55e1
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-05-25 08:06:33 +0500

    feat: create middleware

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -30511,3 +30511,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2025-05-25
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
