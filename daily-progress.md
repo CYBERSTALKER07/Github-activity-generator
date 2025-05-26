a7143a94c2d4a81d3fc74a2ba66b1a4129006fbe
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-05-26 08:18:27 +0500

    feat: add new utility functions - 2025-05-26

--- daily-progress.md
+++ daily-progress.md
@@ -1849,3 +1849,8 @@
 - Enhanced project functionality
 - Improved code quality
 - Added meaningful contributions
+
+## 2025-05-26
+- Enhanced project functionality
+- Improved code quality
+- Added meaningful contributions
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2506,3 +2506,10 @@ function pick(...args) {
     console.log('pick called with:', args);
     return args[0];
 }
+
+// omit helper method - Added 2025-05-26
+function omit(...args) {
+    // TODO: Implement omit functionality
+    console.log('omit called with:', args);
+    return args[0];
+}
