97b9346027fb3176c7c87a08cfa80d8525c56996
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-05-30 14:54:27 +0500

    style: standardize quotes

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -30719,3 +30719,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2025-05-30
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
