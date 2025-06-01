f522566724e64e9288b5dec93400e8710f42dfc7
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-06-01 10:44:11 +0500

    feat: create data validator

--- src/config/database.js
+++ src/config/database.js
@@ -3345,3 +3345,10 @@ module.exports.database_75 = {
     version: '1.75',
     enabled: true
 };
+
+// database config update - 2025-06-01
+module.exports.database_35 = {
+    updated: '2025-06-01',
+    version: '1.35',
+    enabled: true
+};
