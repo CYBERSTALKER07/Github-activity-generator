58d6897f414567e7f48d7b8607b293898b783416
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-06-01 16:23:13 +0500

    style: improve code structure

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -30791,3 +30791,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2025-06-01
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
