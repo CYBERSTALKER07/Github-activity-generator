a3fff9d1757ddff894c408efbd90e9a39aeeaf59
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-06-03 01:17:30 +0500

    style: improve code formatting

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -30831,3 +30831,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2025-06-03
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
