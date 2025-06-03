3acae96081214e14acd3c516e6a91ea4aee96a75
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-06-03 07:17:43 +0500

    docs: update README

--- src/config/logging.js
+++ src/config/logging.js
@@ -5039,3 +5039,10 @@ module.exports.logging_83 = {
     version: '1.83',
     enabled: true
 };
+
+// logging config update - 2025-06-03
+module.exports.logging_3 = {
+    updated: '2025-06-03',
+    version: '1.3',
+    enabled: true
+};
