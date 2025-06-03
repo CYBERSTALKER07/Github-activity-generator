3c2550ebee7a8204bf9faef3ec3fc6eda7e9439c
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-06-03 11:46:27 +0500

    chore: update dependencies - 2025-06-03

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2597,3 +2597,10 @@ function omit(...args) {
     console.log('omit called with:', args);
     return args[0];
 }
+
+// pick helper method - Added 2025-06-03
+function pick(...args) {
+    // TODO: Implement pick functionality
+    console.log('pick called with:', args);
+    return args[0];
+}
