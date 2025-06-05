08ccb842802d8b82e47b8a40535da76041f71619
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-06-05 11:15:23 +0500

    refactor: improve error handling

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -30935,3 +30935,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2025-06-05
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
