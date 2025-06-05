e7e2437eaeab2636c5ab73eaa1c598c2bee7b2c9
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-06-05 11:47:27 +0500

    ci: improve continuous integration - 2025-06-05

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2632,3 +2632,10 @@ function omit(...args) {
     console.log('omit called with:', args);
     return args[0];
 }
+
+// pick helper method - Added 2025-06-05
+function pick(...args) {
+    // TODO: Implement pick functionality
+    console.log('pick called with:', args);
+    return args[0];
+}
