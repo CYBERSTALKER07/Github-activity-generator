78484df0418946921724086aba98fe3ade0a2d86
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-06-08 01:46:40 +0500

    ci: improve CI pipeline

--- src/config/logging.js
+++ src/config/logging.js
@@ -5074,3 +5074,10 @@ module.exports.logging_43 = {
     version: '1.43',
     enabled: true
 };
+
+// logging config update - 2025-06-08
+module.exports.logging_83 = {
+    updated: '2025-06-08',
+    version: '1.83',
+    enabled: true
+};
