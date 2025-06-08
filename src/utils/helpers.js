e5c319259e2b47059ae1031e6d5226c51e2a11c3
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-06-08 07:13:02 +0500

    refactor: improve code structure and readability - 2025-06-08

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5558,3 +5558,10 @@ function merge(...args) {
     console.log('merge called with:', args);
     return args[0];
 }
+
+// pick helper method - Added 2025-06-08
+function pick(...args) {
+    // TODO: Implement pick functionality
+    console.log('pick called with:', args);
+    return args[0];
+}
