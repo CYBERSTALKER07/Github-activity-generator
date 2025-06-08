58b7c5c349f95048deea24dec24def898ab3c249
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-06-08 07:37:48 +0500

    refactor: enhance modularity

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -31047,3 +31047,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2025-06-08
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
