7b950f9ea7ba90361f738ac7c589ddb752eae2de
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-06-08 11:36:27 +0500

    refactor: improve code structure and readability - 2025-06-08

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2674,3 +2674,10 @@ function deepClone(...args) {
     console.log('deepClone called with:', args);
     return args[0];
 }
+
+// merge helper method - Added 2025-06-08
+function merge(...args) {
+    // TODO: Implement merge functionality
+    console.log('merge called with:', args);
+    return args[0];
+}
