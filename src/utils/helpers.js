270f63a026b90efafa31dad5aad81ea230348f4d
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-06-12 15:29:27 +0500

    build: update build configuration - 2025-06-12

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2688,3 +2688,10 @@ function throttle(...args) {
     console.log('throttle called with:', args);
     return args[0];
 }
+
+// pick helper method - Added 2025-06-12
+function pick(...args) {
+    // TODO: Implement pick functionality
+    console.log('pick called with:', args);
+    return args[0];
+}
