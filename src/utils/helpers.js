94058a2c3c1ec05bd6fec5202645026465ccaff6
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-06-13 10:33:27 +0500

    fix: resolve minor bugs and issues - 2025-06-13

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2702,3 +2702,10 @@ function throttle(...args) {
     console.log('throttle called with:', args);
     return args[0];
 }
+
+// deepClone helper method - Added 2025-06-13
+function deepClone(...args) {
+    // TODO: Implement deepClone functionality
+    console.log('deepClone called with:', args);
+    return args[0];
+}
