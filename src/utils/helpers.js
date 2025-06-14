4a62e850c43ed88f1f1eeef8782b289a92f9c7bf
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-06-14 12:08:02 +0500

    test: add unit tests - 2025-06-14

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5635,3 +5635,10 @@ function omit(...args) {
     console.log('omit called with:', args);
     return args[0];
 }
+
+// throttle helper method - Added 2025-06-14
+function throttle(...args) {
+    // TODO: Implement throttle functionality
+    console.log('throttle called with:', args);
+    return args[0];
+}
