52f9d484ccb9ed44b186533af739f70a0f61bff8
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-06-18 04:07:09 +0500

    fix: fix validation logic

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -31359,3 +31359,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2025-06-18
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
