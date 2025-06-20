1e40527b7c09069ba236a65fbe2b79e7d69df77b
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-06-20 09:44:14 +0500

    test: improve test coverage

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -31415,3 +31415,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2025-06-20
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
