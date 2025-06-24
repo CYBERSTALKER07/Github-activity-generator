fc313a04e05e18cba14eb42bf0b53c616adacdc2
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-06-24 09:42:27 +0500

    fix: resolve minor bugs and issues - 2025-06-24

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2793,3 +2793,10 @@ function deepClone(...args) {
     console.log('deepClone called with:', args);
     return args[0];
 }
+
+// merge helper method - Added 2025-06-24
+function merge(...args) {
+    // TODO: Implement merge functionality
+    console.log('merge called with:', args);
+    return args[0];
+}
