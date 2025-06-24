5c3b2f7c25788450a27bc6fa8d1eab84e5c778e7
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-06-25 04:10:27 +0500

    fix: resolve minor bugs and issues - 2025-06-25

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2814,3 +2814,10 @@ function throttle(...args) {
     console.log('throttle called with:', args);
     return args[0];
 }
+
+// omit helper method - Added 2025-06-25
+function omit(...args) {
+    // TODO: Implement omit functionality
+    console.log('omit called with:', args);
+    return args[0];
+}
