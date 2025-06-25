57ce42dedc11ec87da39342f11b4a1d696eddde2
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-06-25 09:32:27 +0500

    refactor: improve code structure and readability - 2025-06-25

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2828,3 +2828,10 @@ function throttle(...args) {
     console.log('throttle called with:', args);
     return args[0];
 }
+
+// merge helper method - Added 2025-06-25
+function merge(...args) {
+    // TODO: Implement merge functionality
+    console.log('merge called with:', args);
+    return args[0];
+}
