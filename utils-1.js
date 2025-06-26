5fd7f9239d64f457aea40e89eccc029a216cb349
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-06-26 10:39:57 +0500

    test: add unit tests - 2025-06-26

--- utils-1.js
+++ utils-1.js
@@ -31,3 +31,11 @@ function formatDate(input) {
 }
 
 module.exports.formatDate = formatDate;
+
+// parseConfig utility function - Added 2025-06-26
+function parseConfig(input) {
+    // TODO: Implement parseConfig functionality
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
