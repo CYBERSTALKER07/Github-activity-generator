71392500c0ab7a98f339818e801ddc213d6d367c
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-06-27 16:27:26 +0500

    refactor: optimize data structures

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -31711,3 +31711,11 @@ function merge(input) {
 }
 
 module.exports.merge = merge;
+
+// hashString utility - Added 2025-06-27
+function hashString(input) {
+    // Implementation for hashString
+    return input;
+}
+
+module.exports.hashString = hashString;
