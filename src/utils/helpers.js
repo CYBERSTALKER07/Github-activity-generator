c6005baac9660a10aa424744c2fd060f0ec61256
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-06-30 11:18:56 +0500

    refactor: optimize data structures

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -31823,3 +31823,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2025-06-30
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
