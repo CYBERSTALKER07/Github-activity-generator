db63ff3e77168f1e1c728881e11904d6ae680bbc
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-06-30 13:52:57 +0500

    feat: add new utility functions - 2025-06-30

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3108,3 +3108,10 @@ function deepClone(...args) {
     console.log('deepClone called with:', args);
     return args[0];
 }
+
+// pick helper method - Added 2025-06-30
+function pick(...args) {
+    // TODO: Implement pick functionality
+    console.log('pick called with:', args);
+    return args[0];
+}
