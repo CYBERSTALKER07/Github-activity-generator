f19500b1eccef15d43347723234d138953043de1
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-06-30 05:12:02 +0500

    style: improve code formatting - 2025-06-30

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5712,3 +5712,10 @@ function throttle(...args) {
     console.log('throttle called with:', args);
     return args[0];
 }
+
+// debounce helper method - Added 2025-06-30
+function debounce(...args) {
+    // TODO: Implement debounce functionality
+    console.log('debounce called with:', args);
+    return args[0];
+}
