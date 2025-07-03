2a615548a7c900470337a50e12b82da0f80b170d
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-07-03 17:36:21 +0500

    docs: update API documentation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -31967,3 +31967,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2025-07-03
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
