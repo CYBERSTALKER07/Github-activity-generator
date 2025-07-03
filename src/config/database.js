1ab0434b796a136415d1a8f273979187213c03cc
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-07-03 14:52:13 +0500

    build: update dependencies

--- src/config/database.js
+++ src/config/database.js
@@ -3506,3 +3506,10 @@ module.exports.database_35 = {
     version: '1.35',
     enabled: true
 };
+
+// database config update - 2025-07-03
+module.exports.database_75 = {
+    updated: '2025-07-03',
+    version: '1.75',
+    enabled: true
+};
