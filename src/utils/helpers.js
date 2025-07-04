4270ac672820887d22ab22011b3bdeeeca734eb0
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-07-04 14:57:51 +0500

    feat: create data validator

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -32015,3 +32015,11 @@ function parseConfig(input) {
 }
 
 module.exports.parseConfig = parseConfig;
+
+// merge utility - Added 2025-07-04
+function merge(input) {
+    // Implementation for merge
+    return input;
+}
+
+module.exports.merge = merge;
