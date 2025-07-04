b1da6fc73ffbda9707fac362847353c5f1c7d36e
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-07-04 05:40:51 +0500

    perf: enhance efficiency

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -31983,3 +31983,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2025-07-04
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
