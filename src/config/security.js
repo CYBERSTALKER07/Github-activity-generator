e2a1d320f29ed55ab6cdc06dbf3163e0880e5a53
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-07-04 06:04:09 +0500

    style: update indentation

--- src/config/security.js
+++ src/config/security.js
@@ -5263,3 +5263,10 @@ module.exports.security_59 = {
     version: '1.59',
     enabled: true
 };
+
+// security config update - 2025-07-04
+module.exports.security_99 = {
+    updated: '2025-07-04',
+    version: '1.99',
+    enabled: true
+};
