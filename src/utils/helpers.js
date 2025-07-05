6f9ed0ec826d1185279b50bb296bbe20bfc58675
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-07-05 05:28:07 +0500

    style: format JSON files

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -32031,3 +32031,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2025-07-05
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
