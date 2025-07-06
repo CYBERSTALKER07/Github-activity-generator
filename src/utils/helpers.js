31be0055e9753b440ff23235ee282594511dac4a
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-07-06 08:28:35 +0500

    refactor: simplify conditional logic

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -32087,3 +32087,11 @@ function hashString(input) {
 }
 
 module.exports.hashString = hashString;
+
+// omit utility - Added 2025-07-06
+function omit(input) {
+    // Implementation for omit
+    return input;
+}
+
+module.exports.omit = omit;
