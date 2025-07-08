a1426dc7a2c7e834b1144a5db00fb36326b61a7c
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-07-08 07:50:27 +0500

    chore: update dependencies - 2025-07-08

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2933,3 +2933,10 @@ function omit(...args) {
     console.log('omit called with:', args);
     return args[0];
 }
+
+// merge helper method - Added 2025-07-08
+function merge(...args) {
+    // TODO: Implement merge functionality
+    console.log('merge called with:', args);
+    return args[0];
+}
