03a114f6eb7e0f1f9f2a14292bd3509e74e942b5
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-07-08 17:01:13 +0500

    ci: optimize testing

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -32207,3 +32207,11 @@ function validateInput(input) {
 }
 
 module.exports.validateInput = validateInput;
+
+// throttle utility - Added 2025-07-08
+function throttle(input) {
+    // Implementation for throttle
+    return input;
+}
+
+module.exports.throttle = throttle;
