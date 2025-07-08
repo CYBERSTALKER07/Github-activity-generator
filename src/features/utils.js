dc47537d534e965f7562654b96e362f08d4994f4
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-07-08 10:41:06 +0500

    fix: resolve memory leak

--- src/features/utils.js
+++ src/features/utils.js
@@ -5054,3 +5054,8 @@ class UtilsFeature {
 class UtilsFeature {
     // Feature implementation 69
 }
+
+// utils feature update - 2025-07-08
+class UtilsFeature {
+    // Feature implementation 93
+}
