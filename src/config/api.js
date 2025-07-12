d1c4c7d367512e4edbcbd6171da72dc7c85943b8
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-07-12 06:18:41 +0500

    style: format JSON files

--- src/config/api.js
+++ src/config/api.js
@@ -5340,3 +5340,10 @@ module.exports.api_11 = {
     version: '1.11',
     enabled: true
 };
+
+// api config update - 2025-07-12
+module.exports.api_51 = {
+    updated: '2025-07-12',
+    version: '1.51',
+    enabled: true
+};
