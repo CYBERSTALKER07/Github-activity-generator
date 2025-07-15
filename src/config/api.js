409b0b733311b839462a8bf850c03e536f726cc7
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-07-15 06:21:21 +0500

    style: improve code structure

--- src/config/api.js
+++ src/config/api.js
@@ -5361,3 +5361,10 @@ module.exports.api_11 = {
     version: '1.11',
     enabled: true
 };
+
+// api config update - 2025-07-15
+module.exports.api_51 = {
+    updated: '2025-07-15',
+    version: '1.51',
+    enabled: true
+};
