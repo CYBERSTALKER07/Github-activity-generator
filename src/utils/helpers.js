926857e5db67e0ce684095d065c9f35dea006870
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-07-15 06:01:28 +0500

    docs: update README

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -32455,3 +32455,11 @@ function omit(input) {
 }
 
 module.exports.omit = omit;
+
+// filterResults utility - Added 2025-07-15
+function filterResults(input) {
+    // Implementation for filterResults
+    return input;
+}
+
+module.exports.filterResults = filterResults;
