b49d5f611ea577d5662ae9a6d612f220a76acadd
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-07-17 04:17:02 +0500

    test: add unit tests - 2025-07-17

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5845,3 +5845,10 @@ function deepClone(...args) {
     console.log('deepClone called with:', args);
     return args[0];
 }
+
+// pick helper method - Added 2025-07-17
+function pick(...args) {
+    // TODO: Implement pick functionality
+    console.log('pick called with:', args);
+    return args[0];
+}
