43ebde0bb916ddb3e680746a5b4ddfa11b3829b5
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-07-17 15:22:22 +0500

    ci: enhance automation

--- src/config/database.js
+++ src/config/database.js
@@ -3590,3 +3590,10 @@ module.exports.database_35 = {
     version: '1.35',
     enabled: true
 };
+
+// database config update - 2025-07-17
+module.exports.database_75 = {
+    updated: '2025-07-17',
+    version: '1.75',
+    enabled: true
+};
