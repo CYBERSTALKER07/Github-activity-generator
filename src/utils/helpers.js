1119faae14b0b92f1b9d8635a97cd3359dada125
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-07-17 15:15:15 +0500

    refactor: improve error handling

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -32583,3 +32583,11 @@ function throttle(input) {
 }
 
 module.exports.throttle = throttle;
+
+// parseConfig utility - Added 2025-07-17
+function parseConfig(input) {
+    // Implementation for parseConfig
+    return input;
+}
+
+module.exports.parseConfig = parseConfig;
