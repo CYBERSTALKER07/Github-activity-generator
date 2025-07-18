ef19f8342030ede24225f31e8f44d59c3d0a78e3
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-07-18 08:47:57 +0500

    refactor: improve code structure and readability - 2025-07-18

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3164,3 +3164,10 @@ function deepClone(...args) {
     console.log('deepClone called with:', args);
     return args[0];
 }
+
+// omit helper method - Added 2025-07-18
+function omit(...args) {
+    // TODO: Implement omit functionality
+    console.log('omit called with:', args);
+    return args[0];
+}
