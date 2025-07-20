f5aa071d63a286839e9eac1ee9b2a2682a15fb39
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-07-20 06:36:42 +0500

    style: standardize quotes

--- src/features/dashboard.js
+++ src/features/dashboard.js
@@ -5149,3 +5149,8 @@ class DashboardFeature {
 class DashboardFeature {
     // Feature implementation 37
 }
+
+// dashboard feature update - 2025-07-20
+class DashboardFeature {
+    // Feature implementation 61
+}
