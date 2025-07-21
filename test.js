4f88ea6e643340001c2882254547472cf27fe357
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-07-21 22:13:39 +0500

    Add unit tests

---
+++ test.js
@@ -0,0 +1,28 @@
+// Unit Tests
+// Generated: 2025-07-21T17:13:39.528Z
+
+const assert = require('assert');
+const CommitBooster = require('./commit-booster.js');
+
+describe('CommitBooster', () => {
+    let booster;
+
+    beforeEach(() => {
+        booster = new CommitBooster();
+    });
+
+    it('should initialize correctly', () => {
+        assert.ok(booster);
+        assert.ok(booster.projectRoot);
+    });
+
+    it('should generate daily commits', async () => {
+        // Test implementation
+        assert.ok(true);
+    });
+
+    it('should create micro commits', () => {
+        // Test implementation
+        assert.ok(true);
+    });
+});
