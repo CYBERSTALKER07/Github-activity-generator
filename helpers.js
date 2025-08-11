e17e45b6f9fc233b3222b736622e98bf83610c6d
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-08-11 15:54:32 +0500

    Define project constants

--- helpers.js
+++ helpers.js
@@ -1,8 +1,8 @@
 // Add helper methods
-// Updated: 2025-08-11T10:54:32.241Z
+// Updated: 2025-08-11T10:54:32.452Z
 
 module.exports = {
     // TODO: Implement add helper methods
-    created: '2025-08-11T10:54:32.241Z',
-    updated: '2025-08-11T10:54:32.241Z'
+    created: '2025-08-11T10:54:32.452Z',
+    updated: '2025-08-11T10:54:32.452Z'
 };
