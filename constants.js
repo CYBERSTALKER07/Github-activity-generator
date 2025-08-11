1718cd6bb8b4450ce4b2ecc023ab1f3b30f7c5f1
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-08-11 15:55:20 +0500

    Define project constants

--- constants.js
+++ constants.js
@@ -1,8 +1,8 @@
 // Define project constants
-// Updated: 2025-08-11T10:55:19.447Z
+// Updated: 2025-08-11T10:55:19.979Z
 
 module.exports = {
     // TODO: Implement define project constants
-    created: '2025-08-11T10:55:19.447Z',
-    updated: '2025-08-11T10:55:19.447Z'
+    created: '2025-08-11T10:55:19.979Z',
+    updated: '2025-08-11T10:55:19.979Z'
 };
--- helpers.js
+++ helpers.js
@@ -1,8 +1,8 @@
 // Add helper methods
-// Updated: 2025-08-11T10:55:19.903Z
+// Updated: 2025-08-11T10:55:19.955Z
 
 module.exports = {
     // TODO: Implement add helper methods
-    created: '2025-08-11T10:55:19.903Z',
-    updated: '2025-08-11T10:55:19.903Z'
+    created: '2025-08-11T10:55:19.955Z',
+    updated: '2025-08-11T10:55:19.955Z'
 };
