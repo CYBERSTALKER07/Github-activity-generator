efd752a974338d0e6fe9adbd14e68b818b3e0879
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-08-11 15:54:34 +0500

    Add helper methods

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // Update configuration
-// Updated: 2025-08-11T10:54:34.776Z
+// Updated: 2025-08-11T10:54:34.938Z
 
 module.exports = {
     // TODO: Implement update configuration
-    created: '2025-08-11T10:54:34.776Z',
-    updated: '2025-08-11T10:54:34.776Z'
+    created: '2025-08-11T10:54:34.938Z',
+    updated: '2025-08-11T10:54:34.938Z'
 };
--- helpers.js
+++ helpers.js
@@ -1,8 +1,8 @@
 // Add helper methods
-// Updated: 2025-08-11T10:54:34.603Z
+// Updated: 2025-08-11T10:54:34.959Z
 
 module.exports = {
     // TODO: Implement add helper methods
-    created: '2025-08-11T10:54:34.603Z',
-    updated: '2025-08-11T10:54:34.603Z'
+    created: '2025-08-11T10:54:34.959Z',
+    updated: '2025-08-11T10:54:34.959Z'
 };
