c5f72723da83847e910deb00890021ce34132716
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-08-11 15:55:16 +0500

    Add utility functions

--- constants.js
+++ constants.js
@@ -1,8 +1,8 @@
 // Define project constants
-// Updated: 2025-08-11T10:55:15.922Z
+// Updated: 2025-08-11T10:55:16.061Z
 
 module.exports = {
     // TODO: Implement define project constants
-    created: '2025-08-11T10:55:15.922Z',
-    updated: '2025-08-11T10:55:15.922Z'
+    created: '2025-08-11T10:55:16.061Z',
+    updated: '2025-08-11T10:55:16.061Z'
 };
