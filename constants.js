5bbc5f44bbd21f3db7035bbe1f54ad2a1a15b0ad
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-08-11 15:54:15 +0500

    Add utility functions

--- constants.js
+++ constants.js
@@ -1,8 +1,8 @@
 // Define project constants
-// Updated: 2025-08-11T10:54:15.462Z
+// Updated: 2025-08-11T10:54:15.935Z
 
 module.exports = {
     // TODO: Implement define project constants
-    created: '2025-08-11T10:54:15.462Z',
-    updated: '2025-08-11T10:54:15.462Z'
+    created: '2025-08-11T10:54:15.935Z',
+    updated: '2025-08-11T10:54:15.935Z'
 };
--- utils.js
+++ utils.js
@@ -1,8 +1,8 @@
 // Add utility functions
-// Updated: 2025-08-11T10:54:15.868Z
+// Updated: 2025-08-11T10:54:15.929Z
 
 module.exports = {
     // TODO: Implement add utility functions
-    created: '2025-08-11T10:54:15.868Z',
-    updated: '2025-08-11T10:54:15.868Z'
+    created: '2025-08-11T10:54:15.929Z',
+    updated: '2025-08-11T10:54:15.929Z'
 };
