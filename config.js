08d92ca5b3e6925f1b0384559c47c0f0569ad7c5
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-08-11 15:55:22 +0500

    Add helper methods

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // Update configuration
-// Updated: 2025-08-11T10:55:22.487Z
+// Updated: 2025-08-11T10:55:22.814Z
 
 module.exports = {
     // TODO: Implement update configuration
-    created: '2025-08-11T10:55:22.487Z',
-    updated: '2025-08-11T10:55:22.487Z'
+    created: '2025-08-11T10:55:22.814Z',
+    updated: '2025-08-11T10:55:22.814Z'
 };
