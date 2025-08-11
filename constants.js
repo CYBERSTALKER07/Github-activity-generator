5644f6866ce5db91a1fef708edd3aa7f8318374a
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-08-11 15:55:08 +0500

    Define project constants

--- constants.js
+++ constants.js
@@ -1,8 +1,8 @@
 // Define project constants
-// Updated: 2025-08-11T10:55:08.283Z
+// Updated: 2025-08-11T10:55:08.516Z
 
 module.exports = {
     // TODO: Implement define project constants
-    created: '2025-08-11T10:55:08.283Z',
-    updated: '2025-08-11T10:55:08.283Z'
+    created: '2025-08-11T10:55:08.516Z',
+    updated: '2025-08-11T10:55:08.516Z'
 };
