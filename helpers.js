3ddbfe55d0dd97b90bdb4d224f1275b9f67ae76d
GitHub Contributor <shsoliyev@aut-edu.uz>
2025-08-11 15:52:23 +0500

    Add helper methods

--- helpers.js
+++ helpers.js
@@ -1,8 +1,8 @@
 // Add helper methods
-// Updated: 2025-08-11T10:52:22.944Z
+// Updated: 2025-08-11T10:52:23.158Z
 
 module.exports = {
     // TODO: Implement add helper methods
-    created: '2025-08-11T10:52:22.944Z',
-    updated: '2025-08-11T10:52:22.944Z'
+    created: '2025-08-11T10:52:23.158Z',
+    updated: '2025-08-11T10:52:23.158Z'
 };
